4dc97a5c3ef13afc1ff9e0f70914f1f5ef77fba2
Rajan Marwaha <rajan.marwaha@gmail.com>
2021-08-07 19:09:05 +0100

    test

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -7,15 +7,17 @@ constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
+
 struct S {
-  int i;
   long long l;
-  short s;
+  int i;
   double d;
+  short s;
   bool b;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+
 
 void init(std::array<S, N> &arr);
 void solution(std::array<S, N> &arr);
