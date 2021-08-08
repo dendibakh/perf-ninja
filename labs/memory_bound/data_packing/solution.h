1aab8f5615aa898757cf8eb2dd948b7e5e95d11f
Oleksandr Koval <oleksandr.koval.dev@gmail.com>
2021-08-08 12:37:26 +0300

    data_packing solution

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -7,12 +7,12 @@ constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
-struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
-  bool b;
+struct __attribute__((packed)) S{
+  float d;
+  uint8_t i : 7;  // 7 bits are enough to represent 0-100 range
+  uint8_t s : 7;
+  uint16_t l : 14;  // max value  99*99, 14 bits are enough
+  bool b : 1;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
