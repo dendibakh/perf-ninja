4588cf20108a97b2e07479c73695ae42b8665fe8
Ivan Andrus <iandrus@adobe.com>
2021-08-13 15:23:32 -0600

    When packing bits, alignment is still important!

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -9,7 +9,7 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S_Old {
-  double d;                     // 0-1
+  float d;                     // 0-1
   short l;                      // (0-100)*(0-100) = 0-10000 -- 14
   signed char i;                // 0-100 -- 7
   signed char s;                // 0-100 -- 7
@@ -22,14 +22,20 @@ static_assert( std::numeric_limits<signed char>::max() >= 100);
 
 struct S_bitpacked {
   float d;                      // 0-1
-  unsigned l : 14;              // (0-100)*(0-100) = 0-10000 -- 14
+  unsigned l : 16;              // (0-100)*(0-100) = 0-10000 -- 14
+  unsigned s : 8;               // 0-100 -- 7
+  bool b : 1;                   // true/false -- 1
   unsigned i : 7;               // 0-100 -- 7
-  unsigned s : 7;               // 0-100 -- 7
-  unsigned b : 1;               // true/false -- 1
 
   bool operator<(const S_bitpacked &s) const { return this->i < s.i; }
 };
-using S = S_Old;
+using S = S_bitpacked;
+
+template<int N>
+class TD;
+
+// TD<sizeof(S_Old)> td1;
+// TD<sizeof(S_bitpacked)> td2;
 
 void init(std::array<S, N> &arr);
 void solution(std::array<S, N> &arr);
