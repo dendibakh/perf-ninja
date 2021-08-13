966daf57e5011a7c3e73a264754d1c9df193951d
Ivan Andrus <iandrus@adobe.com>
2021-08-13 14:50:32 -0600

    Optimize the data structure
    Interestingly, using a double seemed faster OMM

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -1,5 +1,6 @@
 
 #include <array>
+#include <limits>
 
 // Assume those constants never change
 constexpr int N = 10000;
@@ -8,14 +9,16 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
-  bool b;
+  double d;                     // 0-1
+  short l;                      // (0-100)*(0-100) = 0-10000
+  signed char i;                // 0-100
+  signed char s;                // 0-100
+  bool b;                       // true/false
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+static_assert( std::numeric_limits<short>::max() >= 10000);
+static_assert( std::numeric_limits<signed char>::max() >= 100);
 
 void init(std::array<S, N> &arr);
 void solution(std::array<S, N> &arr);
