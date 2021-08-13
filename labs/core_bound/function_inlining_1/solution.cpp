cd8c6391dd00f421ad294f3ed902edb7c5b7504c
Oleksandr Koval <oleksandr.koval.dev@gmail.com>
2021-08-13 21:14:04 +0300

    solve function_inlining_1

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -1,27 +1,34 @@
 
 #include "solution.h"
 #include <algorithm>
-#include <stdlib.h>
 
-static int compare(const void *lhs, const void *rhs) {
-  auto &a = *reinterpret_cast<const S *>(lhs);
-  auto &b = *reinterpret_cast<const S *>(rhs);
+void solution(std::array<S, N>& arr)
+{
+    std::sort(
+        std::begin(arr),
+        std::end(arr),
+        [](const auto& lhs, const auto& rhs)
+        {
+            if(lhs.key1 < rhs.key1)
+            {
+                return true;
+            }
 
-  if (a.key1 < b.key1)
-    return -1;
+            if(lhs.key1 > rhs.key1)
+            {
+                return false;
+            }
 
-  if (a.key1 > b.key1)
-    return 1;
+            if(lhs.key2 < rhs.key2)
+            {
+                return true;
+            }
 
-  if (a.key2 < b.key2)
-    return -1;
+            if(lhs.key2 > rhs.key2)
+            {
+                return false;
+            }
 
-  if (a.key2 > b.key2)
-    return 1;
-
-  return 0;
-}
-
-void solution(std::array<S, N> &arr) {
-  qsort(arr.data(), arr.size(), sizeof(S), compare);
+            return false;
+        });
 }
