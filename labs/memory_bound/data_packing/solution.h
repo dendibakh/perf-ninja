489d5520b4a35a59a4f2ebacec55bd8c5e4d15ef
Christian D <github@deneke.biz>
2021-08-14 14:26:14 +0200

    Double check with struct packing

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -10,16 +10,20 @@ constexpr int maxRandom = 100;
 struct S {
   // prefer use of byte sized types instead of 7 bit bitfield for ~5% better
   // performance at no increase of overall structure size
-  short i;
-  short s;
+  float d;
+  long long l : 16;
+  int i : 8;
+  short s : 7;
+  bool b : 1;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 
-  // Accessor methods that will still allow to retrieve values for l, d, b even
-  // though they are not represented as members anymore
-  long long get_l() const { return static_cast<short>(i * s); }
-  double get_d() const { return static_cast<double>(i) / maxRandom; }
-  bool get_b() const { return i < s; }
+  // // Accessor methods that will still allow to retrieve values for l, d, b
+  // even
+  // // though they are not represented as members anymore
+  // long long get_l() const { return static_cast<short>(i * s); }
+  // double get_d() const { return static_cast<double>(i) / maxRandom; }
+  // bool get_b() const { return i < s; }
 };
 
 void init(std::array<S, N> &arr);
