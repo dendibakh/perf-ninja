743de1281399eb90872aadb25d5400cb166261a0
Christian Deneke <43907599+chris0x44@users.noreply.github.com>
2021-08-15 21:49:48 +0200

    Extend validate to check for valid members (#4)
    * Extend validate to check for valid members
    
    * Fix use of members instead of access methods
    
    * Reduce redundant code
    
    * Remove accessors

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -18,4 +18,5 @@ struct S {
 };
 
 void init(std::array<S, N> &arr);
+S create_entry(int first_value, int second_value);
 void solution(std::array<S, N> &arr);
--- labs/memory_bound/data_packing/validate.cpp
+++ labs/memory_bound/data_packing/validate.cpp
@@ -3,6 +3,38 @@
 #include <algorithm>
 #include <iostream>
 
+template <typename Received_t, typename Expected_t>
+static bool validate(const char *var_name, Received_t received,
+                     Expected_t expected, int first_value, int second_value) {
+  if (received == expected)
+    return true;
+
+  std::cerr << "Validation Failed. Value " << var_name << " is " << received
+            << ". Expected is " << expected << " for intialization values "
+            << first_value << " and " << second_value << std::endl;
+  return false;
+}
+
+bool check_entry(int first, int second) {
+  S entry = create_entry(first, second);
+
+  bool result = validate("i", entry.i, first, first, second);
+
+  // check result of validate_value first to ensure all failures are printed
+  result = validate("s", entry.s, second, first, second) && result;
+
+  const auto expected_l = static_cast<short>(first * second);
+  result = validate("l", entry.l, expected_l, first, second) && result;
+
+  const auto expected_d = static_cast<double>(first) / maxRandom;
+  result = validate("d", entry.d, expected_d, first, second) && result;
+
+  const auto expected_b = (first < second);
+  result = validate("b", entry.b, expected_b, first, second) && result;
+
+  return result;
+}
+
 int main() {
   std::array<S, N> arr;
   init(arr);
@@ -18,6 +50,16 @@ int main() {
                 << ". Expected[" << i << "].i = " << expected[i].i << std::endl;
       return 1;
     }
+  }
+
+  bool checks_passed = check_entry(minRandom, minRandom);
+  checks_passed = check_entry(minRandom, maxRandom) && checks_passed;
+  checks_passed = check_entry(minRandom + 1, maxRandom - 1) && checks_passed;
+  checks_passed = check_entry(maxRandom, minRandom) && checks_passed;
+  checks_passed = check_entry(maxRandom, maxRandom) && checks_passed;
+
+  if (!checks_passed) {
+    return 2;
   }
 
   std::cout << "Validation Successful" << std::endl;
