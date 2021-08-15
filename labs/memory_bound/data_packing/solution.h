4f22ecc3d3a4aec2591fdf5bc889dc73b8bee4bf
Nico Galoppo <nico.galoppo@intel.com>
2021-08-15 15:27:54 -0700

    Reduce datatype bitwidth & shuffle order

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -6,16 +6,18 @@ constexpr int N = 10000;
 constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
-// FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
   double d;
+  short l;
+  char i;
+  char s;
   bool b;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+
+//template <int N> class TD;
+//TD<sizeof(S)> td;
 
 void init(std::array<S, N> &arr);
 void solution(std::array<S, N> &arr);
