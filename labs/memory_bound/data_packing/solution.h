6cbf27b3fd2563d446b4d31e66261cccfb430509
stdbug <68459057+stdbug@users.noreply.github.com>
2021-09-03 20:50:55 +0200

    data_packing v0

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -1,6 +1,8 @@
 
 #include <array>
 
+#include <stdint.h>
+
 // Assume those constants never change
 constexpr int N = 10000;
 constexpr int minRandom = 0;
@@ -8,14 +10,16 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
-  bool b;
+  unsigned l:  14;
+  unsigned b:   2;
+  unsigned s:   8;
+  unsigned i:   8;
+  float d;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+
+static_assert(sizeof(S) == 8);
 
 void init(std::array<S, N> &arr);
 S create_entry(int first_value, int second_value);
