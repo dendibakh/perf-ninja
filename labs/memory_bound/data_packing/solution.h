c92f820ed41d3afe988acdf9c48d811e3fb34234
pragmafox <87648934+pragmafox@users.noreply.github.com>
2021-09-12 17:47:05 +1000

    packing fields

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -8,11 +8,11 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
   double d;
-  bool b;
+  short l;
+  unsigned i: 7;
+  unsigned b: 1;
+  unsigned s: 7;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
