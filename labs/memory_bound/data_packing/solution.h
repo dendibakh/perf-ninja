01247c467e34a381b68dff4f689e15be244bc236
pragmafox <87648934+pragmafox@users.noreply.github.com>
2021-09-12 18:45:59 +1000

    Revert "data-packing: removed shuffle"
    This reverts commit 3a612258f3f5c14f009882124c01c518df19cfaa.

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -8,11 +8,11 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  unsigned char i;
-  unsigned s: 7;
-  unsigned b: 1;
-  short l;
   double d;
+  short l;
+  unsigned i: 7;
+  unsigned b: 1;
+  unsigned s: 7;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
