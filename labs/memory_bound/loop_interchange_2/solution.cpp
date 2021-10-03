f82e44ab01a861fccd7ef448b312a0699b44f33a
Kevin Kuehler <kevin@segfault.fun>
2021-10-03 11:51:41 -0700

    Pull out hottest part loop and sequentially access

--- labs/memory_bound/loop_interchange_2/solution.cpp
+++ labs/memory_bound/loop_interchange_2/solution.cpp
@@ -11,6 +11,26 @@ static void filterVertically(uint8_t *output, const uint8_t *input,
                              const int shift) {
   const int rounding = 1 << (shift - 1);
 
+  for (int r = radius; r < height - radius; r++) {
+    int dot[width]; // VLA not a great idea but whatever ;)
+    for (int c = 0; c < width; c++) {
+      dot[c] = 0;
+    }
+    // Middle part of computations with full kernel
+    for (int c = 0; c < width; c++) {
+      // Accumulation
+      for (int i = 0; i < radius + 1 + radius; i++) {
+        dot[c] += input[(r - radius + i) * width + c] * kernel[i];
+      }
+    }
+
+    for (int c = 0; c < width; c++) {
+      // Fast shift instead of division
+      int value = (dot[c] + rounding) >> shift;
+      output[r * width + c] = static_cast<uint8_t>(value);
+    }
+  }
+
   for (int c = 0; c < width; c++) {
     // Top part of line, partial kernel
     for (int r = 0; r < std::min(radius, height); r++) {
@@ -26,19 +46,6 @@ static void filterVertically(uint8_t *output, const uint8_t *input,
 
       // Normalization
       int value = static_cast<int>(dot / static_cast<float>(sum) + 0.5f);
-      output[r * width + c] = static_cast<uint8_t>(value);
-    }
-
-    // Middle part of computations with full kernel
-    for (int r = radius; r < height - radius; r++) {
-      // Accumulation
-      int dot = 0;
-      for (int i = 0; i < radius + 1 + radius; i++) {
-        dot += input[(r - radius + i) * width + c] * kernel[i];
-      }
-
-      // Fast shift instead of division
-      int value = (dot + rounding) >> shift;
       output[r * width + c] = static_cast<uint8_t>(value);
     }
 
