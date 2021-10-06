63dbfd61cc3043fb70d5ae15e5424dea1775c7f8
Jan Eisenhauer <mail@jan-ubben.de>
2021-10-06 22:21:10 +0200

    First contact.

--- tools/make_benchmark_library.sh
+++ tools/make_benchmark_library.sh
@@ -7,8 +7,8 @@ cd benchmark
 # Make a build directory to place the build output.
 cmake -E make_directory "build"
 # Generate build system files with cmake.
-cmake -E chdir "build" cmake -DCMAKE_BUILD_TYPE=Release ../
+#cmake -E chdir "build" cmake -DCMAKE_BUILD_TYPE=Release ../
 # or, starting with CMake 3.13, use a simpler form:
-# cmake -DCMAKE_BUILD_TYPE=Release -S . -B "build"
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -S . -B "build"
 # Build the library.
-cmake --build "build" --config Release --parallel 4
+cmake --build "build" --config Release
