d97ff504c1caf375168b7bfd49fecf53e1ae25b4
Kenny Shen <kenny@machinesung.com>
2021-10-09 02:53:18 +0800

    Data packing solution

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -6,13 +6,12 @@ constexpr int N = 10000;
 constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
-// FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
-  bool b;
+  float d;
+  long long l:16;
+  int i:8;
+  unsigned short s:7;
+  bool b:1;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
