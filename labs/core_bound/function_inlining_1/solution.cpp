cafbcd8685e78ed53b2b31e71e6f7bebe8d76a15
stdbug <68459057+stdbug@users.noreply.github.com>
2021-10-22 23:31:05 +0200

    function_inlining_1 V0 (cleanup)

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -3,25 +3,6 @@
 #include <algorithm>
 #include <stdlib.h>
 
-inline int compare(const void *lhs, const void *rhs) {
-  auto &a = *reinterpret_cast<const S *>(lhs);
-  auto &b = *reinterpret_cast<const S *>(rhs);
-
-  if (a.key1 < b.key1)
-    return -1;
-
-  if (a.key1 > b.key1)
-    return 1;
-
-  if (a.key2 < b.key2)
-    return -1;
-
-  if (a.key2 > b.key2)
-    return 1;
-
-  return 0;
-}
-
 void solution(std::array<S, N> &arr) {
   std::sort(arr.begin(), arr.end(), [](const S& a, const S& b) {
     if (a.key1 != b.key1) {
