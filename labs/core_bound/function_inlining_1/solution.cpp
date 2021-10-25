f19030dcf00bda03923972859a99cc732dd4b606
stdbug <68459057+stdbug@users.noreply.github.com>
2021-10-25 18:54:06 +0200

    function_inlining_1 V0 (ad hoc sort)

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -3,11 +3,48 @@
 #include <algorithm>
 #include <stdlib.h>
 
-void solution(std::array<S, N> &arr) {
-  std::sort(arr.begin(), arr.end(), [](const S& a, const S& b) {
-    if (a.key1 != b.key1) {
-      return a.key1 < b.key1;
+inline bool operator<(const S& a, const S& b) {
+  if (a.key1 != b.key1) {
+    return a.key1 < b.key1;
+  }
+  return a.key2 < b.key2;
+}
+
+template<typename Iter>
+void MyInsertionSort(Iter beg, Iter end) {
+  if (beg == end) {
+    return;
+  }
+  for (Iter it = std::next(beg); it != end; ++it) {
+    auto value = *it;
+    Iter i = it;
+    Iter j = std::prev(it);
+    while (value < *j) {
+      *i = *j;
+      --i;
+      if (j == beg)
+        break;
+      --j;
     }
-    return a.key2 < b.key2;
+    *i = value;
+  }
+}
+
+template<typename Iter>
+void MyQsort(Iter beg, Iter end, uint32_t from, uint32_t to) {
+  const size_t N = std::distance(beg, end);
+  if (N < 50) {
+    MyInsertionSort(beg, end);
+    return;
+  }
+  uint32_t pivot = from + (to-from)/2;
+  Iter mid = std::partition(beg, end, [pivot](const auto& elem){
+    return elem.key1 < pivot;
   });
+  MyQsort(beg, mid, from, pivot);
+  MyQsort(mid, end, pivot, to);
+}
+
+void solution(std::array<S, N> &arr) {
+  MyQsort(arr.begin(), arr.end(), 0, std::numeric_limits<uint32_t>::max());
 }
