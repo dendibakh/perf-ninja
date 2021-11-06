63c9f7cde92d3f35402d6a137de887f20f8bf2f5
Ivan Litov <ivanslitov@gmail.com>
2021-11-06 16:26:33 +0200

    [2nd try] Solved loop_interchange_1 lab.

--- labs/memory_bound/loop_interchange_1/solution.cpp
+++ labs/memory_bound/loop_interchange_1/solution.cpp
@@ -27,44 +27,42 @@ void multiply(Matrix &result, const Matrix &a, const Matrix &b) {
   zero(result);
 
   for (int i = 0; i < N; i++) {
-      for (int k = 0; k < N; k++) {
-        for (int j = 0; j < N; j++) {
-          result[i][j] += a[i][k] * b[k][j];
-        }
+    for (int k = 0; k < N; k++) {
+      for (int j = 0; j < N; j++) {
+        result[i][j] += a[i][k] * b[k][j];
+      }
     }
   }
 }
 
 // Compute integer power of a given square matrix
 Matrix power(const Matrix &input, const uint32_t k) {
+  std::unique_ptr<Matrix> tempMatrix(new Matrix());
+
   // Temporary products
   std::unique_ptr<Matrix> productCurrent(new Matrix());
-  std::unique_ptr<Matrix> productNext(new Matrix());
 
   // Temporary elements = a^(2^integer)
   std::unique_ptr<Matrix> elementCurrent(new Matrix());
-  std::unique_ptr<Matrix> elementNext(new Matrix());
 
   // Initial values
   identity(*productCurrent);
   *elementCurrent = input;
 
   // Use binary representation of k to be O(log(k))
-  for (auto i = k; i > 0; i /= 2) {
-    if (i % 2 != 0) {
+  for (auto i = k; i > 1; i /= 2) {
+    if (i & 1) {
       // Multiply the product by element
-      multiply(*productNext, *productCurrent, *elementCurrent);
-      std::swap(productNext, productCurrent);
-
-      // Exit early to skip next squaring
-      if (i == 1)
-        break;
+      multiply(*tempMatrix, *productCurrent, *elementCurrent);
+      std::swap(tempMatrix, productCurrent);
     }
 
     // Square an element
-    multiply(*elementNext, *elementCurrent, *elementCurrent);
-    std::swap(elementNext, elementCurrent);
+    multiply(*tempMatrix, *elementCurrent, *elementCurrent);
+    std::swap(tempMatrix, elementCurrent);
   }
 
-  return std::move(*productCurrent);
+  multiply(*tempMatrix, *productCurrent, *elementCurrent);
+
+  return std::move(*tempMatrix);
 }
