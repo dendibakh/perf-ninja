195984e3f2373c7ec064b407e05a2f75b5f83332
stdbug <68459057+stdbug@users.noreply.github.com>
2021-11-12 18:44:14 +0100

    bad_speculation/virtual_call_mispredict V0

--- labs/bad_speculation/virtual_call_mispredict/solution.cpp
+++ labs/bad_speculation/virtual_call_mispredict/solution.cpp
@@ -6,15 +6,25 @@ void generateObjects(InstanceArray& array) {
     std::default_random_engine generator(0);
     std::uniform_int_distribution<std::uint32_t> distribution(0, 2);
 
+    int zeros = 0;
+    int ones = 0;
     for (std::size_t i = 0; i < N; i++) {
         int value = distribution(generator);
         if (value == 0) {
-            array.push_back(std::make_unique<ClassA>());
+            zeros++;
         } else if (value == 1) {
-            array.push_back(std::make_unique<ClassB>());
-        } else {
-            array.push_back(std::make_unique<ClassC>());
+            ones++;
         }
+    }
+
+    for (int i = 0; i < zeros; i++) {
+        array.push_back(std::make_unique<ClassA>());
+    }
+    for (int i = 0; i < ones; i++) {
+        array.push_back(std::make_unique<ClassB>());
+    }
+    for (std::size_t i = 0; i < N-zeros-ones; i++) {
+        array.push_back(std::make_unique<ClassC>());
     }
 }
 
