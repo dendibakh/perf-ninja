4ec95c2780c705af091c5a9ca406c8805e533620
Denis Bakhvalov <dendibakh@gmail.com>
2021-11-18 13:08:54 -0800

    [New lab] Added a new lab about SW memory prefetching (#17)
    Authored-by: @ibogosavljevic

---
+++ labs/memory_bound/swmem_prefetch_1/bench.cpp
@@ -0,0 +1,24 @@
+
+#include "benchmark/benchmark.h"
+#include "solution.hpp"
+#include <memory>
+
+static void bench1(benchmark::State &state) {
+  // Init benchmark data
+  auto hash_map = std::make_unique<hash_map_t>(HASH_MAP_SIZE);
+  std::vector<int> lookups;
+  lookups.resize(NUMBER_OF_LOOKUPS);
+  init(hash_map.get(), lookups);
+
+  // Run the benchmark
+  for (auto _ : state) {
+    auto output = solution(hash_map.get(), lookups);
+    benchmark::DoNotOptimize(output);
+  }
+}
+
+// Register the function as a benchmark and measure time in microseconds
+BENCHMARK(bench1)->Unit(benchmark::kMillisecond);
+
+// Run the benchmark
+BENCHMARK_MAIN();
