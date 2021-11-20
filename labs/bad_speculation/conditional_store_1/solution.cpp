8435c6c3dd28d678571c4ec3422fb7e26706deaa
Ivan Litov <ivanslitov@gmail.com>
2021-11-20 16:54:32 +0200

    Solved conditional_store_1 lab.

--- labs/bad_speculation/conditional_store_1/solution.cpp
+++ labs/bad_speculation/conditional_store_1/solution.cpp
@@ -1,14 +1,16 @@
 
 #include "solution.h"
+#include <algorithm>
 
 // Select items which have S.first in range [lower..upper]
-std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
-                   const std::uint32_t lower, const std::uint32_t upper) {
+std::size_t select(std::array<S, N> &output, const std::array<S, N> &input, const std::uint32_t lower, const std::uint32_t upper) {
   std::size_t count = 0;
-  for (const auto item : input) {
-    if ((lower <= item.first) && (item.first <= upper)) {
-      output[count++] = item;
-    }
+
+  for (const S &item : input) {
+    const int result = (lower <= item.first) && (item.first <= upper);
+    output[count] = item;
+    count += result;
   }
+
   return count;
 }
