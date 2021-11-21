422087b8830f1d061e73a5761a8d4efa01becff3
Ivan Litov <ivanslitov@gmail.com>
2021-11-21 17:04:47 +0200

    Solved virtual_call_mispredict lab.

--- labs/bad_speculation/virtual_call_mispredict/solution.cpp
+++ labs/bad_speculation/virtual_call_mispredict/solution.cpp
@@ -1,33 +1,33 @@
 #include "solution.h"
 
 #include <random>
-#include <algorithm>
 
 void generateObjects(InstanceArray& array) {
     std::default_random_engine generator(0);
     std::uniform_int_distribution<std::uint32_t> distribution(0, 2);
+
+    std::size_t typeB = 0;
 
     for (std::size_t i = 0; i < N; i++) {
         int value = distribution(generator);
         if (value == 0) {
             array.push_back(std::make_unique<ClassA>());
         } else if (value == 1) {
-            array.push_back(std::make_unique<ClassB>());
-        } else {
-            array.push_back(std::make_unique<ClassC>());
+            ++typeB;
         }
+    }
+
+    for (std::size_t i = 0; i < typeB; ++i) {
+        array.push_back(std::make_unique<ClassB>());
+    }
+
+    for (std::size_t i = array.size(); i < N; ++i) {
+        array.push_back(std::make_unique<ClassC>());
     }
 }
 
 // Invoke the `handle` method on all instances in `output`
 void invoke(InstanceArray& array, std::size_t& data) {
-    std::sort(
-        begin(array)
-        , end(array)
-        , [](const std::unique_ptr<BaseClass> &a, const std::unique_ptr<BaseClass> &b) {
-            return a->classType < b->classType;
-        });
-
     for (const auto& item: array) {
         item->handle(data);
     }
