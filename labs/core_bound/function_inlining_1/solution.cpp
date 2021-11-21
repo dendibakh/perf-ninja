bc812299e2808c8574013bd3f0e4369ed12b6711
Ivan Litov <ivanslitov@gmail.com>
2021-11-21 17:44:17 +0200

    Solved function_inlining_1 lab.

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -1,27 +1,24 @@
 
 #include "solution.h"
 #include <algorithm>
-#include <stdlib.h>
-
-static int compare(const void *lhs, const void *rhs) {
-  auto &a = *reinterpret_cast<const S *>(lhs);
-  auto &b = *reinterpret_cast<const S *>(rhs);
-
-  if (a.key1 < b.key1)
-    return -1;
-
-  if (a.key1 > b.key1)
-    return 1;
-
-  if (a.key2 < b.key2)
-    return -1;
-
-  if (a.key2 > b.key2)
-    return 1;
-
-  return 0;
-}
 
 void solution(std::array<S, N> &arr) {
-  qsort(arr.data(), arr.size(), sizeof(S), compare);
+    std::sort(
+        begin(arr)
+        , end(arr)
+        , [](const S &a, const S &b) {
+            if (a.key1 < b.key1)
+                return true;
+
+            if (a.key1 > b.key1)
+                return false;
+
+            if (a.key2 < b.key2)
+                return true;
+
+            if (a.key2 > b.key2)
+                return false;
+
+            return false;
+        });
 }
