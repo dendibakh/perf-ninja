6c6a6790233e9328b0719c56a1daebcc80a54399
dbakhval <dbakhval@DBAKHVAL-MOBL>
2021-11-30 11:42:26 -0800

    [DataPacking] - Solution

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -8,11 +8,11 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
-  bool b;
+  float d;
+  unsigned long long l:16;
+  unsigned int i:8;
+  unsigned short s:7;
+  bool b:1;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
