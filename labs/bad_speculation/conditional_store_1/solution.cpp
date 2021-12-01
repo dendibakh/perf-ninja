65f40dbc82baa754ee16cb7764576887c9294ab7
dbakhval <dbakhval@DBAKHVAL-MOBL>
2021-11-30 19:51:52 -0800

    [ConditionalStore1] - Solution

--- labs/bad_speculation/conditional_store_1/solution.cpp
+++ labs/bad_speculation/conditional_store_1/solution.cpp
@@ -6,9 +6,8 @@ std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
                    const std::uint32_t lower, const std::uint32_t upper) {
   std::size_t count = 0;
   for (const auto item : input) {
-    if ((lower <= item.first) && (item.first <= upper)) {
-      output[count++] = item;
-    }
+    output[count] = item;
+    count += lower <= item.first && item.first <= upper;
   }
   return count;
 }
