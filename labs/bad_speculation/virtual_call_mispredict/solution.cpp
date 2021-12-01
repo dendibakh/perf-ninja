31296216faeddc6f28bcc1ce8e7d19fc408c32d8
dbakhval <dbakhval@DBAKHVAL-MOBL>
2021-11-30 20:02:26 -0800

    [CppVirtCallMispred] - Solution

--- labs/bad_speculation/virtual_call_mispredict/solution.cpp
+++ labs/bad_speculation/virtual_call_mispredict/solution.cpp
@@ -6,16 +6,23 @@ void generateObjects(InstanceArray& array) {
     std::default_random_engine generator(0);
     std::uniform_int_distribution<std::uint32_t> distribution(0, 2);
 
+    InstanceArray arrayA;
+    InstanceArray arrayB;
+    InstanceArray arrayC;
     for (std::size_t i = 0; i < N; i++) {
         int value = distribution(generator);
         if (value == 0) {
-            array.push_back(std::make_unique<ClassA>());
+            arrayA.push_back(std::make_unique<ClassA>());
         } else if (value == 1) {
-            array.push_back(std::make_unique<ClassB>());
+            arrayB.push_back(std::make_unique<ClassB>());
         } else {
-            array.push_back(std::make_unique<ClassC>());
+            arrayC.push_back(std::make_unique<ClassC>());
         }
     }
+
+    array.insert( array.end(), std::make_move_iterator(arrayA.begin()), std::make_move_iterator(arrayA.end()) );
+    array.insert( array.end(), std::make_move_iterator(arrayB.begin()), std::make_move_iterator(arrayB.end()) );
+    array.insert( array.end(), std::make_move_iterator(arrayC.begin()), std::make_move_iterator(arrayC.end()) );
 }
 
 // Invoke the `handle` method on all instances in `output`
