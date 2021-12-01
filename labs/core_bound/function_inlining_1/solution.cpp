e9df5cd9ad198c478aa8e0946093e9140443f207
dbakhval <dbakhval@DBAKHVAL-MOBL>
2021-11-30 19:42:23 -0800

    [FunctionInlining1] - Solution

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -23,5 +23,8 @@ static int compare(const void *lhs, const void *rhs) {
 }
 
 void solution(std::array<S, N> &arr) {
-  qsort(arr.data(), arr.size(), sizeof(S), compare);
+  //qsort(arr.data(), arr.size(), sizeof(S), compare);
+  std::sort(arr.begin(), arr.end(), [](S& a, S& b){
+    return a.key1 < b.key1 || (a.key1 == b.key1) && (a.key2 < b.key2);
+  });
 }
