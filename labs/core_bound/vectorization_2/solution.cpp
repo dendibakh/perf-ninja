65da1da251a3c3d52c7e4340b56b21a7c1671dec
dbakhval <dbakhval@DBAKHVAL-MOBL>
2021-11-30 19:46:35 -0800

    [Vectorization2] - Solution

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,10 +1,15 @@
 #include "solution.hpp"
 
-uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
-  for (auto value : blob) {
+uint16_t checksum(const Blob& blob) {
+  uint32_t acc = 0;
+  for (const auto value : blob) {
     acc += value;
-    acc += acc < value; // add carry
   }
-  return acc;
+
+  while ((acc >> 16) != 0) {
+    auto high = acc >> 16;
+    auto low = acc & 0xFFFFu;
+    acc = low + high;
+  }
+  return static_cast<uint16_t>(acc);
 }
