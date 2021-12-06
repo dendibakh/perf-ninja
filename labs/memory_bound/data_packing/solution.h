3a7577626e57006f2a68117870f60e081c52269b
Alastair Harrison <aharrison24@gmail.com>
2021-12-06 23:01:38 +0000

    Solve data packing problem

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -1,18 +1,19 @@
+#pragma once
 
 #include <array>
+#include <cstdint>
 
 // Assume those constants never change
 constexpr int N = 10000;
 constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
-// FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
-  bool b;
+  float d;         // {0, 0.1, 1}
+  uint16_t l : 14; // [0, 10000]
+  uint16_t b : 2;  // [0..1]
+  uint8_t i;       // [0..100]
+  uint8_t s;       // [0..100]
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
