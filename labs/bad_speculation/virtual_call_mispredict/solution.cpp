e3ff84c0063bcb733a59ebe6f28602b0f814ff70
Oleksandr Koval <oleksandr.koval.dev@gmail.com>
2021-12-18 18:18:24 +0200

    solve `virtual_call_mispredict`

--- labs/bad_speculation/virtual_call_mispredict/solution.cpp
+++ labs/bad_speculation/virtual_call_mispredict/solution.cpp
@@ -5,17 +5,25 @@
 void generateObjects(InstanceArray& array) {
     std::default_random_engine generator(0);
     std::uniform_int_distribution<std::uint32_t> distribution(0, 2);
+    InstanceArray a;
+    InstanceArray b;
+    InstanceArray c;
 
     for (std::size_t i = 0; i < N; i++) {
         int value = distribution(generator);
         if (value == 0) {
-            array.push_back(std::make_unique<ClassA>());
+            a.push_back(std::make_unique<ClassA>());
         } else if (value == 1) {
-            array.push_back(std::make_unique<ClassB>());
+            b.push_back(std::make_unique<ClassB>());
         } else {
-            array.push_back(std::make_unique<ClassC>());
+            c.push_back(std::make_unique<ClassC>());
         }
     }
+
+    array.reserve(array.size() + N);
+    std::move(std::begin(a), std::end(a), std::back_inserter(array));
+    std::move(std::begin(b), std::end(b), std::back_inserter(array));
+    std::move(std::begin(c), std::end(c), std::back_inserter(array));
 }
 
 // Invoke the `handle` method on all instances in `output`
