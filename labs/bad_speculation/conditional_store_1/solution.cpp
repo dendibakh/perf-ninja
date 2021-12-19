e781eb2f8e75b912c8d3041fdab23acef45a8f78
Oleksandr Koval <oleksandr.koval.dev@gmail.com>
2021-12-19 19:25:03 +0200

    solve `conditional_store_1`

--- labs/bad_speculation/conditional_store_1/solution.cpp
+++ labs/bad_speculation/conditional_store_1/solution.cpp
@@ -1,14 +1,27 @@
 
 #include "solution.h"
 
+#include <algorithm>
+#include <iterator>
+
 // Select items which have S.first in range [lower..upper]
 std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
                    const std::uint32_t lower, const std::uint32_t upper) {
-  std::size_t count = 0;
-  for (const auto item : input) {
-    if ((lower <= item.first) && (item.first <= upper)) {
-      output[count++] = item;
-    }
-  }
-  return count;
+  const auto cmp = [](const auto& lhs, const auto& rhs){
+    return lhs.first < rhs.first;
+  };
+
+  const auto begin = std::lower_bound(
+    std::cbegin(input),
+    std::cend(input),
+    S{lower, 0},
+    cmp);
+  const auto end = std::upper_bound(
+    begin,
+    std::cend(input),
+    S{upper,0},
+    cmp);
+  
+  std::copy(begin, end, std::begin(output));
+  return std::distance(begin, end);
 }
