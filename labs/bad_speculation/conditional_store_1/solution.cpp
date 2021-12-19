9998ae12f6f775cf74b3273c2fedc47bf4e71aa7
Oleksandr Koval <oleksandr.koval.dev@gmail.com>
2021-12-19 21:33:02 +0200

    add logs

--- labs/bad_speculation/conditional_store_1/solution.cpp
+++ labs/bad_speculation/conditional_store_1/solution.cpp
@@ -3,12 +3,12 @@
 
 #include <algorithm>
 #include <iterator>
-#include <cassert>
+#include <iostream>
 
 // Select items which have S.first in range [lower..upper]
 std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
                    const std::uint32_t lower, const std::uint32_t upper) {
-  assert(std::is_sorted(std::cbegin(input), std::cend(input)));
+  std::cout << "lower: " << lower << ", upper: " << upper << std::endl;
   const auto begin = std::lower_bound(
     std::cbegin(input),
     std::cend(input),
