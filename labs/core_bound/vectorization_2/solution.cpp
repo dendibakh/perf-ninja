8b28487b801e93ca38c5b4f08dd492102da60846
stdbug <68459057+stdbug@users.noreply.github.com>
2021-12-25 23:08:13 +0100

    core_bound/vectorization_2 V0

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,10 +1,11 @@
 #include "solution.hpp"
 
 uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
+  uint32_t acc = 0;
   for (auto value : blob) {
     acc += value;
-    acc += acc < value; // add carry
   }
-  return acc;
+  acc += acc >> 16;
+
+  return (uint16_t)acc;
 }
