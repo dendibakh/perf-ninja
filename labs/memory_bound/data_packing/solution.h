5b9095f845e5c3c252f4487c5d82e895fb3e8034
Yuriy Lyfenko <lyfenko@yahoo.com>
2022-01-03 13:49:49 -0500

    Data packing w/alignas

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -7,12 +7,12 @@ constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
-struct S {
+struct alignas(8) S {
   float d;
-  unsigned long long l: 16; // 0..10000
-  unsigned int i:8; // 0..100
-  unsigned short s: 7; // 0..100
-  bool b: 1;
+  unsigned int l: 16; // 0..10000
+  unsigned int i: 8; // 0..100
+  unsigned int s: 7; // 0..100
+  bool b: 1; // 0..1
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
