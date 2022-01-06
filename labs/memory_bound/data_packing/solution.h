189a172059595662034b1c7068676008fd3970b4
xurongdong <xurongdong@agora.io>
2022-01-06 14:05:03 +0800

    adjust size

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -8,11 +8,11 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
-  bool b;
+  u_char i;
+  u_char s: 7;
+  u_char b: 1;
+  short l;
+  float d;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
