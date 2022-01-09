62d3d7fa1be6dc481fddc7ca6e7041d6db392558
EsipovPA <esipov.p@mail.ru>
2022-01-09 23:13:09 +0300

    Solved data packing lab

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -8,14 +8,15 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
+  float d;
+  long l: 15;
+  unsigned short i: 8;
+  unsigned short s: 8;
   bool b;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+#pragma pack()
 
 void init(std::array<S, N> &arr);
 S create_entry(int first_value, int second_value);
