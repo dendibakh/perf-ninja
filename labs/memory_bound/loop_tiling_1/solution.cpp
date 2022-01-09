f4fbcf681d3d64e0fe92b4f53744ef27d72d99ef
Ivan Litov <ivanslitov@gmail.com>
2022-01-09 18:42:18 +0200

    Solved loop_tiling_1 lab.

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -2,11 +2,28 @@
 #include <algorithm>
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
-  int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
+  constexpr int CACHE_LINE_SIZE = 64;
+  constexpr int ELMNS_PER_CL = CACHE_LINE_SIZE / sizeof(MatrixOfDoubles::value_type::value_type);
+  constexpr int HORIZONTAL_STRIP = 8 * ELMNS_PER_CL;
+  constexpr int VERTICAL_STRIP = HORIZONTAL_STRIP / 2;
+
+  const int size = static_cast<int>(in.size());
+
+  for (int ii = 0; ii < size; ii += HORIZONTAL_STRIP) {
+
+    for (int jj = 0; jj < size; jj += VERTICAL_STRIP) {
+
+      for (int i = ii; i < std::min(size, ii + HORIZONTAL_STRIP); ++i) {
+
+        for (int j = jj; j < std::min(size, jj + VERTICAL_STRIP); ++j) {
+          out[i][j] = in[j][i];
+        }
+
+      }
+
     }
+
   }
-  return out[0][size - 1];
+
+  return out.front().back();
 }
