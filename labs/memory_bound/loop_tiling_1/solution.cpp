6726a1338acf11f2d7c0772f9ae0fd471ab5e88f
stdbug <68459057+stdbug@users.noreply.github.com>
2022-01-10 22:13:12 +0100

    memory_bound/loop_tiling_1 V3

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -2,29 +2,17 @@
 #include <algorithm>
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
-  size_t size = in.size();
+  const size_t size = in.size();
 
   constexpr size_t kStride = 40;
 
-  const size_t full_strides = size - size % kStride;
-
-  for (size_t i = 0; i < full_strides; i += kStride) {
-    for (size_t j = 0; j < full_strides; j += kStride) {
-      for (size_t k = 0; k < kStride; k++) {
-        for (size_t l = 0; l < kStride; l++) {
-          out[j + l][i + k] = in[i + k][j + l];
+  for (size_t i = 0; i < size; i += kStride) {
+    for (size_t j = 0; j < size; j += kStride) {
+      for (size_t l = j; l < std::min(size, j+kStride); l++) {
+        for (size_t k = i; k < std::min(size, i+kStride); k++) {
+          out[l][k] = in[k][l];
         }
       }
-    }
-  }
-  for (size_t i = full_strides; i < size; i++) {
-    for (size_t j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
-    }
-  }
-  for (size_t i = 0; i < size; i++) {
-    for (size_t j = full_strides; j < size; j++) {
-      out[i][j] = in[j][i];
     }
   }
 
