5bb35b9ae649679708ce3d7270b2d38e8615f166
dendibakh <dendibakh@gmail.com>
2022-01-26 22:28:14 -0800

    [LoopTiling1] - Solution

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -1,12 +1,18 @@
 #include "solution.hpp"
 #include <algorithm>
 
-bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
+bool solution(MatrixOfDoubles& in, MatrixOfDoubles& out) {
+  static constexpr int TILE_SIZE = 16;
   int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
+  for (int ii = 0; ii < size; ii+= TILE_SIZE) {
+    for (int jj = 0; jj < size; jj+= TILE_SIZE) {
+      for (int i = ii; i < std::min(ii + TILE_SIZE, size); i++) {
+        for (int j = jj; j < std::min(jj + TILE_SIZE, size); j++) {
+          out[i][j] = in[j][i];
+        }
+      }
     }
   }
+
   return out[0][size - 1];
 }
