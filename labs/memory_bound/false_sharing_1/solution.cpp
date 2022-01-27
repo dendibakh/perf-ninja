6d9a810db0013e09eb6414b66d2c2e6b08a94249
dendibakh <dendibakh@gmail.com>
2022-01-26 22:27:13 -0800

    [FalseSharing1] - Solution

--- labs/memory_bound/false_sharing_1/solution.cpp
+++ labs/memory_bound/false_sharing_1/solution.cpp
@@ -8,7 +8,8 @@ std::size_t solution(const std::vector<uint32_t> &data, int thread_count) {
   // Using std::atomic counters to disallow compiler to promote `target`
   // memory location into a register. This way we ensure that the store
   // to `target` stays inside the loop.
-  struct Accumulator {
+  #define CACHELINE_ALIGN alignas(64)
+  struct CACHELINE_ALIGN Accumulator {
     std::atomic<uint32_t> value = 0;
   };
   std::vector<Accumulator> accumulators(thread_count);
