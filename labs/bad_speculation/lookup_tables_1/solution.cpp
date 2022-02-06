1e0d2b7845d54d8693d4ee971b268902b051b082
Ivan Litov <ivanslitov@gmail.com>
2022-02-06 18:57:06 +0200

    Solved lookup_tables_1 lab.

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -11,10 +11,50 @@ static std::size_t mapToBucket(std::size_t v) {    // diff
   return -1;  // let it crash
 }
 
-std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
-  std::array<std::size_t, NUM_BUCKETS> retBuckets{0};
-  for (auto v : values) {
-    retBuckets[mapToBucket(v)]++;
+static size_t mapToBucket2(size_t v) {
+	size_t index{ 0 };
+
+	index += v >= 13;
+	index += v >= 29;
+	index += v >= 41;
+	index += v >= 53;
+	index += v >= 71;
+	index += v >= 83;
+	
+	return index;
+}
+
+constexpr size_t MAX_VALUE = 100;
+using lookupArray_t = std::array<size_t, MAX_VALUE>;
+
+constexpr lookupArray_t fillLookupArray() {
+	lookupArray_t lookupArray{};
+	constexpr std::array steps = { 13,29,41,53,71,83,100 };
+
+	for (size_t i = 1; i < steps.size(); ++i) {
+		const int from = steps[i - 1];
+		const int to = steps[i];
+
+		for (size_t j = from; j < to; ++j) {
+			lookupArray[j] = i;
+		}
+	}
+
+	return lookupArray;
+}
+
+constexpr lookupArray_t lookupArray = fillLookupArray();
+
+static size_t mapToBucket3(size_t v) {
+	return lookupArray[v];
+}
+
+std::array<size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
+  std::array<size_t, NUM_BUCKETS> retBuckets{0};
+
+  for (int v : values) {
+    retBuckets[mapToBucket3(v)]++;
   }
+
   return retBuckets;
 }
