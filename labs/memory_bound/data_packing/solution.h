f3fa3538e90a1ca4032fcb254a11da72978357d2
aschhabra <chhabra.asingh@gmail.com>
2022-03-10 17:16:41 -0500

    windows data packing

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -9,10 +9,10 @@ constexpr int maxRandom = 100;
 
 // this data structure can be reduced in size
 struct S {
-  unsigned b:1;
   unsigned i:8;
   unsigned s:8;
   unsigned l:14;
+  unsigned b:2;
   float d;
 
   bool operator<(const S &s) const { return this->i < s.i; }
