bc5de55b05777a6e20946d649be80e7e044d4a85
Yassir Karroum <ukarroum17@gmail.com>
2022-03-25 07:17:05 +0100

    [misc] Warmup first solution

--- labs/misc/warmup/solution.cpp
+++ labs/misc/warmup/solution.cpp
@@ -3,8 +3,8 @@
 
 int solution(int *arr, int N) {
   int res = 0;
-  for (int i = 0; i < N; i++) {
-    res += arr[i];
-  }
-  return res;
+  //for (int i = 0; i < N; i++) {
+  //  res += arr[i];
+  //}
+  return (N * (N + 1))/2;
 }
