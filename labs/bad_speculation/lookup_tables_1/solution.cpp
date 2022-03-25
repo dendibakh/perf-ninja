1b46b0ae06e95ea428a444cf8469d7d3ec8c3a44
Luc Pelletier <pelletier.luc@gmail.com>
2022-03-25 14:39:40 -0400

    Small fix for lookup_tables_1

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -8,13 +8,13 @@ class BucketMapper {
     BucketMapper() {
       std::call_once(lookup_table_initialized_, [this](){
         for (size_t i = 0; i < lookup_table_.size(); i++) {
-          if (i >= 0 && i < 13)        lookup_table_[i] =  0; // 13
+          if (i < 13)                  lookup_table_[i] =  0; // 13
           else if (i >= 13 && i < 29)  lookup_table_[i] =  1; // 16
           else if (i >= 29 && i < 41)  lookup_table_[i] =  2; // 12
           else if (i >= 41 && i < 53)  lookup_table_[i] =  3; // 12
           else if (i >= 53 && i < 71)  lookup_table_[i] =  4; // 18
           else if (i >= 71 && i < 83)  lookup_table_[i] =  5; // 12
-          else if (i >= 83 && i < lookup_table_.size()) lookup_table_[i] =  6; // 17
+          else if (i >= 83)            lookup_table_[i] =  6; // 17
         }
       });
     }
