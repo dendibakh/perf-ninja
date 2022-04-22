54ca0c79c74dd405c32d8a1f1cfe0c94018ddab9
fuyw <royxroy@163.com>
2022-04-21 20:07:29 -0400

    shift bite

--- labs/memory_bound/data_packing/init.cpp
+++ labs/memory_bound/data_packing/init.cpp
@@ -19,10 +19,10 @@
 S create_entry(int first_value, int second_value) {
   S entry;
 
-  entry.i = static_cast<short>(first_value);
+  entry.i = first_value;
   entry.s = static_cast<short>(second_value);
-  entry.l = static_cast<int>(first_value * second_value);
-  entry.d = static_cast<float>(first_value) / maxRandom;
+  entry.l = static_cast<long long>(first_value * second_value);
+  entry.d = static_cast<double>(first_value) / maxRandom;
   entry.b = first_value < second_value;
 
   return entry;
--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -6,40 +6,20 @@ constexpr int N = 10000;
 constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
+struct S {  // TD<24>
+  float d;
+  long long l:16;
+  int i:8;
+  short s:7;
+  bool b:1;
 
-// Baseline ==> 363
-// struct S {
-//   int i;
-//   long long l;
-//   short s;
-//   double d;
-//   bool b;
-
-//   bool operator<(const S &s) const { return this->i < s.i; }
-// };
-
-
-// solution 1 ==> 303/311
-// struct S {
-//   double d;
-//   long long l;
-//   int i;
-//   short s;
-//   bool b;
-
-//   bool operator<(const S &s) const { return this->i < s.i; }
-// };
-
-
-// solution 2 ==> 279
-struct S {
   bool operator<(const S &s) const { return this->i < s.i; }
-  bool b;       // 1
-  short i;      // 2
-  short s;      // 2
-  int l;        // 4
-  float d;      // 8
 };
+
+template <int N>
+class TD;
+
+// TD<sizeof(S)> td;
 
 void init(std::array<S, N> &arr);
 S create_entry(int first_value, int second_value);
