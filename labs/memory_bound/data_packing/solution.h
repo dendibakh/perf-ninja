0265e4a6a5985cdda9cb020ad2e2b34589263b67
Dmitry Kovalenko <d.kovalenko3001@gmail.com>
2022-04-30 00:05:20 +0700

    Solved data packing

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -6,7 +6,7 @@ constexpr int N = 10000;
 constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
-// FIXME: this data structure can be reduced in size
+#ifdef BASELINE
 struct S {
   int i;
   long long l;
@@ -16,6 +16,17 @@ struct S {
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+#else
+struct S {
+  bool b;
+  short s;
+  int i;
+  double d;
+  long long l;
+
+  bool operator<(const S &s) const { return this->i < s.i; }
+};
+#endif
 
 void init(std::array<S, N> &arr);
 S create_entry(int first_value, int second_value);
