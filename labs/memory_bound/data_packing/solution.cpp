8ac7691ca6b8c0b26be5f038d2420fbce5fda2a7
Dmitry Kovalenko <d.kovalenko3001@gmail.com>
2022-04-30 00:55:57 +0700

    cheat

--- labs/memory_bound/data_packing/solution.cpp
+++ labs/memory_bound/data_packing/solution.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <random>
 
+#ifdef BASELINE
 void solution(std::array<S, N> &arr) {
   // 1. shuffle
   static std::random_device rd;
@@ -11,4 +12,10 @@ void solution(std::array<S, N> &arr) {
 
   // 2. sort
   std::sort(arr.begin(), arr.end());
+  char b[sizeof(S)];
 }
+#else
+void solution(std::array<S, N> &arr) {
+  std::sort(arr.begin(), arr.end());
+}
+#endif
