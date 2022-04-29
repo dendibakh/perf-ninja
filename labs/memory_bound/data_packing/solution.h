60710947d43f5a4b4e13231e0a56642c41c13f8a
fuyw <royxroy@163.com>
2022-04-28 21:24:28 -0400

    use long

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -6,20 +6,31 @@ constexpr int N = 10000;
 constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
+
 // FIXME: this data structure can be reduced in size
-struct S {
+//struct S {  // 40 bits
+//  int i;
+//  long long l;
+//  short s;
+//  double d;
+//  bool b;
+//
+//  bool operator<(const S &s) const { return this->i < s.i; }
+//};
+
+struct S {  // 24 bits
   float d;
-  long long l:16;
-  int i:8;
+  long l;
+  int i;
   short s;
-  bool b:1;
+  bool b;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
 
 template <int N>
 class TD;
-// TD(sizeof(S)) td;
+TD<sizeof(S)> td;
 
 void init(std::array<S, N> &arr);
 S create_entry(int first_value, int second_value);
