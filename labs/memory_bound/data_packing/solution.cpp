0cee34332cf07c4c9cb10a6dc8d18d9abd62fe09
Dmitry Kovalenko <d.kovalenko3001@gmail.com>
2022-04-30 01:23:09 +0700

    One more try on alignment

--- labs/memory_bound/data_packing/solution.cpp
+++ labs/memory_bound/data_packing/solution.cpp
@@ -3,7 +3,6 @@
 #include <algorithm>
 #include <random>
 
-#ifdef BASELINE
 void solution(std::array<S, N> &arr) {
   // 1. shuffle
   static std::random_device rd;
@@ -12,10 +11,4 @@ void solution(std::array<S, N> &arr) {
 
   // 2. sort
   std::sort(arr.begin(), arr.end());
-  char b[sizeof(S)];
 }
-#else
-void solution(std::array<S, N> &arr) {
-  std::sort(arr.begin(), arr.end());
-}
-#endif
