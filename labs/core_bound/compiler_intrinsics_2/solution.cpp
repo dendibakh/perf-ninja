3fcbfd324127be95ce777972e74a8b2d409a6894
stdbug <68459057+stdbug@users.noreply.github.com>
2022-05-01 00:47:52 +0200

    core_bound/compiler_intrinsics_2 v0.0.1

--- labs/core_bound/compiler_intrinsics_2/solution.cpp
+++ labs/core_bound/compiler_intrinsics_2/solution.cpp
@@ -1,57 +1,8 @@
 #include "solution.hpp"
 
 #include <iostream>
-#include <immintrin.h>
 
-constexpr size_t kStep = 16;
 
-unsigned solution(const std::string& inputContents) {
-  const char* beg = inputContents.c_str();
-  const char* end = beg + inputContents.length();
-  uint64_t u64beg = reinterpret_cast<uint64_t>(beg);
-  uint64_t u64beg_aligned =
-      u64beg % kStep == 0 ? u64beg : u64beg + (kStep - u64beg % kStep);
-  uint64_t u64end = reinterpret_cast<uint64_t>(end);
-  uint64_t u64end_aligned = u64end - u64end % kStep;
-
-  const char* beg_aligned = reinterpret_cast<const char*>(u64beg_aligned);
-  ptrdiff_t longest = 0;
-  const char* last_line_beg = beg;
-  for (const char* ch = beg; ch < beg_aligned; ch++) {
-    if (*ch == '\n') {
-      longest = std::max<ptrdiff_t>(longest, ch - last_line_beg);
-      last_line_beg = ch + 1;
-    }
-  }
-
-  const char* end_aligned = reinterpret_cast<const char*>(u64end_aligned);
-  __m128i spaces = _mm_set1_epi8('\n');
-  for (const char* pch = beg_aligned; pch < end_aligned; pch += kStep) {
-    __m128i newlines = _mm_cmpeq_epi8(*(const __m128i*)pch, spaces);
-    if (_mm_testz_si128(newlines, newlines)) {
-      continue;
-    }
-
-    for (int i = 0; i < kStep; i++) {
-      if (pch[i] == '\n') {
-        longest = std::max<ptrdiff_t>(longest, &pch[i] - last_line_beg);
-        last_line_beg = &pch[i + 1];
-      }
-    }
-  }
-
-  for (const char* ch = end_aligned; ch < end; ch++) {
-    if (*ch == '\n') {
-      longest = std::max<ptrdiff_t>(longest, ch - last_line_beg);
-      last_line_beg = ch + 1;
-    }
-  }
-
-  longest = std::max<ptrdiff_t>(longest, end - last_line_beg);
-
-  return static_cast<unsigned>(longest);
-}
-/*
 bool ContainsZeroByte(uint64_t v) {
   return ((v - UINT64_C(0x0101010101010101)) & ~v &
           UINT64_C(0x8080808080808080)) != 0;
@@ -106,4 +57,3 @@ unsigned solution(const std::string& inputContents) {
 
   return static_cast<unsigned>(longest);
 }
-*/
