7450367ba75cbe07435926edc1b3219c52fde8d3
fuyw <royxroy@163.com>
2022-05-07 00:48:24 -0400

    solve swmem

--- labs/memory_bound/swmem_prefetch_1/solution.cpp
+++ labs/memory_bound/swmem_prefetch_1/solution.cpp
@@ -9,10 +9,32 @@ static int getSumOfDigits(int n) {
   return sum;
 }
 
+// Origin implementation
+// int solution(const hash_map_t *hash_map, const std::vector<int> &lookups) {
+//   int result = 0;
+
+//   for (int val : lookups) {
+//     if (hash_map->find(val))
+//       result += getSumOfDigits(val);
+//   }
+
+//   return result;
+// }
+
+constexpr int look_ahead = 16;
+
 int solution(const hash_map_t *hash_map, const std::vector<int> &lookups) {
   int result = 0;
 
-  for (int val : lookups) {
+  for (size_t i=0; i < lookups.size() - look_ahead; i++) {
+    int val = lookups[i];
+    if (hash_map->find(val))
+      result += getSumOfDigits(val);
+    hash_map->prefetchForVal(lookups[i + look_ahead]);
+  }
+
+  for (size_t i=lookups.size() - look_ahead; i < lookups.size(); i++) {
+    int val = lookups[i];
     if (hash_map->find(val))
       result += getSumOfDigits(val);
   }
