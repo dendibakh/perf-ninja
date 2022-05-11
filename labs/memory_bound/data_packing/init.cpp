703c52154e6e1e7d2084c89559a756224eb0dd40
somelun <somelun@gmail.com>
2022-05-11 20:41:42 +0100

    Data Packing attempt

--- labs/memory_bound/data_packing/init.cpp
+++ labs/memory_bound/data_packing/init.cpp
@@ -5,10 +5,10 @@
 S create_entry(int first_value, int second_value) {
   S entry;
 
-  entry.i = first_value;
-  entry.s = second_value;
-  entry.l = static_cast<uint32_t>(first_value * second_value);
   entry.d = static_cast<float>(first_value) / maxRandom;
+  entry.l = static_cast<uint16_t>(first_value * second_value);
+  entry.i = static_cast<uint16_t>(first_value);
+  entry.s = static_cast<uint16_t>(second_value);
   entry.b = first_value < second_value;
 
   return entry;
