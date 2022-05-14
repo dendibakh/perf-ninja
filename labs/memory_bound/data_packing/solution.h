c4f4c19a9893c8d13dd34e38285fd39aca7e16ad
Ole Schulz-Trieglaff <ole.st@gmx.de>
2022-05-14 23:07:52 +0100

    using bitfields

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -8,11 +8,11 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  double d;
-  long long l;
-  int i;
-  short s;
-  bool b;
+  float d;
+  long long l:16;
+  int i:8;
+  short s:7;
+  bool b:1;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
