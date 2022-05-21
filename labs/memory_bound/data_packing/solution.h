61066b6f82438c312532c6407b050b68541b24fe
Ole Schulz-Trieglaff <ole.st@gmx.de>
2022-05-21 22:49:25 +0100

    moving more bits around

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -7,15 +7,22 @@ constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
+#pragma pack(push, 1)
 struct S {
   float d;
-  long long l:16;
-  unsigned int i:8;
+  unsigned long long l:14;
+  unsigned int i:7;
   unsigned short s:7;
-  unsigned int b:1;
+  unsigned b:1;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+#pragma pack(pop)
+
+/*template <int N>
+class TD;
+
+TD<sizeof(S)> td;*/
 
 void init(std::array<S, N> &arr);
 S create_entry(int first_value, int second_value);
