7d85486f6718a8d6007dacb234da13b2e7875dbb
dendibakh <dendibakh@gmail.com>
2022-07-22 04:48:24 -0700

    [DepChains1] - Solution

--- labs/core_bound/dep_chains_1/solution.cpp
+++ labs/core_bound/dep_chains_1/solution.cpp
@@ -11,19 +11,45 @@ unsigned getSumOfDigits(unsigned n) {
   return sum;
 }
 
-// Task: lookup all the values from l2 in l1.
-// For every found value, find the sum of its digits.
-// Return the sum of all digits in every found number.
-// Both lists have no duplicates and elements placed in *random* order.
-// Do NOT sort any of the lists. Do NOT store elements in a hash_map/sets.
-
-// Hint: Traversing a linked list is a long data dependency chain:
-//       to get the node N+1 you need to retrieve the node N first.
-//       Think how you can execute multiple dependency chains in parallel.
-unsigned solution(List *l1, List *l2) {
+template <int M> unsigned solution(List *l1, List *l2) {
   unsigned retVal = 0;
-
   List *head2 = l2;
+  List *head1 = l1;
+
+  int length1 = 0;
+  while (l1) {
+    length1++;
+    l1 = l1->next;
+  }
+
+  l1 = head1;
+
+  // Simultaneously lookup M elements in l1.
+  for (int i = 0; i < length1 / M; i++) {
+    std::array<unsigned, M> vals;
+    // remember M values from l1
+    for (int j = 0; j < M; j++) {
+      vals[j] = l1->value;
+      l1 = l1->next;
+    }
+    // traverse l2 and lookup M elements from vals at the same time
+    l2 = head2;
+    int found = 0;
+    while (l2) {
+      for (int j = 0; j < M; j++) {
+        if (l2->value == vals[j]) {
+          retVal += getSumOfDigits(l2->value);
+          // stop if all M values found
+          if (++found == M)
+            break;
+        }
+      }
+      l2 = l2->next;
+    }
+  }
+
+  // Process the remainder with sequential algorithm
+
   // O(N^2) algorithm:
   while (l1) {
     unsigned v = l1->value;
@@ -40,3 +66,5 @@ unsigned solution(List *l1, List *l2) {
 
   return retVal;
 }
+
+unsigned solution(List *l1, List *l2) { return solution<4>(l1, l2); }
