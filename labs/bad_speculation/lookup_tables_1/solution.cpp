b54a5020aed8e7a0f417cf1d7a0ad26eb3a3d0c0
dendibakh <dendibakh@gmail.com>
2022-07-22 04:52:46 -0700

    [LookupTables1] - Solution

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -1,14 +1,20 @@
 #include "solution.hpp"
 
-static std::size_t mapToBucket(std::size_t v) {    // diff
-  if (v >= 0 && v < 13)        return 0; // 13
-  else if (v >= 13 && v < 29)  return 1; // 16
-  else if (v >= 29 && v < 41)  return 2; // 12
-  else if (v >= 41 && v < 53)  return 3; // 12
-  else if (v >= 53 && v < 71)  return 4; // 18
-  else if (v >= 71 && v < 83)  return 5; // 12
-  else if (v >= 83 && v < 100) return 6; // 17
-  return -1;  // let it crash
+int buckets[100] = {
+// 1  2  3  4  5  6  7  8  9  0  1  2  3  4  5  6  7  8  9  0
+   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+   2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+   3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+   4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+   5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+   6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6
+};
+
+static std::size_t mapToBucket(std::size_t v) {
+  if (v < (sizeof (buckets) / sizeof (int)))
+    return buckets[v];
+  return -1; // let it crash
 }
 
 std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
