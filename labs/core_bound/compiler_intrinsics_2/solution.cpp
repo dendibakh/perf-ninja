4dd103cb3ce8c8fa306f875cfef911c7cef97901
Denis Bakhvalov <dendibakh@gmail.com>
2022-07-26 07:14:27 -0400

    [CompilerIntrinsics2] - Small improvement

--- labs/core_bound/compiler_intrinsics_2/solution.cpp
+++ labs/core_bound/compiler_intrinsics_2/solution.cpp
@@ -15,12 +15,11 @@ if (s == '\n') {
 unsigned solution(const std::string &inputContents) {
   unsigned longestLine = 0;
   unsigned curLineLength = 0;
+
   for (auto s : inputContents) {
-    longestLine = std::max(curLineLength, longestLine);
     curLineLength = (s == '\n') ? 0 : curLineLength + 1;
+    longestLine = std::max(curLineLength, longestLine);
   }
 
-  // if no end-of-line in the end
-  longestLine = std::max(curLineLength, longestLine);
   return longestLine;
 }
