510eb10fe79ddfdeb00ee3c9fb78553dee6cc42d
dendibakh <dendibakh@gmail.com>
2022-08-05 06:33:30 -0700

    [IO opt1] - Solution

--- labs/misc/io_opt1/solution.cpp
+++ labs/misc/io_opt1/solution.cpp
@@ -3,22 +3,18 @@
 #include <fstream>
 #include <stdexcept>
 
+#include "MappedFile.hpp"
 uint32_t solution(const char *file_name) {
-  std::fstream file_stream{file_name};
-  if (!file_stream.is_open())
-    throw std::runtime_error{"The file could not be opened"};
+  // Map file contents into the process address space
+  MappedFile file{file_name};
+  const auto data = file.getContents();
 
   // Initial value has all bits set to 1
   uint32_t crc = 0xff'ff'ff'ff;
 
   // Update the CRC32 value character by character
-  char c;
-  while (true) {
-    file_stream.read(&c, 1);
-    if (file_stream.eof())
-      break;
+  for (char c : data)
     update_crc32(crc, static_cast<uint8_t>(c));
-  }
 
   // Invert the bits
   crc ^= 0xff'ff'ff'ff;
