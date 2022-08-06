645c8f8d4645eb242a91192c693408c7e871cc6a
Alex Popov <alexgpg@gmail.com>
2022-08-06 23:44:54 +0300

    data packing: Add solution (win)

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -1,21 +1,32 @@
 
 #include <array>
+#include <cstdint>
 
 // Assume those constants never change
 constexpr int N = 10000;
 constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
-// FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
-  bool b;
+  float d;
 
+  /**
+  float d;
+  
+  uint32_t l : 16;
+  uint32_t i : 8;
+  uint32_t s : 7;
+  uint32_t b : 1;
+  **/
+  unsigned i : 8;
+  unsigned s : 8;
+  unsigned b : 2;
+  unsigned l : 14; 
+  
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+
+static_assert(sizeof(S) == 8, "size mismatch");
 
 void init(std::array<S, N> &arr);
 S create_entry(int first_value, int second_value);
