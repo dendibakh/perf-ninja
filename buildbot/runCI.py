d9c9acd9382b9fd0e48db830d3d3e819fa658a49
dendibakh <dendibakh@gmail.com>
2022-09-27 11:16:54 -0400

    Enabled MacosM1 runner

--- buildbot/runCI.py
+++ buildbot/runCI.py
@@ -41,7 +41,7 @@ class LabPath:
 
 parser = argparse.ArgumentParser(description='test results')
 parser.add_argument("-workdir", type=str, help="working directory", default="")
-parser.add_argument("-runner", type=str, help="CI runner machine", default="LinuxIntel")
+parser.add_argument("-runner", type=str, help="CI runner machine", default="LinuxIntelAlderlake")
 parser.add_argument("-v", help="verbose", action="store_true", default=False)
 
 args = parser.parse_args()
@@ -120,6 +120,27 @@ elif runner == "WinZen3":
   Labs["bad_speculation"]["conditional_store_1"] = LabParams(threshold=70.0)
   Labs["bad_speculation"]["lookup_tables_1"] = LabParams(threshold=80.0)
   Labs["bad_speculation"]["virtual_call_mispredict"] = LabParams(threshold=40.0)
+elif runner == "MacosM1":
+  Labs["memory_bound"]["data_packing"] = LabParams(threshold=13.0)
+  Labs["memory_bound"]["false_sharing_1"] = LabParams(threshold=70.0)
+  Labs["memory_bound"]["huge_pages_1"] = LabParams(threshold=10.0)
+  Labs["memory_bound"]["loop_interchange_1"] = LabParams(threshold=85.0)
+  Labs["memory_bound"]["loop_interchange_2"] = LabParams(threshold=75.0)
+  Labs["memory_bound"]["loop_tiling_1"] = LabParams(threshold=35.0)
+  Labs["memory_bound"]["swmem_prefetch_1"] = LabParams(threshold=60.0)
+  Labs["misc"]["warmup"] = LabParams(threshold=50.0)
+  Labs["misc"]["lto"] = LabParams(threshold=20.0)
+  Labs["misc"]["pgo"] = LabParams(threshold=10.0)
+  Labs["misc"]["io_opt1"] = LabParams(threshold=70.0)
+  Labs["core_bound"]["function_inlining_1"] = LabParams(threshold=25.0)
+  Labs["core_bound"]["compiler_intrinsics_1"] = LabParams(threshold=60.0)
+  Labs["core_bound"]["compiler_intrinsics_2"] = LabParams(threshold=65.0)
+  Labs["core_bound"]["dep_chains_1"] = LabParams(threshold=60.0)
+  Labs["core_bound"]["vectorization_1"] = LabParams(threshold=80.0)
+  Labs["core_bound"]["vectorization_2"] = LabParams(threshold=85.0)
+  Labs["bad_speculation"]["conditional_store_1"] = LabParams(threshold=70.0)
+  Labs["bad_speculation"]["lookup_tables_1"] = LabParams(threshold=80.0)
+  Labs["bad_speculation"]["virtual_call_mispredict"] = LabParams(threshold=60.0)
 
 def getLabCurrentStatus(labPath):
   return Labs[labPath.category][labPath.name].result
