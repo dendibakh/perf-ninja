8f266b090aef33883dd74baf103ee62307ce8101
Fredrik Lindblom <flindblo@gmail.com>
2022-10-22 09:23:37 +0200

    Add data packing lab solution

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -7,16 +7,19 @@ constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
-struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
-  bool b;
 
-  bool operator<(const S &s) const { return this->i < s.i; }
+struct S {
+    float d;
+    unsigned long long l : 16;
+    unsigned int i: 8;
+    unsigned short s: 7;
+    bool b : 1;
+
+    bool operator<(const S &s) const { return this->i < s.i; }
 };
 
 void init(std::array<S, N> &arr);
+
 S create_entry(int first_value, int second_value);
+
 void solution(std::array<S, N> &arr);
