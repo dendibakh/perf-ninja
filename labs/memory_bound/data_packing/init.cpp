f43894ea8dcc9b814b495ef7554edd9a4d0fdce2
siddmo <msmajeed9@gmail.com>
2022-11-01 14:38:56 +0100

    Adds data packing solution

--- labs/memory_bound/data_packing/init.cpp
+++ labs/memory_bound/data_packing/init.cpp
@@ -5,10 +5,11 @@
 S create_entry(int first_value, int second_value) {
   S entry;
 
-  entry.i = first_value;
+  entry.i = static_cast<short>(first_value);
   entry.s = static_cast<short>(second_value);
-  entry.l = static_cast<long long>(first_value * second_value);
-  entry.d = static_cast<double>(first_value) / maxRandom;
+  
+  entry.l = static_cast<int>(first_value * second_value);
+  entry.d = static_cast<float>(first_value) / maxRandom;
   entry.b = first_value < second_value;
 
   return entry;
--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -8,10 +8,10 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
+  short i;
+  int l;
   short s;
-  double d;
+  float d;
   bool b;
 
   bool operator<(const S &s) const { return this->i < s.i; }
