2e3339e297a96c573b1a71ad1952187ec292c324
Cosmin <38663153+Cosmin-B@users.noreply.github.com>
2022-12-11 16:22:22 +0000

    Optimize branch predictions by using a bucket lookup table with precalculated values using limits

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -1,14 +1,41 @@
 #include "solution.hpp"
 
-static std::size_t mapToBucket(std::size_t v) {    // diff
-  if (v >= 0 && v < 13)        return 0; // 13
-  else if (v >= 13 && v < 29)  return 1; // 16
-  else if (v >= 29 && v < 41)  return 2; // 12
-  else if (v >= 41 && v < 53)  return 3; // 12
-  else if (v >= 53 && v < 71)  return 4; // 18
-  else if (v >= 71 && v < 83)  return 5; // 12
-  else if (v >= 83 && v < 100) return 6; // 17
-  return -1;  // let it crash
+// This table is used to look up the bucket number for a given
+// value. It is used by the lookup table to quickly find the
+// bucket where a given key is stored.
+//
+// The BUCKET_LOOKUP array is used to determine the initial
+// bucket to search.  The range value is used as an index into
+// the BUCKET_LOOKUP array. The value at that index is the
+// bucket number to search.
+//
+// The BUCKET_LOOKUP array is ordered by hash value.  The
+// values in the array are the bucket numbers.  The index value
+// will be between 0 and 99.  The BUCKET_LOOKUP array is
+// indexed by hash value.
+// This will remove most of the branches from the code. And tremendously help with performance
+
+static constexpr std::size_t MAX_RANGE = 100;
+static constexpr std::size_t BUCKET_LOOKUP[MAX_RANGE] = {
+  // Map limits to bucket indices
+//  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5  6  7  8
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0 - 12
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 13 - 28
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,      // 29 - 40
+    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,      // 41 - 52
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,   // 53 - 70
+    5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,           // 71 - 82
+    6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6    // 83 - 99
+};
+
+static std::size_t mapToBucket(std::size_t v) {
+  // Calculate the bucket index for the value v
+  // but make sure that v is in the range [0, 99]
+  // and return -1 if it is not.
+  if(v < (sizeof(BUCKET_LOOKUP)/sizeof(BUCKET_LOOKUP[0])))
+      return BUCKET_LOOKUP[v];
+  else
+      return -1;
 }
 
 std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
