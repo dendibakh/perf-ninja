42b8bc450d9774f57a6aec3a4c671bf334913462
Cosmin-B <38663153+Cosmin-B@users.noreply.github.com>
2022-12-14 21:27:12 +0000

    Optimize file IO by mapping the file to memory, for io_opt1 lab

--- labs/misc/io_opt1/solution.cpp
+++ labs/misc/io_opt1/solution.cpp
@@ -1,24 +1,21 @@
 #include "solution.hpp"
-
+#include "MappedFile.hpp"
 #include <fstream>
+#include <algorithm>
 #include <stdexcept>
 
 uint32_t solution(const char *file_name) {
-  std::fstream file_stream{file_name};
-  if (not file_stream.is_open())
-    throw std::runtime_error{"The file could not be opened"};
+  // Map the file into memory
+  MappedFile file{file_name};
 
   // Initial value has all bits set to 1
   uint32_t crc = 0xff'ff'ff'ff;
 
-  // Update the CRC32 value character by character
-  char c;
-  while (true) {
-    file_stream.read(&c, 1);
-    if (file_stream.eof())
-      break;
+  // Update the CRC32 value with an algorithm
+  const auto& str = file.getContents();
+  std::for_each(str.begin(), str.end(), [&crc](char c){
     update_crc32(crc, static_cast<uint8_t>(c));
-  }
+  });
 
   // Invert the bits
   crc ^= 0xff'ff'ff'ff;
