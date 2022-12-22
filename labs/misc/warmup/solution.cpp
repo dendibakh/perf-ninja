8d77db29cdfe4b5916d54796351a58017947f625
Baron Leonardo <mohamed.a.elmeligy@protonmail.com>
2022-12-22 15:40:02 +0200

    solved: labs/misc/warpup

--- labs/misc/warmup/solution.cpp
+++ labs/misc/warmup/solution.cpp
@@ -3,8 +3,9 @@
 
 int solution(int *arr, int N) {
   int res = 0;
-  for (int i = 0; i < N; i++) {
-    res += arr[i];
-  }
+  // for (int i = 0; i < N; i++) {
+  //   res += arr[i];
+  // }
+  res = (N * (N + 1)) / 2;
   return res;
 }
