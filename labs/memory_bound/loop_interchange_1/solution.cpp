6d5bfb0d3b9c05ce6dce70938df2546ebcb9d778
John Ziamos <iziamos@gmail.com>
2023-01-01 11:58:19 +0000

    loop interchange 1

--- labs/memory_bound/loop_interchange_1/solution.cpp
+++ labs/memory_bound/loop_interchange_1/solution.cpp
@@ -29,7 +29,7 @@ void multiply(Matrix &result, const Matrix &a, const Matrix &b) {
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
       for (int k = 0; k < N; k++) {
-        result[i][j] += a[i][k] * b[k][j];
+        result[j][k] += a[j][i] * b[i][k];
       }
     }
   }
