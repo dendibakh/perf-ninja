4e4a634e5811a97bcc73c0dec603930c8de3bca7
Joseph Zhang <jzhang25@seas.upenn.edu>
2023-01-08 03:06:18 +0000

    Data packing second try

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -8,11 +8,11 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  double d;
-  long long l;
-  int i;
-  short s;
-  bool b;
+  float d;
+  long long l : 16;
+  unsigned int i : 7;
+  unsigned short s : 7;
+  bool b : 1;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
