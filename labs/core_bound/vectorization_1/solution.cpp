1ff7fe0ef70bc8bfb1ba4fcb7330ae3a04959f6f
Maksim Dzabraev <dzabraew@gmail.com>
2023-02-16 18:19:06 +0300

    [CheckAll] solution task:vectorization_1 attempt:1

--- labs/core_bound/vectorization_1/solution.cpp
+++ labs/core_bound/vectorization_1/solution.cpp
@@ -6,7 +6,83 @@
 // The alignment algorithm which computes the alignment of the given sequence
 // pairs.
 result_t compute_alignment(std::vector<sequence_t> const &sequences1,
-                           std::vector<sequence_t> const &sequences2) {
+                           std::vector<sequence_t> const &sequences2)
+{
+#ifdef SOLUTION
+  result_t result{};
+  constexpr int batch_size_v = 8;
+  using score_t = int16_t;
+  using column_t = std::array<score_t, sequence_size_v + 1>;
+  constexpr score_t gap_open{-11};
+  constexpr score_t gap_extension{-1};
+  constexpr score_t match{6};
+  constexpr score_t mismatch{-4};
+  assert(sequences1.size() % batch_size_v == 0);
+
+  for (size_t sequence_idx = 0; sequence_idx < sequences1.size();
+       sequence_idx += batch_size_v) {
+    std::array<column_t, batch_size_v> score_column{};
+    std::array<column_t, batch_size_v> horizontal_gap_column{};
+    std::array<score_t,  batch_size_v> last_vertical_gap{};
+
+    /*
+     * Initialise the first column of the matrix.
+     */
+    for (size_t b{}; b < batch_size_v; ++b) {
+      horizontal_gap_column[b][0] = gap_open;
+      last_vertical_gap[b] = gap_open;
+      for (size_t i = 1; i < sequence_size_v + 1; ++i) {
+        score_column[b][i] = last_vertical_gap[b];
+        horizontal_gap_column[b][i] = last_vertical_gap[b] + gap_open;
+        last_vertical_gap[b] += gap_extension;
+      }
+    }
+
+    /*
+     * Compute the main recursion to fill the matrix.
+     */
+    for (unsigned col = 1; col <= sequence_size_v; ++col) {
+      std::array<score_t, batch_size_v> last_diagonal_score;
+      for (size_t b{}; b < batch_size_v; ++b) {
+        last_diagonal_score[b] = score_column[b][0];
+        score_column[b][0] = horizontal_gap_column[b][0];
+        last_vertical_gap[b] = horizontal_gap_column[b][0] + gap_open;
+        horizontal_gap_column[b][0] += gap_extension;
+      }
+
+      for (unsigned row = 1; row <= sequence_size_v; ++row) {
+        // Compute next score from diagonal direction with match/mismatch.
+        std::array<score_t, batch_size_v> best_cell_score;
+        #pragma clang loop vectorize(enable)
+        for (size_t b{}; b < batch_size_v; ++b) {
+          sequence_t const &sequence1 = sequences1[sequence_idx + b];
+          sequence_t const &sequence2 = sequences2[sequence_idx + b];
+          best_cell_score[b] = last_diagonal_score[b] +
+              (sequence1[row - 1] == sequence2[col - 1] ? match : mismatch);
+          // Determine best score from diagonal, vertical, or horizontal
+          // direction.
+          best_cell_score[b] = std::max(best_cell_score[b], last_vertical_gap[b]);
+          best_cell_score[b] = std::max(best_cell_score[b], horizontal_gap_column[b][row]);
+          // Cache next diagonal value and store optimum in score_column.
+          last_diagonal_score[b] = score_column[b][row];
+          score_column[b][row] = best_cell_score[b];
+          // Compute the next values for vertical and horizontal gap.
+          best_cell_score[b] += gap_open;
+          last_vertical_gap[b] += gap_extension;
+          horizontal_gap_column[b][row] += gap_extension;
+          // Store optimum between gap open and gap extension.
+          last_vertical_gap[b] = std::max(last_vertical_gap[b], best_cell_score[b]);
+          horizontal_gap_column[b][row] =
+              std::max(horizontal_gap_column[b][row], best_cell_score[b]);
+        }
+      }
+    }
+
+    // Report the best score.
+    for (size_t b{}; b < batch_size_v; ++b)
+      result[sequence_idx + b] = score_column[b].back();
+  }
+#else
   result_t result{};
 
   for (size_t sequence_idx = 0; sequence_idx < sequences1.size();
@@ -83,6 +159,6 @@ result_t compute_alignment(std::vector<sequence_t> const &sequences1,
     // Report the best score.
     result[sequence_idx] = score_column.back();
   }
-
+#endif
   return result;
 }
