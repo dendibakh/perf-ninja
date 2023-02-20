ee9c44fd63e26be11180044ff25e273f8842645b
Maksim Dzabraev <dzabraew@gmail.com>
2023-02-20 22:59:05 +0300

    official solution

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -24,15 +24,21 @@ uint16_t checksum(const Blob &blob) {
   return acc1;
 }
 #else // MYSOLUTION
-uint16_t checksum(const Blob &blob) {
+uint16_t checksum(const Blob& blob) {
   uint32_t acc = 0;
-  for (auto value : blob)
+  for (const auto value : blob) {
     acc += value;
+  }
+
   auto high = acc >> 16;
   auto low = acc & 0xFFFFu;
   acc = low + high;
 
-  return static_cast<uint16_t>(acc) + !!(acc >= 0xFFFu);
+  high = acc >> 16;
+  low = acc & 0xFFFFu;
+  acc = low + high;
+
+  return static_cast<uint16_t>(acc);
 }
 #endif // THEIRS
 #else
