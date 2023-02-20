b141cefc58cb1772322e38e4c614803a85c40f3b
Maksim Dzabraev <dzabraew@gmail.com>
2023-02-20 22:29:25 +0300

    vec2

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,5 +1,27 @@
 #include "solution.hpp"
 
+#define SOLUTION
+
+#ifdef SOLUTION
+uint16_t checksum(const Blob &blob) {
+  constexpr unsigned bs{64};
+  std::array<uint16_t, bs> acc{};
+  for (unsigned k{}; k < N; k += bs) {
+    auto *p = &blob[k];
+    for (unsigned m{}; m < bs; ++m) {
+      auto value = p[m];
+      acc[m] += value;
+      acc[m] += acc[m] < value;
+    }
+  }
+  uint16_t acc1{};
+  for (auto value : acc) {
+    acc1 += value;
+    acc1 += acc1 < value; // add carry
+  }
+  return acc1;
+}
+#else
 uint16_t checksum(const Blob &blob) {
   uint16_t acc = 0;
   for (auto value : blob) {
@@ -8,3 +30,4 @@ uint16_t checksum(const Blob &blob) {
   }
   return acc;
 }
+#endif
