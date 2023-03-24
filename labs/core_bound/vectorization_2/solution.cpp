a32b8b4d233aa9434b9033c75e337efa6e0b272f
TN <tnie@tuta.io>
2023-03-24 15:08:25 -0500

    vec2

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,10 +1,16 @@
 #include "solution.hpp"
 
 uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
-  for (auto value : blob) {
+  uint32_t acc{0};
+  for (auto value : blob)
     acc += value;
-    acc += acc < value; // add carry
-  }
-  return acc;
+
+  auto high = acc >> 16;
+  auto low = acc & 0xFFFFu;
+  acc = low + high;
+
+  high = acc >> 16;
+  low = acc & 0xFFFFu;
+  acc = low + high;
+  return static_cast<uint16_t>(acc);
 }
