2904344fee8d25d1ccdf0fb75472b0432f55aaa3
Ilknur <ilknuri607@gmail.com>
2023-04-03 14:37:46 -0400

    can't seem to fit short into 7 bits

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -9,13 +9,20 @@ constexpr int maxRandom = 100;
 // FIXME: this data structure can be reduced in size
 struct S {
   float d;
-  long long l:16;
+  int l:16;
   int i:8;
-  short s:7;
+  short s:8;
   bool b:1;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+
+
+#if 0
+  template <int N>
+  class TD;
+  TD<sizeof(S)> td;
+#endif
 
 void init(std::array<S, N> &arr);
 S create_entry(int first_value, int second_value);
