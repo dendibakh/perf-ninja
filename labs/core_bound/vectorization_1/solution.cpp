bb79fec888b7b286ad86cedd135f88d7726e8723
Yoni Hikri <yonihik@gmail.com>
2023-04-07 14:15:23 +0300

    best cell column opt

--- labs/core_bound/vectorization_1/solution.cpp
+++ labs/core_bound/vectorization_1/solution.cpp
@@ -51,11 +51,10 @@ result_t compute_alignment(std::vector<sequence_t> const &sequences1,
      * Compute the main recursion to fill the matrix.
      */
     for (unsigned col = 1; col <= sequence2.size(); ++col) {
-      column_t new_score_column = score_column;
-      new_score_column[0] = horizontal_gap_column[0];
+      column_t best_cell_column = score_column;
+      best_cell_column[0] = horizontal_gap_column[0];
       last_vertical_gap = horizontal_gap_column[0] + gap_open;
       horizontal_gap_column[0] += gap_extension;
-      column_t best_cell_column{};
 
       for (unsigned row = 1; row <= sequence1.size(); ++row) {
         // Compute next score from diagonal direction with match/mismatch.
@@ -63,15 +62,10 @@ result_t compute_alignment(std::vector<sequence_t> const &sequences1,
         best_cell_column[row] = std::max(best_cell_score, horizontal_gap_column[row]);
       }
       for (unsigned row = 1; row <= sequence1.size(); ++row) {
-        score_t best_cell_score = best_cell_column[row];
-        // Determine best score from diagonal, vertical, or horizontal
-        // direction.
-        best_cell_score = std::max(best_cell_score, last_vertical_gap);
-        // Cache next diagonal value and store optimum in score_column.
-        new_score_column[row] = best_cell_score;
-        last_vertical_gap = std::max(last_vertical_gap + gap_extension, best_cell_score + gap_open);
+        best_cell_column[row] = std::max(best_cell_column[row], last_vertical_gap);
+        last_vertical_gap = std::max(last_vertical_gap + gap_extension, best_cell_column[row] + gap_open);
       }
-      score_column = new_score_column;
+      score_column = best_cell_column;
         // Compute the next values for vertical and horizontal gap.
       for (unsigned row = 1; row <= sequence1.size(); ++row) {
         horizontal_gap_column[row] =
