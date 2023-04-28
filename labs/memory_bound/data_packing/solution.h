42ad81dd5ac5f37b49513b7ed941d1ffeab4e4bc
kocchop <kocchop@kocchops-MacBook-Air.local>
2023-04-28 19:51:14 -0400

    [CheckAll] Solved data packing

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -8,14 +8,26 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
-  bool b;
+  float d;
+  unsigned long long l : 16; // 14 is enough
+  unsigned int i : 8; // 7 is enough
+  unsigned short s : 7;
+  bool b : 1;
+
+  // int i;
+  // long long l;
+  // short s;
+  // double d;
+  // bool b;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+
+// template <int N> 
+// class TD;
+
+// never compiles but shows the size of S
+// TD<sizeof(S)> td;
 
 void init(std::array<S, N> &arr);
 S create_entry(int first_value, int second_value);
