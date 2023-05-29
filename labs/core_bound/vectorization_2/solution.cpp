3eb3405c212e2fa592b8b6750e51c08614b416e8
bushiwoshifeng <3474563973@qq.com>
2023-05-29 22:07:33 +0800

    vectorization_2

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,10 +1,18 @@
 #include "solution.hpp"
 
+using acc_t = std::array<uint16_t, 16>;
+
 uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
-  for (auto value : blob) {
-    acc += value;
-    acc += acc < value; // add carry
+  acc_t acc = {0};
+  for (std::size_t i = 0; i < N; i += 16) {
+    for (std::size_t j = 0; j < 16; j++) {
+      acc[j] += blob[i + j];
+      acc[j] += acc[j] < blob[i + j];  // add carry
+    }
   }
-  return acc;
+  for (std::size_t i = 1; i < 16; i++) {
+    acc[0] += acc[i];
+    acc[0] += acc[0] < acc[i];  // add carry
+  }
+  return acc[0];
 }
