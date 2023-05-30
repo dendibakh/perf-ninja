4b3bd96256d162990a9c1d025b64904ea354ac99
bushiwoshifeng <3474563973@qq.com>
2023-05-30 13:56:28 +0800

    function_inlining_1

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -23,5 +23,5 @@ static int compare(const void *lhs, const void *rhs) {
 }
 
 void solution(std::array<S, N> &arr) {
-  qsort(arr.data(), arr.size(), sizeof(S), compare);
+  std::sort(arr.begin(), arr.end(), [](const S& a, const S& b) {return (a.key1 < b.key1) || (a.key1 == b.key1 && a.key2 < b.key2);});
 }
