b6a84a245e8bfb2a74c694360d404e394e98ab66
bushiwoshifeng <3474563973@qq.com>
2023-05-31 14:34:44 +0800

    loop_tiling_1

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -1,11 +1,17 @@
 #include "solution.hpp"
 #include <algorithm>
 
+constexpr int BLKSZ = 200;
+
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
+  for (int i = 0; i < size; i += BLKSZ) {
+    for (int j = 0; j < size; j += BLKSZ) {
+      for (int i2 = i; i2 < std::min(i + BLKSZ, size); i2++) {
+        for (int j2 = j; j2 < std::min(j + BLKSZ, size); j2++) {
+          out[i2][j2] = in[j2][i2];
+        }
+      }
     }
   }
   return out[0][size - 1];
