eedf84d99309a19c4cd1c57d2b18731f0738288e
kocchop <fahim.faysal11@gmail.com>
2023-07-21 07:29:38 -0400

    solved warm up at last

--- labs/misc/warmup/solution.cpp
+++ labs/misc/warmup/solution.cpp
@@ -2,9 +2,13 @@
 #include "solution.h"
 
 int solution(int *arr, int N) {
-  int res = 0;
-  for (int i = 0; i < N; i++) {
-    res += arr[i];
-  }
-  return res;
+  return (N*(N+1))>>1;  
 }
+
+// int solution(int *arr, int N) {
+//   int res = 0;
+//   for (int i = 0; i < N; i++) {
+//     res += arr[i];
+//   }
+//   return res;
+// }
