3d0f6c4615f869a1b9dc1d9a46792e37288ea3e0
Martin Nilsson <ibbles@hotmail.com>
2023-09-04 20:34:00 +0200

    Solution for Function Inlining 1

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -3,7 +3,16 @@
 #include <algorithm>
 #include <stdlib.h>
 
-static int compare(const void *lhs, const void *rhs) {
+
+#ifndef SOLUTION
+#  define SOLUTION 1
+#endif
+
+#if SOLUTION == 0
+// Baseline.
+
+static int compare(const void *lhs, const void *rhs)
+{
   auto &a = *reinterpret_cast<const S *>(lhs);
   auto &b = *reinterpret_cast<const S *>(rhs);
 
@@ -25,3 +34,49 @@ static int compare(const void *lhs, const void *rhs) {
 void solution(std::array<S, N> &arr) {
   qsort(arr.data(), arr.size(), sizeof(S), compare);
 }
+
+#elif SOLUTION == 1
+// My solution.
+
+#include <algorithm>
+
+
+struct Compare_s
+{
+  bool operator()(const S& lhs, const S& rhs)
+  {
+    if (lhs.key1 < rhs.key1)
+      return true;
+
+    if (lhs.key1 > rhs.key1)
+      return false;
+
+    if (lhs.key2 < rhs.key2)
+      return true;
+
+    if (lhs.key2 > rhs.key2)
+      return false;
+
+    return false;
+  }
+};
+
+void solution(std::array<S, N>& array)
+{
+  std::sort(std::begin(array), std::end(array), Compare_s());
+}
+
+#elif SOLUTION == 2
+// Facit.
+
+void solution(std::array<S, N>& arr)
+{
+	std::sort(arr.begin(), arr.end(), [](S&a, S&b)
+	{
+		return a.key1 < b.key1 || (a.key1 == b.key1) && (a.key2 < b.key2);
+	});
+}
+
+#else
+#  pragma error("Unknown solution. Valid values are 0 through 2.")
+#endif
