0c2fcd7471f6d838cd064d6a3ae2878a25bff1d6
kevin leung <lzhkai1111@qq.com>
2023-09-20 23:42:52 +0800

    solve v1 lab

--- labs/misc/warmup/solution.cpp
+++ labs/misc/warmup/solution.cpp
@@ -2,10 +2,13 @@
 #include "solution.h"
 
 int solution(int *arr, int N) {
+#ifdef SOLUTION
   return N * (N+1) / 2;
-  //int res = 0;
-  //for (int i = 0; i < N; i++) {
-    //res += arr[i];
-  //}
-  //return res;
+#else
+  int res = 0;
+  for (int i = 0; i < N; i++) {
+    res += arr[i];
+  }
+  return res;
+#endif
 }
