cce4b38dded03cc408dbe59438f4a74e1ce1919b
dendibakh <dendibakh@gmail.com>
2023-09-22 22:28:30 -0400

    [New Lab] Loop Unrolling 1

---
+++ labs/core_bound/loop_unrolling_1/init.cpp
@@ -0,0 +1,22 @@
+#include "solution.hpp"
+#include <algorithm>
+#include <random>
+
+// Init random starting grid of the game
+Life::Grid initRandom() {
+  std::mt19937_64 random_engine{};
+  std::uniform_int_distribution<int> distrib(0, 9);
+
+  Life::Grid retGrid;
+  retGrid.resize(GridXDimension);
+
+  for (auto &row : retGrid) {
+    row.resize(GridYDimension);
+    std::generate(row.begin(), row.end(),[&]() { 
+      // 70% dead cells and 30% alive cells
+      return distrib(random_engine) > 6; 
+    });
+  }
+
+  return retGrid;
+}
