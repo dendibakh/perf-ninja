9b54c5df8c3218268efa512942d84a98e5f61ac2
Martin Nilsson <ibbles@hotmail.com>
2023-10-02 20:40:12 +0200

    First steps of compiler intrinsics 1, not done

--- labs/core_bound/compiler_intrinsics_1/solution.cpp
+++ labs/core_bound/compiler_intrinsics_1/solution.cpp
@@ -6,7 +6,7 @@ void imageSmoothing(const InputVector &input, uint8_t radius,
                     OutputVector &output) {
   int pos = 0;
   int currentSum = 0;
-  int size = static_cast<int>(input.size());
+  const int size = static_cast<int>(input.size());
 
   // 1. left border - time spend in this loop can be ignored, no need to
   // optimize it
@@ -21,6 +21,59 @@ void imageSmoothing(const InputVector &input, uint8_t radius,
   }
 
   // 2. main loop.
+  limit = size - radius;
+  for (; pos < limit; ++pos) {
+    currentSum -= input[pos - radius - 1];
+    currentSum += input[pos + radius];
+    output[pos] = currentSum;
+  }
+
+  // 3. special case, executed only if size <= 2*radius + 1
+  limit = std::min(radius + 1, size);
+  for (; pos < limit; pos++) {
+    output[pos] = currentSum;
+  }
+
+  // 4. right border - time spend in this loop can be ignored, no need to
+  // optimize it
+  for (; pos < size; ++pos) {
+    currentSum -= input[pos - radius - 1];
+    output[pos] = currentSum;
+  }
+}
+
+
+
+void imageSmoothing_blocked(const InputVector &input, uint8_t radius,
+                    OutputVector &output) {
+  int pos = 0;
+  int currentSum = 0;
+  const int size = static_cast<int>(input.size());
+
+  // 1. left border - time spend in this loop can be ignored, no need to
+  // optimize it
+  for (int i = 0; i < std::min<int>(size, radius); ++i) {
+    currentSum += input[i];
+  }
+
+  int limit = std::min(radius + 1, size - radius);
+  for (pos = 0; pos < limit; ++pos) {
+    currentSum += input[pos + radius];
+    output[pos] = currentSum;
+  }
+
+  // 2. main loop.
+  constexpr int width {8};
+  uint16_t currentSumW[width] {0.0};
+  // Populate initial sums.
+  for (int i = -radius; i <= radius; ++i)
+  {
+    for (int w = 0; w < width; ++w)
+    {
+      currentSumW[w] += input[pos + i + w];
+    }
+  }
+
   limit = size - radius;
   for (; pos < limit; ++pos) {
     currentSum -= input[pos - radius - 1];
