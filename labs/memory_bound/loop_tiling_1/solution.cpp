8446aac2df834ec5acdc08cfe1ef494e3e5c85ce
Martin Nilsson <ibbles@hotmail.com>
2023-10-09 20:21:12 +0200

    Attempt 2 for Loop Tiling 1, process slack elements in the main loops

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -24,39 +24,20 @@ bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
 
 bool solution(MatrixOfDoubles& in, MatrixOfDoubles& out)
 {
-  constexpr int TILE_SIZE {2};
+  constexpr int TILE_SIZE {16};
 
   int size = in.size();
-  for (int i_base = 0; i_base < size - TILE_SIZE; i_base += TILE_SIZE)
+  for (int i_base = 0; i_base < size; i_base += TILE_SIZE)
   {
-    for (int j_base = 0; j_base < size - TILE_SIZE; j_base += TILE_SIZE)
+    for (int j_base = 0; j_base < size; j_base += TILE_SIZE)
     {
-      for (int i_tile = 0; i_tile < TILE_SIZE; ++i_tile)
+      for (int i_tile = i_base; i_tile < std::min(i_base + TILE_SIZE, size); ++i_tile)
       {
-        int i = i_base + i_tile;
-        for (int j_tile = 0; j_tile < TILE_SIZE; ++j_tile)
+        for (int j_tile = j_base; j_tile < std::min(j_base + TILE_SIZE, size); ++j_tile)
         {
-          int j = j_base + j_tile;
-          out[i][j] = in[j][i];
+          out[i_tile][j_tile] = in[j_tile][i_tile];
         }
       }
-    }
-    for (int i_tile = 0; i_tile < TILE_SIZE; ++i_tile)
-    {
-      int i = i_base + i_tile;
-      for (int j = (size / TILE_SIZE) * TILE_SIZE; j < size; ++j)
-      {
-        out[i][j] = in[j][i];
-      }
-    }
-  }
-  for (int i = (size / TILE_SIZE) * TILE_SIZE; i < size; ++i)
-  {
-    for (int j = 0; j < size; ++j)
-    {
-      double in_v = in[j][i];
-      double& out_v = out[i][j];
-      out_v = in_v;
     }
   }
   return out[0][size - 1];
@@ -65,14 +46,19 @@ bool solution(MatrixOfDoubles& in, MatrixOfDoubles& out)
 #elif SOLUTION == 2
 // Facit.
 
-// TODO Still using baseline code.
-bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
+bool solution(MatrixOfDoubles& in, MatrixOfDoubles& out) {
+  static constexpr int TILE_SIZE = 16;
   int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
+  for (int ii = 0; ii < size; ii+= TILE_SIZE) {
+    for (int jj = 0; jj < size; jj+= TILE_SIZE) {
+      for (int i = ii; i < std::min(ii + TILE_SIZE, size); i++) {
+        for (int j = jj; j < std::min(jj + TILE_SIZE, size); j++) {
+          out[i][j] = in[j][i];
+        }
+      }
     }
   }
+
   return out[0][size - 1];
 }
 
