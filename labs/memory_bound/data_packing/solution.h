dc5c48802b304c9e8700188d66acd9d8d3393a96
Anton Krasnokutskiy <antonreborn777@gmail.com>
2023-10-12 14:26:12 -0700

    warmup & data packing.

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -8,14 +8,15 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  int i;
   long long l;
+  float d;
+  int i;
   short s;
-  double d;
   bool b;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+
 
 void init(std::array<S, N> &arr);
 S create_entry(int first_value, int second_value);
