685a2bd071f4ad5aab4f141670fbaee21f295375
Kacper Marchwant <kacper.marchwant@gmail.com>
2023-11-26 12:42:13 +0000

    remove unsigned

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -8,10 +8,10 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-    float d;                    // 4 max 1
-    unsigned long long l : 15;  // 2 max 10k
-    unsigned int i : 7;         // 1 max 100
-    unsigned short s : 7;       // 1 max 100
+    float d;           // 4 max 1
+    long long l : 15;  // 2 max 10k
+    int i : 7;         // 1 max 100
+    short s : 7;       // 1 max 100
     bool b : 1;
 
     bool operator<(const S &s) const { return this->i < s.i; }
