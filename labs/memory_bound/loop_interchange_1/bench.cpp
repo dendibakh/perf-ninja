720c2d0509c017618d2f4393bb9da01c74cef2ce
Kacper Marchwant <kacper.marchwant@gmail.com>
2023-11-26 14:02:18 +0000

    Lab: loop_interchange_1

--- labs/memory_bound/loop_interchange_1/bench.cpp
+++ labs/memory_bound/loop_interchange_1/bench.cpp
@@ -1,23 +1,24 @@
 
-#include "benchmark/benchmark.h"
-#include "solution.h"
 #include <memory>
 
+#include "benchmark/benchmark.h"
+#include "solution.h"
+
 static void bench1(benchmark::State &state) {
-  std::unique_ptr<Matrix> a(new Matrix());
-  init(*a);
+    std::unique_ptr<Matrix> a(new Matrix());
+    init(*a);
 
-  std::unique_ptr<Matrix> b(new Matrix());
-  zero(*b);
+    std::unique_ptr<Matrix> b(new Matrix());
+    zero(*b);
 
-  for (auto _ : state) {
-    *b = power(*a, 2021);
-    benchmark::DoNotOptimize(b);
-  }
+    for (auto _ : state) {
+        *b = power(*a, 2021);
+        benchmark::DoNotOptimize(b);
+    }
 }
 
 // Register the function as a benchmark
-BENCHMARK(bench1);
+BENCHMARK(bench1)->Iterations(10);
 
 // Run the benchmark
 BENCHMARK_MAIN();
