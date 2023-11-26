90b6cf05c07c8f9c576520551ec87217b65abc87
Kacper Marchwant <kacper.marchwant@gmail.com>
2023-11-26 12:40:39 +0000

    Decrease bitfields

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -9,13 +9,18 @@ constexpr int maxRandom = 100;
 // FIXME: this data structure can be reduced in size
 struct S {
     float d;                    // 4 max 1
-    unsigned long long l : 16;  // 2 max 10k
-    unsigned int i : 8;         // 1 max 100
+    unsigned long long l : 15;  // 2 max 10k
+    unsigned int i : 7;         // 1 max 100
     unsigned short s : 7;       // 1 max 100
     bool b : 1;
 
     bool operator<(const S &s) const { return this->i < s.i; }
 };
+
+template <int N>
+class TD;
+
+// TD<sizeof(S)> td;
 
 void init(std::array<S, N> &arr);
 S create_entry(int first_value, int second_value);
