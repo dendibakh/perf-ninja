b48aed82296eefc43fa7a125f11dbd58e6173cd9
Andi Qu <andiqu@mit.edu>
2024-01-01 19:00:23 +0200

    Implement data packing lab

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -8,14 +8,19 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
-  bool b;
+  float d;
+  uint16_t l;
+  uint8_t i;
+  uint8_t s : 7;
+  bool b : 1;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+
+template <int N>
+class TD;
+
+// TD<sizeof(S)> td;
 
 void init(std::array<S, N> &arr);
 S create_entry(int first_value, int second_value);
