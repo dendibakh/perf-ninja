4f931c7215e45ea4d35caf2d72d83346834f063f
Andi Qu <andiqu@mit.edu>
2024-01-03 11:55:14 +0200

    Make bucketing inline

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -1,15 +1,9 @@
 #include "solution.hpp"
 
-static std::size_t mapToBucket(std::size_t v) {    // diff
-  if (v >= 0 && v < 100)
-    return (v >= 13) + (v >= 29) + (v >= 41) + (v >= 53) + (v >= 71) + (v >= 83);
-  return -1;  // let it crash
-}
-
 std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
   std::array<std::size_t, NUM_BUCKETS> retBuckets{0};
   for (auto v : values) {
-    retBuckets[mapToBucket(v)]++;
+    retBuckets[(v >= 13) + (v >= 29) + (v >= 41) + (v >= 53) + (v >= 71) + (v >= 83)]++;
   }
   return retBuckets;
 }
