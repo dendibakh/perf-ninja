1b383d12fa5089cf2acecbcf488e2aa6c057adf7
Andi Qu <andiqu@mit.edu>
2024-01-03 11:41:38 +0200

    Implement 2D tiling

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -1,14 +1,15 @@
 #include "solution.hpp"
 #include <algorithm>
 
-const int TILE_SIZE = 128;
-
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
+  static constexpr int TILE_SIZE = 32;
   int size = in.size();
-  for (int k = 0; k < size; k += TILE_SIZE) {
-    for (int i = 0; i < size; i++) {
-      for (int j = k; j < size && j < k + TILE_SIZE; j++) {
-        out[i][j] = in[j][i];
+  for (int ii = 0; ii < size; ii += TILE_SIZE) {
+    for (int jj = 0; jj < size; jj += TILE_SIZE) {
+      for (int i = ii; i < std::min(size, ii + TILE_SIZE); i++) {
+        for (int j = jj; j < std::min(size, jj + TILE_SIZE); j++) {
+          out[i][j] = in[j][i];
+        }
       }
     }
   }
