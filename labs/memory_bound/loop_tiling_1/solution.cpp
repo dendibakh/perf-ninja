ebb8536e1cc6c0a42e98ac28e979eb74e6ad797e
Andi Qu <andiqu@mit.edu>
2024-01-03 11:37:32 +0200

    [CheckAll] change tile size

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -6,7 +6,6 @@ const int TILE_SIZE = 128;
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
   for (int k = 0; k < size; k += TILE_SIZE) {
-    int lim = std::min(size, k + TILE_SIZE);
     for (int i = 0; i < size; i++) {
       for (int j = k; j < size && j < k + TILE_SIZE; j++) {
         out[i][j] = in[j][i];
