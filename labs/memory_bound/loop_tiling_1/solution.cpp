440c228ae68e3e00849a7e6c68f17ded9c32ec76
Andi Qu <andiqu@mit.edu>
2024-01-03 11:33:51 +0200

    Implement loop tiling 1

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -1,11 +1,16 @@
 #include "solution.hpp"
 #include <algorithm>
 
+const int TILE_SIZE = 256;
+
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
+  for (int k = 0; k < size; k += TILE_SIZE) {
+    int lim = std::min(size, k + TILE_SIZE);
+    for (int i = 0; i < size; i++) {
+      for (int j = k; j < lim; j++) {
+        out[i][j] = in[j][i];
+      }
     }
   }
   return out[0][size - 1];
