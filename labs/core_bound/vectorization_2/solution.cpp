2cf639a295ca616c5f2905288747ecf4b18ba45b
niccolot <nico_tiezzi98@hotmail.it>
2024-01-05 12:14:21 +0100

    vect lab 2

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,11 +1,11 @@
 #include "solution.hpp"
 
 uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
-  uint16_t temp = 0;
+  uint32_t acc = 0;
+
   for (auto value : blob) {
     acc += value;
-    temp += (acc < value); // add carry
   }
-  return acc+temp;
+  acc<<16;
+  return static_cast<uint16_t>(acc);
 }
