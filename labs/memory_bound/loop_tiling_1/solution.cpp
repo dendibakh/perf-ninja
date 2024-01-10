74b92a41d2995478724c2dffe74adb2581ecb029
niccolot <nico_tiezzi98@hotmail.it>
2024-01-10 18:11:14 +0100

    labs/memory_bound/loop_tiling_1/solution.cpp

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -3,9 +3,14 @@
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
+  int blockSize = 8;
+  for (int i = 0; i < size; i+=blockSize) {
+    for (int j = 0; j < size; j+=blockSize) {
+      for(int x=i; x<i+blockSize; ++x){
+        for(int y=j; y<j+blockSize; ++y){
+          out[x][y] = in[y][x];
+        }
+      }
     }
   }
   return out[0][size - 1];
