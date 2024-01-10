55d9c1ce78d71b652eaddbc016ae9a166896fde2
niccolot <nico_tiezzi98@hotmail.it>
2024-01-10 18:16:25 +0100

    tiling lab

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -3,7 +3,7 @@
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  int blockSize = 8;
+  int blockSize = 16;
   for (int i = 0; i < size; i+=blockSize) {
     for (int j = 0; j < size; j+=blockSize) {
       for(int x=i; x<std::min(i+blockSize, size); ++x){
