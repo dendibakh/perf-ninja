cc8d160ed76fb17e73a82dc38ce9e23679dcb026
niccolot <nico_tiezzi98@hotmail.it>
2024-01-11 09:51:46 +0100

    tiling lab

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -1,6 +1,7 @@
 #include "solution.hpp"
 #include <algorithm>
 
+#ifdef SOLUTION
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
   int blockSize = 16;
@@ -15,3 +16,14 @@ bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   }
   return out[0][size - 1];
 }
+#else
+bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
+  int size = in.size();
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      out[i][j] = in[j][i];
+    }
+  }
+  return out[0][size - 1];
+}
+#endif
