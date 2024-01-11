e3a407b4fdceb9fce227e38a3b232b6669ccb510
niccolot <nico_tiezzi98@hotmail.it>
2024-01-11 14:49:12 +0100

    sw prefetch lab

--- labs/memory_bound/swmem_prefetch_1/solution.cpp
+++ labs/memory_bound/swmem_prefetch_1/solution.cpp
@@ -10,11 +10,29 @@ static int getSumOfDigits(int n) {
 }
 
 int solution(const hash_map_t *hash_map, const std::vector<int> &lookups) {
+  static constexpr int UNUSED = std::numeric_limits<int>::max();
   int result = 0;
+  std::size_t nBuckets = hash_map->get_nBuckets();
+  std::vector<int> v = hash_map->get_vector();
+  int look_ahead = 16;
+  auto size = lookups.size();
+  int steps = 0;
 
-  for (int val : lookups) {
-    if (hash_map->find(val))
-      result += getSumOfDigits(val);
+  for (int i=0; i<size; ++i) {
+    
+    if(steps<look_ahead){
+      for(int k=0; k<look_ahead; ++k){
+        auto idx = lookups[i+k]%nBuckets;
+      __builtin_prefetch (&v[idx], 0, 1);
+      ++steps;
+      }
+    }
+    else{
+      steps=0;
+    }
+
+    if (v[lookups[i]%nBuckets] != UNUSED)
+      result += getSumOfDigits(lookups[i]);
   }
 
   return result;
