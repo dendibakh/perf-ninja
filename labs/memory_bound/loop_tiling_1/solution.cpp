4804a7a9fa217e1692614e541434e3ad467e6daa
niccolot <nico_tiezzi98@hotmail.it>
2024-01-11 10:11:23 +0100

    loop tiling 64 blocksize

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -1,10 +1,10 @@
 #include "solution.hpp"
 #include <algorithm>
 
-#ifdef SOLUTION
+
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  int blockSize = 16;
+  int blockSize = 64;
   for (int i = 0; i < size; i+=blockSize) {
     for (int j = 0; j < size; j+=blockSize) {
       for(int x=i; x<std::min(i+blockSize, size); ++x){
@@ -16,14 +16,4 @@ bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   }
   return out[0][size - 1];
 }
-#else
-bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
-  int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
-    }
-  }
-  return out[0][size - 1];
-}
-#endif
+
