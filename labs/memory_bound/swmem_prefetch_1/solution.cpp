4e2475a222b2c7bcad53c130c0a06acfff9079a4
niccolot <nico_tiezzi98@hotmail.it>
2024-01-11 15:36:31 +0100

    sw prefetch lab baseline

--- labs/memory_bound/swmem_prefetch_1/solution.cpp
+++ labs/memory_bound/swmem_prefetch_1/solution.cpp
@@ -10,23 +10,11 @@ static int getSumOfDigits(int n) {
 }
 
 int solution(const hash_map_t *hash_map, const std::vector<int> &lookups) {
-  
   int result = 0;
-  int look_ahead = 2;
-  auto size = lookups.size();
 
-  for (int i=0; i<size-look_ahead; ++i) {
-
-    if (hash_map->find(lookups[i])){
-      result += getSumOfDigits(lookups[i]);
-    }
-    hash_map->prefetch(lookups[i+look_ahead]);
-      
-  }
-
-  for(int i=size-look_ahead; i<size; ++i){
-    if (hash_map->find(lookups[i]))
-      result += getSumOfDigits(lookups[i]);
+  for (int val : lookups) {
+    if (hash_map->find(val))
+      result += getSumOfDigits(val);
   }
 
   return result;
