58f2b2fd33a33d324186259ea1f5cd1c783020c0
Anton Krasnokutskiy <antonreborn777@gmail.com>
2024-01-26 00:10:31 -0800

    test with no tuples

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -4,20 +4,7 @@
 #include <tuple>
 
 bool less(const S& a, const S& b) {
-//    return std::make_tuple(a.key1, a.key2) < std::make_tuple(b.key1, b.key2);
-    if (a.key1 < b.key1)
-        return true;
-
-    if (a.key1 > b.key1)
-        return false;
-
-    if (a.key2 < b.key2)
-        return true;
-
-    if (a.key2 > b.key2)
-        return false;
-
-    return false;
+    return std::make_tuple(a.key1, a.key2) < std::make_tuple(b.key1, b.key2);
 }
 
 void solution(std::array<S, N> &arr) {
