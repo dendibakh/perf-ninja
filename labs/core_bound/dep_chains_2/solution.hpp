ae2ee0dabf4703ef0ef721c3909d3928ecc36636
XCemaXX <xcemaxx@gmail.com>
2024-01-27 14:46:53 +0300

    dep_chains_2

--- labs/core_bound/dep_chains_2/solution.hpp
+++ labs/core_bound/dep_chains_2/solution.hpp
@@ -20,7 +20,7 @@ std::vector<Particle> initParticles();
 // Medium-quality random number generator.
 // https://www.javamex.com/tutorials/random_numbers/xorshift.shtml
 struct XorShift32 {
-  uint32_t val;
+    uint32_t val;
   XorShift32 (uint32_t seed) : val(seed) {}
 public:
   uint32_t gen() {
@@ -29,7 +29,7 @@ public:
     val ^= (val << 5);
     return val;
   }
-};
+  };
 
 constexpr double PI_D = 3.141592653589793238463;
 constexpr float PI_F = 3.14159265358979f;
@@ -50,17 +50,30 @@ static float cosine(float x) {
 // We do calculations in double precision then convert to float.
 constexpr float DEGREE_TO_RADIAN = (2 * PI_D) / UINT32_MAX;
 
+inline void recal_coords(uint32_t angle, Particle& p) {
+  float angle_rad = angle * DEGREE_TO_RADIAN;
+  p.x += cosine(angle_rad) * p.velocity;
+  p.y += sine(angle_rad) * p.velocity;
+}
+
 // Simulate the motion of the particles.
 // For every particle, we generate a random angle and move the particle
 // in the corresponding direction.
 template <class RNG>
 void randomParticleMotion(std::vector<Particle> &particles, uint32_t seed) {
-  RNG rng(seed);  
-  for (int i = 0; i < STEPS; i++)
-    for (auto &p : particles) {
-      uint32_t angle = rng.gen();
-      float angle_rad = angle * DEGREE_TO_RADIAN;
-      p.x += cosine(angle_rad) * p.velocity;
-      p.y += sine(angle_rad) * p.velocity;
+  constexpr uint32_t UNROLL_SIZE = 18;
+  std::vector<RNG> rngs(UNROLL_SIZE + 1, RNG(seed));
+  // for different seed also working
+  //std::vector<RNG> rngs{RNG(seed), RNG(seed+1), RNG(seed+2), RNG(seed+3), RNG(seed+4), RNG(seed+5), RNG(seed+6)};
+  for (int i = 0; i < STEPS; i++) {
+    int j = 0;
+    for(; j + UNROLL_SIZE <= particles.size(); j+= UNROLL_SIZE) {
+      for(int k = 0; k < UNROLL_SIZE; ++k) {
+        recal_coords(rngs[k].gen(), particles[j+k]);
+      }
     }
+    for(; j < particles.size(); ++j) {
+      recal_coords(rngs[UNROLL_SIZE].gen(), particles[j]);
+    }
+  }
 }
