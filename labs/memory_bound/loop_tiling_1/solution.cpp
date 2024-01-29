9f1e2c154bb68d80faaae755faa204dda7857811
Anton Krasnokutskiy <antonreborn777@gmail.com>
2024-01-28 19:05:20 -0800

    tiling

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -3,10 +3,19 @@
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
-    }
+  const int N = 5;
+  const auto Batch = size / N;
+  for (int ii = 0; ii < N; ii++) {
+      for (int jj = 0; jj < N; jj++) {
+          int di = Batch * ii;
+          int dj = Batch * jj;
+          for (int i = di; i < std::min(di + Batch, size); i++) {
+              for (int j = dj; j < std::min(dj + Batch, size); j++) {
+                  out[i][j] = in[j][i];
+              }
+          }
+      }
   }
+
   return out[0][size - 1];
 }
