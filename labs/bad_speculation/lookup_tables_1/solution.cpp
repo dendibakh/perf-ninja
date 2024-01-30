ee9b097bca72bb67169db7b2a5aee0018d81b373
Anton Krasnokutskiy <antonreborn777@gmail.com>
2024-01-29 17:06:34 -0800

    lookups

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -1,20 +1,27 @@
 #include "solution.hpp"
 
+std::array<int, 100> Lookup = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+        3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+        4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+        5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+        6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+};
+
 static std::size_t mapToBucket(std::size_t v) {    // diff
-  if (v >= 0 && v < 13)        return 0; // 13
-  else if (v >= 13 && v < 29)  return 1; // 16
-  else if (v >= 29 && v < 41)  return 2; // 12
-  else if (v >= 41 && v < 53)  return 3; // 12
-  else if (v >= 53 && v < 71)  return 4; // 18
-  else if (v >= 71 && v < 83)  return 5; // 12
-  else if (v >= 83 && v < 100) return 6; // 17
-  return -1;  // let it crash
+    if (v < 0 || v > 100)
+        return -1;  // let it crash
+
+    return Lookup[v];
 }
 
+
 std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
-  std::array<std::size_t, NUM_BUCKETS> retBuckets{0};
-  for (auto v : values) {
-    retBuckets[mapToBucket(v)]++;
-  }
-  return retBuckets;
+    std::array<std::size_t, NUM_BUCKETS> retBuckets{0};
+    for (auto v: values) {
+        retBuckets[mapToBucket(v)]++;
+    }
+    return retBuckets;
 }
