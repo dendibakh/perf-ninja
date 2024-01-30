15f6baa31425cc9309e84e888f1af30ff88d1c4a
XCemaXX <xcemaxx@gmail.com>
2024-01-30 16:54:02 +0300

    swmem_prefetch_1

--- labs/memory_bound/swmem_prefetch_1/solution.cpp
+++ labs/memory_bound/swmem_prefetch_1/solution.cpp
@@ -11,10 +11,17 @@ static int getSumOfDigits(int n) {
 
 int solution(const hash_map_t *hash_map, const std::vector<int> &lookups) {
   int result = 0;
-
-  for (int val : lookups) {
-    if (hash_map->find(val))
-      result += getSumOfDigits(val);
+  constexpr int prefetch_ahead = 16;
+  for (int i = 0; i < lookups.size() - prefetch_ahead; ++i) {
+    if (hash_map->find(lookups[i])) {
+      result += getSumOfDigits(lookups[i]);
+    }
+    hash_map->prefetch(lookups[i+prefetch_ahead]);
+  }
+  for (int i = lookups.size() - prefetch_ahead; i < lookups.size(); ++i) {
+    if (hash_map->find(lookups[i])) {
+      result += getSumOfDigits(lookups[i]);
+    }
   }
 
   return result;
