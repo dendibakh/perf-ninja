f9f368fa6c8ce8ad88588339b33473adbd3e2ee5
XCemaXX <xcemaxx@gmail.com>
2024-01-30 11:47:29 +0300

    loop_tiling_1

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -1,11 +1,23 @@
 #include "solution.hpp"
 #include <algorithm>
 
-bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
-  int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
+constexpr int TILE_SIZE = 16;
+
+static void work_tile(const MatrixOfDoubles &in, MatrixOfDoubles &out, const int i_offset, const int j_offset, const int size) {
+  for (int i = i_offset; i < std::min(i_offset + TILE_SIZE, size); ++i) {
+    for (int j = j_offset; j < std::min(j_offset + TILE_SIZE, size); ++j) {
       out[i][j] = in[j][i];
+    }
+  }
+}
+
+bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
+  const int size = in.size();
+  const int tile_count = in.size() / TILE_SIZE + 1;
+  
+  for (int i = 0; i < tile_count; ++i) {
+    for (int j = 0; j < tile_count; ++j) {
+      work_tile(in, out, i * TILE_SIZE, j * TILE_SIZE, size);
     }
   }
   return out[0][size - 1];
