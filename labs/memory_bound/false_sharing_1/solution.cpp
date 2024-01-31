a192ea83a5b6819e46e05cd973dbed6ebfe9fa1f
XCemaXX <xcemaxx@gmail.com>
2024-01-31 08:48:45 +0300

    false_sharing_1_by_alignas

--- labs/memory_bound/false_sharing_1/solution.cpp
+++ labs/memory_bound/false_sharing_1/solution.cpp
@@ -2,22 +2,22 @@
 #include <atomic>
 #include <cstring>
 #include <omp.h>
-#include <list>
+#include <vector>
 
 std::size_t solution(const std::vector<uint32_t> &data, int thread_count) {
   // Using std::atomic counters to disallow compiler to promote `target`
   // memory location into a register. This way we ensure that the store
   // to `target` stays inside the loop.
-  struct Accumulator {
+  struct alignas(64) Accumulator {
     std::atomic<uint32_t> value = 0;
   };
-  std::list<Accumulator> accumulators(thread_count);
+  std::vector<Accumulator> accumulators(thread_count);
 
 #pragma omp parallel num_threads(thread_count) default(none)                   \
     shared(accumulators, data)
   {
     int target_index = omp_get_thread_num();
-    auto &target = *std::next(accumulators.begin(), target_index); //accumulators[target_index];
+    auto &target = accumulators[target_index];
 
 #pragma omp for
     for (int i = 0; i < data.size(); i++) {
