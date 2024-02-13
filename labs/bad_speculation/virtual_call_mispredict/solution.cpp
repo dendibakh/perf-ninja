889361dae20b39b0976df4f1eee99dd08e1006d3
XCemaXX <xcemaxx@gmail.com>
2024-02-13 07:13:08 +0300

    virtual_call_mispredict

--- labs/bad_speculation/virtual_call_mispredict/solution.cpp
+++ labs/bad_speculation/virtual_call_mispredict/solution.cpp
@@ -2,19 +2,28 @@
 
 #include <random>
 
+std::unique_ptr<BaseClass> fabric(int i) {
+    if (i == 0) {
+        return std::make_unique<ClassA>();
+    } else if (i == 1) {
+        return std::make_unique<ClassB>();
+    }
+    return std::make_unique<ClassC>();
+}
+
 void generateObjects(InstanceArray& array) {
     std::default_random_engine generator(0);
     std::uniform_int_distribution<std::uint32_t> distribution(0, 2);
 
+    InstanceArray parts[3];
     for (std::size_t i = 0; i < N; i++) {
         int value = distribution(generator);
-        if (value == 0) {
-            array.push_back(std::make_unique<ClassA>());
-        } else if (value == 1) {
-            array.push_back(std::make_unique<ClassB>());
-        } else {
-            array.push_back(std::make_unique<ClassC>());
-        }
+        parts[value].emplace_back(fabric(value));
+    }
+    for (std::size_t i = 0; i < 3; i++) {
+        array.insert(array.end(),
+                    std::make_move_iterator(parts[i].begin()), 
+                    std::make_move_iterator(parts[i].end()));
     }
 }
 
