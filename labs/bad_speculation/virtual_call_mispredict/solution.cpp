7d476ad3eb2dfcf0c0a5d8303a9c6cf4429380a5
Anton Krasnokutskiy <antonreborn777@gmail.com>
2024-03-13 18:21:45 -0700

    Branch target predictor fix

--- labs/bad_speculation/virtual_call_mispredict/solution.cpp
+++ labs/bad_speculation/virtual_call_mispredict/solution.cpp
@@ -5,13 +5,20 @@
 void generateObjects(InstanceArray& array) {
     std::default_random_engine generator(0);
     std::uniform_int_distribution<std::uint32_t> distribution(0, 2);
+    size_t class_cnts[3] = {};
 
     for (std::size_t i = 0; i < N; i++) {
         int value = distribution(generator);
-        if (value == 0) {
+        class_cnts[value]++;
+    }
+
+    for (std::size_t i = 0; i < N; i++) {
+        if (class_cnts[0] != 0) {
             array.push_back(std::make_unique<ClassA>());
-        } else if (value == 1) {
+            class_cnts[0]--;
+        } else if (class_cnts[1] != 0) {
             array.push_back(std::make_unique<ClassB>());
+            class_cnts[1]--;
         } else {
             array.push_back(std::make_unique<ClassC>());
         }
