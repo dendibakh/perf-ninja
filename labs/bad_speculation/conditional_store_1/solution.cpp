ae1b0b8147f4135b61c2a1199544bc1533fecc13
Anton Krasnokutskiy <antonreborn777@gmail.com>
2024-03-13 18:52:22 -0700

    Changed branch to cmov

--- labs/bad_speculation/conditional_store_1/solution.cpp
+++ labs/bad_speculation/conditional_store_1/solution.cpp
@@ -4,11 +4,11 @@
 // Select items which have S.first in range [lower..upper]
 std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
                    const std::uint32_t lower, const std::uint32_t upper) {
-  std::size_t count = 0;
-  for (const auto item : input) {
-    if ((lower <= item.first) && (item.first <= upper)) {
-      output[count++] = item;
+    std::size_t count = 0;
+    for (const auto item: input) {
+        const std::uint32_t cond = (lower <= item.first) && (item.first <= upper);
+        output[count] = item;
+        count += cond;
     }
-  }
-  return count;
+    return count;
 }
