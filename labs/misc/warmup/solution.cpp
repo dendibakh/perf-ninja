62eb50ad6ce7faf5fb07b659c312303fa938aca9
Dave Stott <Dave.Stott@arm.com>
2024-04-22 11:49:40 +0100

    Solved warmup lab

--- labs/misc/warmup/solution.cpp
+++ labs/misc/warmup/solution.cpp
@@ -2,9 +2,9 @@
 #include "solution.h"
 
 int solution(int *arr, int N) {
-  int res = 0;
-  for (int i = 0; i < N; i++) {
-    res += arr[i];
-  }
-  return res;
+  // int res = 0;
+  // for (int i = 0; i < N; i++) {
+  //   res += arr[i];
+  // }
+  return  (N * (N+1))/2;
 }
