8282260035b405c28e88ae72a218ed3fa292e200
Martyn Jacques <martyn.jacques@arm.com>
2024-04-23 10:51:17 +0100

    Fix cmake and speedup script and done data packing

--- tools/check_speedup.py
+++ tools/check_speedup.py
@@ -64,7 +64,7 @@ def buildAndRunBench(iterNumber, variant, cmakeFlags):
     labAbsPath = labRootPath
     if not os.path.isabs(labAbsPath):
       labAbsPath = os.path.join(saveCWD, labRootPath)
-    callWrapper("cmake -B . -G Ninja -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_BUILD_TYPE=Release " + cmakeFlags + " -S \"" + labAbsPath + "\"")
+    callWrapper("cmake -B . -G Ninja -DCMAKE_CXX_COMPILER=clang++-17 -DCMAKE_C_COMPILER=clang-17 -DCMAKE_BUILD_TYPE=Release " + cmakeFlags + " -S \"" + labAbsPath + "\"")
     callWrapper("cmake --build . --config Release --parallel 8")
     # this will save score in result.json file
     callWrapper("cmake --build . --config Release --target validateLab")
