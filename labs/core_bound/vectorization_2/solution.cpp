19b654a95a6be3db318ad2b7e827acc53d1dad70
om14 <sc_sunjian@163.com>
2024-06-02 18:02:43 +0800

    vectorization_2 my version

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,10 +1,21 @@
 #include "solution.hpp"
 
 uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
-  for (auto value : blob) {
-    acc += value;
-    acc += acc < value; // add carry
-  }
-  return acc;
+    std::array<uint32_t, 8> seg_sum{};
+    seg_sum.fill(0);
+    for (std::size_t i = 0; i < N; i += 8) {
+        for (std::size_t j = 0; j < 8; j++) {
+            seg_sum[j] += blob[i + j];
+        }
+    }
+    uint32_t sum = 0;
+    for (std::size_t i = 0; i < 8; i++) {
+        sum += seg_sum[i];
+    }
+    uint16_t high = sum >> 16;
+    uint16_t low = sum & ((1 << 16) - 1);
+    low += high;
+    low += low < high;
+
+    return low;
 }
