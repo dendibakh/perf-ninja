8046e6c06b0c0d8cc7020479a210ff6496360a35
om14 <sc_sunjian@163.com>
2024-06-02 18:22:19 +0800

    vectorization_2

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,17 +1,11 @@
 #include "solution.hpp"
 
 uint16_t checksum(const Blob &blob) {
-    std::array<uint32_t, 8> seg_sum{};
-    seg_sum.fill(0);
-    for (std::size_t i = 0; i < N; i += 8) {
-        for (std::size_t j = 0; j < 8; j++) {
-            seg_sum[j] += blob[i + j];
-        }
-    }
     uint32_t sum = 0;
-    for (std::size_t i = 0; i < 8; i++) {
-        sum += seg_sum[i];
+    for (auto value: blob) {
+        sum += value;
     }
+
     uint16_t high = sum >> 16;
     uint16_t low = sum & ((1 << 16) - 1);
     low += high;
