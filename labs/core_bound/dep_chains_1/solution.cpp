09cf977a68a2888107c5bb4bf7a8198aefde0186
om14 <sc_sunjian@163.com>
2024-06-04 22:40:38 +0800

    dep_chains_1

--- labs/core_bound/dep_chains_1/solution.cpp
+++ labs/core_bound/dep_chains_1/solution.cpp
@@ -20,23 +20,59 @@ unsigned getSumOfDigits(unsigned n) {
 // Hint: Traversing a linked list is a long data dependency chain:
 //       to get the node N+1 you need to retrieve the node N first.
 //       Think how you can execute multiple dependency chains in parallel.
-unsigned solution(List *l1, List *l2) {
-  unsigned retVal = 0;
+template <int M> unsigned solution(List *l1, List *l2) {
+    unsigned retVal = 0;
+    List *head2 = l2;
+    List *head1 = l1;
 
-  List *head2 = l2;
-  // O(N^2) algorithm:
-  while (l1) {
-    unsigned v = l1->value;
-    l2 = head2;
-    while (l2) {
-      if (l2->value == v) {
-        retVal += getSumOfDigits(v);
-        break;
-      }
-      l2 = l2->next;
+    int length1 = 0;
+    while (l1) {
+        length1++;
+        l1 = l1->next;
     }
-    l1 = l1->next;
-  }
 
-  return retVal;
+    l1 = head1;
+
+    // Simultaneously lookup M elements in l1.
+    for (int i = 0; i < length1 / M; i++) {
+        std::array<unsigned, M> vals;
+        // remember M values from l1
+        for (int j = 0; j < M; j++) {
+            vals[j] = l1->value;
+            l1 = l1->next;
+        }
+        // traverse l2 and lookup M elements from vals at the same time
+        l2 = head2;
+        int found = 0;
+        while (l2) {
+            for (int j = 0; j < M; j++) {
+                if (l2->value == vals[j]) {
+                    retVal += getSumOfDigits(l2->value);
+                    // stop if all M values found
+                    if (++found == M)
+                        break;
+                }
+            }
+            l2 = l2->next;
+        }
+    }
+
+    // Process the remainder with sequential algorithm
+    // O(N^2) algorithm:
+    while (l1) {
+        unsigned v = l1->value;
+        l2 = head2;
+        while (l2) {
+            if (l2->value == v) {
+                retVal += getSumOfDigits(v);
+                break;
+            }
+            l2 = l2->next;
+        }
+        l1 = l1->next;
+    }
+
+    return retVal;
 }
+
+unsigned solution(List *l1, List *l2) { return solution<4>(l1, l2); }
