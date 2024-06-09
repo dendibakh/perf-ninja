1c25e3ec15e3fe2cd4857304fd033dd6d4c0a174
Onur Akdemir <onur.akdemir@icloud.com>
2024-06-09 16:13:56 +0300

    Revert "3 level tiling experiment"
    This reverts commit 37659e6cb994c8a5840dfeec10ab5924c85ca6cc.

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -3,41 +3,16 @@
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  constexpr int block_size_l1 = 16;
-  constexpr int block_size_l2 = 64;
-  constexpr int block_size_l3 = 256;
-
-  for (int iiii = 0; iiii < size; iiii += block_size_l3) {
-    for (int jjjj = 0; jjjj < size; jjjj += block_size_l3) {
-      for (int i = iiii; i < std::min(iiii + block_size_l3, size);
-           i += block_size_l2) {
-        for (int j = jjjj; j < std::min(jjjj + block_size_l3, size);
-             j += block_size_l2) {
-          for (int ii = i; ii < std::min(i + block_size_l2, size);
-               ii += block_size_l1) {
-            for (int jj = j; jj < std::min(j + block_size_l2, size);
-                 jj += block_size_l1) {
-              for (int i = ii; i < std::min(ii + block_size_l1, size); i++) {
-                for (int j = jj; j < std::min(jj + block_size_l1, size); j++) {
-                  out[i][j] = in[j][i];
-                }
-              }
-            }
-          }
+  constexpr int block_size = 16;
+  for (int ii = 0; ii < size; ii += block_size) {
+    for (int jj = 0; jj < size; jj += block_size) {
+      for (int i = ii; i < std::min(ii + block_size, size); i++) {
+        for (int j = jj; j < std::min(jj + block_size, size); j++) {
+          out[i][j] = in[j][i];
         }
       }
     }
   }
-
-  // for (int ii = 0; ii < size; ii += block_size) {
-  //   for (int jj = 0; jj < size; jj += block_size) {
-  //     for (int i = ii; i < std::min(ii + block_size, size); i++) {
-  //       for (int j = jj; j < std::min(jj + block_size, size); j++) {
-  //         out[i][j] = in[j][i];
-  //       }
-  //     }
-  //   }
-  // }
 
   return out[0][size - 1];
 }
