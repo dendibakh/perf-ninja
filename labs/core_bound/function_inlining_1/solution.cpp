607169b367a23154b495ade4ece8271d4d85d2cc
1100fe <kailiang2000@outlook.com>
2024-06-09 22:43:12 +0800

    using lambda to replace function pointer

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -22,6 +22,25 @@ static inline int compare(const void *lhs, const void *rhs) {
   return 0;
 }
 
+auto f = [](const void *lhs, const void *rhs)
+{
+  auto &a = *reinterpret_cast<const S *>(lhs);
+  auto &b = *reinterpret_cast<const S *>(rhs);
+
+  if (a.key1 < b.key1)
+    return -1;
+
+  if (a.key1 > b.key1)
+    return 1;
+
+  if (a.key2 < b.key2)
+    return -1;
+
+  if (a.key2 > b.key2)
+    return 1;
+
+  return 0;
+};
 void solution(std::array<S, N> &arr) {
-  qsort(arr.data(), arr.size(), sizeof(S), compare);
+  qsort(arr.data(), arr.size(), sizeof(S), f);
 }
