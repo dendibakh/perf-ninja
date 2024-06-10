462ca41882ae4e2b7070e1b03503ce5b8557c1a5
1100fe <kailiang2000@outlook.com>
2024-06-10 08:21:03 +0800

    use sort to replace qsort

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -22,25 +22,14 @@ static inline int compare(const void *lhs, const void *rhs) {
   return 0;
 }
 
-auto f = [](const void *lhs, const void *rhs)
-{
-  auto &a = *reinterpret_cast<const S *>(lhs);
-  auto &b = *reinterpret_cast<const S *>(rhs);
-
-  if (a.key1 < b.key1)
-    return -1;
-
-  if (a.key1 > b.key1)
-    return 1;
-
-  if (a.key2 < b.key2)
-    return -1;
-
-  if (a.key2 > b.key2)
-    return 1;
-
-  return 0;
-};
 void solution(std::array<S, N> &arr) {
-  qsort(arr.data(), arr.size(), sizeof(S), f);
+  //qsort(arr.data(), arr.size(), sizeof(S), f);
+  std::sort(arr.begin(), arr.end(), [](auto& a, auto& b)
+  {
+    if(a.key1 == b.key1)
+    {
+      return a.key2 < b.key2;
+    }
+    return a.key1 < b.key1;
+  });
 }
