fcac10a222a7e3c6d26dea2c7e678db8d90976bc
Maqe <smack.ssbm@gmail.com>
2024-06-13 19:02:43 +0200

    Solved warmup lab

--- labs/misc/warmup/solution.cpp
+++ labs/misc/warmup/solution.cpp
@@ -2,9 +2,11 @@
 #include "solution.h"
 
 int solution(int *arr, int N) {
-  int res = 0;
-  for (int i = 0; i < N; i++) {
-    res += arr[i];
-  }
-  return res;
+    //int res = 0;
+    //for (int i = 0; i < N; i++) {
+    //res += arr[i];
+    //}
+    
+    int res = N*(N+1)/2;
+    return res;
 }
