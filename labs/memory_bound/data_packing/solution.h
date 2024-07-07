2c82887d962243019c2af85207dd07af7f9b7d69
Adarsh Agrawal <adarsh@gmail.com>
2024-07-07 20:22:43 +0100

    Memory Bound 1: Data Packing

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -8,14 +8,15 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
-  bool b;
+  float d;
+  int l:16;
+  short i:8;
+  short s : 7;
+  bool b : 1;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+
 
 void init(std::array<S, N> &arr);
 S create_entry(int first_value, int second_value);
