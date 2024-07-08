88b6acb182b7b6d77cfd290fb42d3da7a33b53b3
hungryzzz <512660048@qq.com>
2024-07-08 15:34:55 +0800

    sw prefecth

--- labs/memory_bound/swmem_prefetch_1/solution.cpp
+++ labs/memory_bound/swmem_prefetch_1/solution.cpp
@@ -12,17 +12,24 @@ static int getSumOfDigits(int n) {
 int solution(const hash_map_t *hash_map, const std::vector<int> &lookups) {
   int result = 0;
 
-  int val = lookups[0];
-  hash_map->prefetch(val);
-  for (int i = 1; i < lookups.size(); ++i) {
-    int new_val = lookups[i];
-    hash_map->prefetch(new_val);
-    if (hash_map->find(val))
-      result += getSumOfDigits(val);
-    val = new_val;
+  int val1 = lookups[0], val2 = lookups[1];
+  hash_map->prefetch(val1);
+  hash_map->prefetch(val2);
+  for (int i = 2; i < lookups.size(); i += 2) {
+    int new_val1 = lookups[i], new_val2 = lookups[i+1];
+    hash_map->prefetch(new_val1);
+    hash_map->prefetch(new_val2);
+    if (hash_map->find(val1))
+      result += getSumOfDigits(val1);
+    if (hash_map->find(val2))
+      result += getSumOfDigits(val2);
+    val1 = new_val1;
+    val2 = new_val2;
   }
-  if (hash_map->find(val))
-    result += getSumOfDigits(val);
+  if (hash_map->find(val1))
+      result += getSumOfDigits(val1);
+  if (hash_map->find(val2))
+    result += getSumOfDigits(val2);
 
   return result;
 }
