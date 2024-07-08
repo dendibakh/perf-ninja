3a77ac23e3a0dfe7a6143345a32e147dfbcb291b
hungryzzz <512660048@qq.com>
2024-07-08 15:56:52 +0800

    sw prefecth

--- labs/memory_bound/swmem_prefetch_1/init.cpp
+++ labs/memory_bound/swmem_prefetch_1/init.cpp
@@ -6,13 +6,8 @@ void init(hash_map_t *hash_map, std::vector<int> &lookups) {
   std::default_random_engine generator;
   std::uniform_int_distribution<int> distribution(
       0, std::numeric_limits<int>::max());
-  int val = distribution(generator);
-  hash_map->prefetch(val);
   for (int i = 0; i < HASH_MAP_SIZE; i++) {
-    int new_val = distribution(generator);
-    hash_map->prefetch(new_val);
-    hash_map->insert(val);
-    val = new_val;
+    hash_map->insert(distribution(generator));
   }
 
   for (int i = 0; i < NUMBER_OF_LOOKUPS; i++) {
--- labs/memory_bound/swmem_prefetch_1/solution.cpp
+++ labs/memory_bound/swmem_prefetch_1/solution.cpp
@@ -9,27 +9,24 @@ static int getSumOfDigits(int n) {
   return sum;
 }
 
+constexpr int lookahead = 32; 
+
 int solution(const hash_map_t *hash_map, const std::vector<int> &lookups) {
   int result = 0;
 
-  int val1 = lookups[0], val2 = lookups[1];
-  hash_map->prefetch(val1);
-  hash_map->prefetch(val2);
-  for (int i = 2; i < lookups.size(); i += 2) {
-    int new_val1 = lookups[i], new_val2 = lookups[i+1];
-    hash_map->prefetch(new_val1);
-    hash_map->prefetch(new_val2);
-    if (hash_map->find(val1))
-      result += getSumOfDigits(val1);
-    if (hash_map->find(val2))
-      result += getSumOfDigits(val2);
-    val1 = new_val1;
-    val2 = new_val2;
+  
+  for (int i = 0; i < lookups.size() - lookahead; ++i) {
+    int val = lookups[i];
+    if (hash_map->find(val))
+      result += getSumOfDigits(val);
+    hash_map->prefetch(lookups[i + lookahead]);
   }
-  if (hash_map->find(val1))
-      result += getSumOfDigits(val1);
-  if (hash_map->find(val2))
-    result += getSumOfDigits(val2);
+
+  for (int i = lookups.size() - lookahead; i < lookups.size(); ++i) {
+    int val = lookups[i];
+    if (hash_map->find(val))
+      result += getSumOfDigits(val);
+  }
 
   return result;
 }
