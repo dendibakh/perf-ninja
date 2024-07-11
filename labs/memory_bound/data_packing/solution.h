8b46bbe1dea0f0cb6cbc73a511f0560d26b640e4
Farravid <davidmg103@hotmail.com>
2024-07-11 17:56:44 +0200

    Using bitfields from 12 to 8 bytes!

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -9,11 +9,13 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  float d{};
-  int16_t l{};
-  uint8_t i{};
-  uint8_t s{};
-  bool b{};
+  float d{}; //32 bits
+  unsigned l : 14; 
+  unsigned i : 8; 
+  unsigned s : 8; 
+  unsigned b : 1;
+
+  //total 46 + 16 + 1 = 63 round 64 = 8 bytes, we save 4 bytes
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
