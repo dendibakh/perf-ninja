cee058271d8ca258511f52f54256454ad4c032c2
hungryzzz <512660048@qq.com>
2024-07-11 16:26:18 +0800

    loop tiling

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -3,10 +3,16 @@
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
+  constexpr int TILE_SIZE = 16;
+  for (int i = 0; i < size; i += TILE_SIZE) {
+    for (int j = 0; j < size; j += TILE_SIZE) {
+      for (int ii = i; ii < std::min(i + TILE_SIZE, size); ++ii) {
+        for (int jj = j; jj < std::min(j + TILE_SIZE, size); ++jj) {
+          out[ii][jj] = in[jj][ii];
+        }
+      }
     }
   }
+
   return out[0][size - 1];
 }
