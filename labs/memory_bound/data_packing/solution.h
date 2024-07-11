a7461a9a9fab1e1a79ed9cde89bf3d66f281d0aa
Farravid <davidmg103@hotmail.com>
2024-07-11 18:10:10 +0200

    This is working what is going on servers

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -10,10 +10,10 @@ constexpr int maxRandom = 100;
 // FIXME: this data structure can be reduced in size
 struct S {
   float d; //32 bits
-  long long l : 16; 
-  int i : 8; 
-  short s : 7; 
-  bool b : 1;
+  unsigned l : 16; 
+  unsigned i : 8; 
+  unsigned s : 7; 
+  unsigned b : 1;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
