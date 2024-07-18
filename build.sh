0c50fee11cb1d6989f36ba7d7d936e4b5252b1f5
David Martínez García <davidmg103@hotmail.com>
2024-07-18 17:40:05 +0200

    This is pretty similar to loop unrolling? Dep_chains1
    Changed the order of debug/release on build.sh

--- build.sh
+++ build.sh
@@ -6,14 +6,14 @@ cd "${CALLING_PATH}"
 cmake -E make_directory build
 cd build
 
-if [[ "$1" == "release" ]]; then
-    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang-18 -DCMAKE_CXX_COMPILER=clang++ ..
-    cmake --build . --config Release --parallel 8
+if [[ "$1" == "debug" ]]; then
+    cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang-18 -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_FLAGS="-O0" -DCMAKE_CXX_FLAGS="-O0" ..
+    cmake --build . --config Debug --parallel 8
     cmake --build . --target validateLab
     cmake --build . --target benchmarkLab
 else
-    cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang-18 -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_FLAGS="-O0" -DCMAKE_CXX_FLAGS="-O0" ..
-    cmake --build . --config Debug --parallel 8
+    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang-18 -DCMAKE_CXX_COMPILER=clang++ ..
+    cmake --build . --config Release --parallel 8
     cmake --build . --target validateLab
     cmake --build . --target benchmarkLab
 fi
--- labs/core_bound/dep_chains_1/solution.cpp
+++ labs/core_bound/dep_chains_1/solution.cpp
@@ -6,7 +6,7 @@ unsigned getSumOfDigits(unsigned n) {
   unsigned sum = 0;
   while (n != 0) {
     sum = sum + n % 10;
-    n = n / 10;
+    n = n * 0.1f;
   }
   return sum;
 }
@@ -24,18 +24,37 @@ unsigned solution(List *l1, List *l2) {
   unsigned retVal = 0;
 
   List *head2 = l2;
+
   // O(N^2) algorithm:
-  while (l1) {
+  while (l1) 
+  {
+    bool vFound{};
+    bool v2Found{};
+    
     unsigned v = l1->value;
+    unsigned v2 = l1->next ? l1->next->value : 0;
+
     l2 = head2;
-    while (l2) {
-      if (l2->value == v) {
+    while (l2) 
+    {
+      if (!vFound && l2->value == v) 
+      {
         retVal += getSumOfDigits(v);
-        break;
+        vFound = true;
       }
+      if(v2Found && l2->value == v2)
+      {
+        retVal += getSumOfDigits(v2);
+        v2Found = true;
+      }
+
+      if(vFound & v2Found) break;
+
       l2 = l2->next;
     }
-    l1 = l1->next;
+
+    if(!l1->next) return retVal;
+    l1 = l1->next->next;
   }
 
   return retVal;
