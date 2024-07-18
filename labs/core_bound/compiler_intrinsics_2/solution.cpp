5ea1f6574ad8b9f89bdfae1cb85f1223e41a199b
thanglegons <dthangnguyen19@gmail.com>
2024-07-18 11:09:19 +0700

    try compiler_intrinsics_2

--- labs/core_bound/compiler_intrinsics_2/solution.cpp
+++ labs/core_bound/compiler_intrinsics_2/solution.cpp
@@ -28,7 +28,7 @@ unsigned solution(const std::string &inputContents) {
   for (; i + kBatch - 1 < N; i += kBatch) {
     auto x = _mm_loadu_si128((__m128i*)(inputContents.data() + i));
     auto mask = _mm_cmpeq_epi8(x, newline_broadcast);
-    uint intmask = _mm_movemask_epi8(mask);
+    int intmask = _mm_movemask_epi8(mask);
     int bits = kBatch;
     while (intmask != 0) {
       auto trailing_zeros = _tzcnt_u32(intmask);
