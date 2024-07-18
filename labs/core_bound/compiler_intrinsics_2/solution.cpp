2846b23f80abfffe6913fc492feae541f2705f24
thanglegons <dthangnguyen19@gmail.com>
2024-07-18 11:28:32 +0700

    try compiler_intrinsics_2

--- labs/core_bound/compiler_intrinsics_2/solution.cpp
+++ labs/core_bound/compiler_intrinsics_2/solution.cpp
@@ -20,23 +20,25 @@ unsigned solution(const std::string &inputContents) {
   unsigned curLineLength = 0;
   unsigned N = inputContents.size();
 
-  static constexpr int kBatch = 16;
-
+  static constexpr int kBatch = 32;
 
   int i = 0;
-  auto newline_broadcast = _mm_set1_epi8('\n');
+  auto newline_broadcast = _mm256_set1_epi8('\n');
   for (; i + kBatch - 1 < N; i += kBatch) {
-    auto x = _mm_loadu_si128((__m128i*)(inputContents.data() + i));
-    auto mask = _mm_cmpeq_epi8(x, newline_broadcast);
-    int intmask = _mm_movemask_epi8(mask);
+    auto x = _mm256_loadu_si256((__m256i*)(inputContents.data() + i));
+    auto mask = _mm256_cmpeq_epi8(x, newline_broadcast);
+    uint32_t intmask = _mm256_movemask_epi8(mask);
     int bits = kBatch;
     while (intmask != 0) {
       auto trailing_zeros = _tzcnt_u32(intmask);
       curLineLength += trailing_zeros;
       longestLine = std::max(curLineLength, longestLine);
-      intmask >>= trailing_zeros + 1;
-      bits -= trailing_zeros + 1;
       curLineLength = 0;
+      bits -= trailing_zeros + 1;
+      if (bits == 0) {
+        break;
+      }
+      intmask >>= trailing_zeros + 1;
     }
     curLineLength += bits;
   }
