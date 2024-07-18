75d50328aa1e6eccde07f3a27bbdf47823169df5
David Martínez García <davidmg103@hotmail.com>
2024-07-18 12:29:36 +0200

    Creating one common build for every lab
    Added alias command on my zshrc for better usability

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
-# Can I do this?
+CALLING_PATH=$(pwd)
+
+cd ${CALLING_PATH}
+
 cmake -E make_directory build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release .. -DCMAKE_C_COMPILER=clang-17 -DCMAKE_CXX_COMPILER=clang++
+
+cmake -DCMAKE_BUILD_TYPE=Release .. -DCMAKE_C_COMPILER=clang-18 -DCMAKE_CXX_COMPILER=clang++
 cmake --build . --config Release --parallel 8
 cmake --build . --target validateLab
 cmake --build . --target benchmarkLab
