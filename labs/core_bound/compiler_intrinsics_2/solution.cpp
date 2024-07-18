2226c392c1920673365356b67cc2ab86514a5beb
thanglegons <dthangnguyen19@gmail.com>
2024-07-18 11:06:35 +0700

    try compiler_intrinsics_2

--- labs/core_bound/compiler_intrinsics_2/solution.cpp
+++ labs/core_bound/compiler_intrinsics_2/solution.cpp
@@ -1,5 +1,8 @@
 #include "solution.hpp"
 #include <iostream>
+#include <immintrin.h>
+#include <cstdint>
+#include <string.h>
 
 // Find the longest line in a file.
 // Implementation uses ternary operator with a hope that compiler will
@@ -15,11 +18,33 @@ if (s == '\n') {
 unsigned solution(const std::string &inputContents) {
   unsigned longestLine = 0;
   unsigned curLineLength = 0;
+  unsigned N = inputContents.size();
 
-  for (auto s : inputContents) {
-    curLineLength = (s == '\n') ? 0 : curLineLength + 1;
-    longestLine = std::max(curLineLength, longestLine);
+  static constexpr int kBatch = 16;
+
+  auto newline_broadcast = _mm_set1_epi8('\n');
+
+  int i = 0;
+  for (; i + kBatch - 1 < N; i += kBatch) {
+    auto x = _mm_loadu_si128((__m128i*)(inputContents.data() + i));
+    auto mask = _mm_cmpeq_epi8(x, newline_broadcast);
+    uint intmask = _mm_movemask_epi8(mask);
+    int bits = kBatch;
+    while (intmask != 0) {
+      auto trailing_zeros = _tzcnt_u32(intmask);
+      curLineLength += trailing_zeros;
+      longestLine = std::max(curLineLength, longestLine);
+      intmask >>= trailing_zeros + 1;
+      bits -= trailing_zeros + 1;
+      curLineLength = 0;
+    }
+    curLineLength += bits;
   }
+
+  for (; i < N; i++) {
+    curLineLength = (inputContents[i] == '\n') ? 0 : curLineLength + 1;
+    longestLine = std::max(curLineLength, longestLine);
+  } 
 
   return longestLine;
 }
