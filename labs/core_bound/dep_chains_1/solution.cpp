e121df2dda9dc48ba631820189a85763e0a683fc
thanglegons <dthangnguyen19@gmail.com>
2024-07-19 13:10:27 +0700

    try dep_chains_1

--- labs/core_bound/dep_chains_1/solution.cpp
+++ labs/core_bound/dep_chains_1/solution.cpp
@@ -1,5 +1,8 @@
 #include "solution.hpp"
 #include <array>
+#include <cstdint>
+#include <cstring>
+#include <immintrin.h>
 #include <iostream>
 
 unsigned getSumOfDigits(unsigned n) {
@@ -22,21 +25,46 @@ unsigned getSumOfDigits(unsigned n) {
 //       Think how you can execute multiple dependency chains in parallel.
 unsigned solution(List *l1, List *l2) {
   unsigned retVal = 0;
-
   List *head2 = l2;
-  // O(N^2) algorithm:
+  static constexpr int kBits = 256;
+  static constexpr int kNumInt = kBits / sizeof(unsigned) / 8;
   while (l1) {
-    unsigned v = l1->value;
+    unsigned temp[kNumInt]{0};
+    for (int i = 0; i < kNumInt; i++) {
+      if (l1) {
+        temp[i] = l1->value;
+        l1 = l1->next;
+      }  
+    }
+    auto batch = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(temp));
     l2 = head2;
+    int mask = 0;
     while (l2) {
-      if (l2->value == v) {
-        retVal += getSumOfDigits(v);
-        break;
-      }
+      auto bc = _mm256_set1_epi32(l2->value);
+      auto mask_256 = _mm256_cmpeq_epi32(batch, bc);
+      mask |= _mm256_movemask_epi8(mask_256);
       l2 = l2->next;
     }
-    l1 = l1->next;
+
+    for (int i = 0; i < kNumInt; i++) {
+      if ((mask >> (i * 4)) & 1){
+        retVal += getSumOfDigits(temp[i]);
+      }
+    }
   }
+  // O(N^2) algorithm:
+  //while (l1) {
+  //  unsigned v = l1->value;
+  //  l2 = head2;
+  //  while (l2) {
+  //    if (l2->value == v) {
+  //      retVal += getSumOfDigits(v);
+  //      break;
+  //    }
+  //    l2 = l2->next;
+  //  }
+  //  l1 = l1->next;
+  //}
 
   return retVal;
 }
