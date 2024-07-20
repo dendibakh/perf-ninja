cb7e648c3a03c27eb1a979e389eed796408acce4
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-07-20 17:07:11 +0200

    compiler_intrinsics_1

--- labs/core_bound/compiler_intrinsics_1/solution.cpp
+++ labs/core_bound/compiler_intrinsics_1/solution.cpp
@@ -4,40 +4,70 @@
 
 void imageSmoothing(const InputVector &input, uint8_t radius,
                     OutputVector &output) {
-  int pos = 0;
-  int currentSum = 0;
-  int size = static_cast<int>(input.size());
+    int pos = 0;
+    int currentSum = 0;
+    int size = static_cast<int>(input.size());
 
-  // 1. left border - time spend in this loop can be ignored, no need to
-  // optimize it
-  for (int i = 0; i < std::min<int>(size, radius); ++i) {
-    currentSum += input[i];
-  }
+    // 1. left border - time spend in this loop can be ignored, no need to
+    // optimize it
+    for (int i = 0; i < std::min<int>(size, radius); ++i) {
+        currentSum += input[i];
+    }
 
-  int limit = std::min(radius + 1, size - radius);
-  for (pos = 0; pos < limit; ++pos) {
-    currentSum += input[pos + radius];
-    output[pos] = currentSum;
-  }
+    int limit = std::min(radius + 1, size - radius);
 
-  // 2. main loop.
-  limit = size - radius;
-  for (; pos < limit; ++pos) {
-    currentSum -= input[pos - radius - 1];
-    currentSum += input[pos + radius];
-    output[pos] = currentSum;
-  }
+    for (pos = 0; pos < limit; ++pos) {
+        currentSum += input[pos + radius];
+        output[pos] = currentSum;
+    }
 
-  // 3. special case, executed only if size <= 2*radius + 1
-  limit = std::min(radius + 1, size);
-  for (; pos < limit; pos++) {
-    output[pos] = currentSum;
-  }
+    // 2. main loop.
+    limit = size - radius;
 
-  // 4. right border - time spend in this loop can be ignored, no need to
-  // optimize it
-  for (; pos < size; ++pos) {
-    currentSum -= input[pos - radius - 1];
-    output[pos] = currentSum;
-  }
+    const int unroll = 1024;
+    for (; pos + unroll - 1 < limit; pos += unroll) {
+
+        uint16_t add[unroll];
+
+        for (int i = 0; i < unroll; ++i) add[i] = input[i + pos + radius] - input[i + pos - radius - 1];
+#if 1
+        for (int l = 0; (1 << l) < unroll; l++) {
+#pragma ivdep
+            for (int i = unroll; i-- > (1 << l);) {
+                add[i] = add[i] + add[i - (1 << l)];
+            }
+        }
+#else
+        for (int i = 1; i < unroll; ++i) {
+            add[i] += add[i - 1];
+        }
+#endif
+
+        uint16_t vals[unroll];
+        for (int i = 0; i < unroll; ++i) {
+            vals[i] = currentSum + add[i];
+        }
+
+        for (int i = 0; i < unroll; ++i) output[pos + i] = vals[i];
+        currentSum += add[unroll - 1];
+    }
+
+    for (; pos < limit; ++pos) {
+        currentSum -= input[pos - radius - 1];
+        currentSum += input[pos + radius];
+        output[pos] = currentSum;
+    }
+
+    // 3. special case, executed only if size <= 2*radius + 1
+    limit = std::min(radius + 1, size);
+    for (; pos < limit; pos++) {
+        output[pos] = currentSum;
+    }
+
+    // 4. right border - time spend in this loop can be ignored, no need to
+    // optimize it
+    for (; pos < size; ++pos) {
+        currentSum -= input[pos - radius - 1];
+        output[pos] = currentSum;
+    }
 }
