c429523857b6d35b385e17c4c6002a57133e7b71
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-07-22 16:06:22 +0200

    align to 2^4

--- labs/memory_bound/mem_alignment_1/solution.cpp
+++ labs/memory_bound/mem_alignment_1/solution.cpp
@@ -5,11 +5,16 @@
 // ******************************************
 // Change this function
 // ******************************************
-// This function allows you to change the number of columns in a matrix. 
+// This function allows you to change the number of columns in a matrix.
 // In other words, it defines how many elements are in each row.
 // hint: you need to allocate dummy columns to achieve proper data alignment.
-int n_columns(int N) {  
-  return N;
+int n_columns(int N) {
+    N -= 1;
+    N |= N >> 1;
+    N |= N >> 2;
+    N |= N >> 4;
+    N += 1;
+    return N;
 }
 // ******************************************
 
@@ -20,45 +25,45 @@ int n_columns(int N) {
 // I.e. a matrix has N x N elements, but its actual dimensions are N x K since
 // it has padding. However, only N x N elements are used.
 void initRandom(Matrix &matrix, int N, int K) {
-  std::default_random_engine generator;
-  std::uniform_real_distribution<float> distribution(-0.95f, 0.95f);
-  for (int i = 0; i < N; i++)
-    for (int j = 0; j < N; j++)
-      matrix[i * K + j] = distribution(generator);
+    std::default_random_engine generator;
+    std::uniform_real_distribution<float> distribution(-0.95f, 0.95f);
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            matrix[i * K + j] = distribution(generator);
 }
 
 void initZero(Matrix &matrix, int N, int K) {
-  for (int i = 0; i < N; i++)
-    for (int j = 0; j < N; j++)
-      matrix[i * K + j] = 0.0f;
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            matrix[i * K + j] = 0.0f;
 }
 
 void copyFromMatrix(const Matrix &from, Matrix &to, int N, int K) {
-  for (int i = 0; i < N; i++)
-    for (int j = 0; j < N; j++)
-      to[i * K + j] = from[i * N + j];
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            to[i * K + j] = from[i * N + j];
 }
 
 // A simple GEMM. Use only for small matrices (up to 100 x 100)
-void interchanged_matmul(float* RESTRICT A, 
-                         float* RESTRICT B,
-                         float* RESTRICT C, int N, int K) {
-  for (int i = 0; i < N; ++i)
-    for (int k = 0; k < N; ++k)
-      for (int j = 0; j < N; ++j)
-        C[i * K + j] += A[i * K + k] * B[k * K + j];
+void interchanged_matmul(float *RESTRICT A,
+                         float *RESTRICT B,
+                         float *RESTRICT C, int N, int K) {
+    for (int i = 0; i < N; ++i)
+        for (int k = 0; k < N; ++k)
+            for (int j = 0; j < N; ++j)
+                C[i * K + j] += A[i * K + k] * B[k * K + j];
 }
 
 // Here is a blocked version for larger matrix sizes (e.g. 512 x 512 and beyond).
-void blocked_matmul(float* RESTRICT A, 
-                    float* RESTRICT B,
-                    float* RESTRICT C, int N, int K) {
-  constexpr int blockSize = 64;
-  for (int ii = 0; ii < N; ii += blockSize)
-    for (int kk = 0; kk < N; kk += blockSize)
-      for (int jj = 0; jj < N; jj += blockSize)
-        for (int i = ii; i < std::min(ii + blockSize, N); ++i)
-          for (int k = kk; k < std::min(kk + blockSize, N); ++k)
-            for (int j = jj; j < std::min(jj + blockSize, N); ++j)                        
-              C[i * K + j] += A[i * K + k] * B[k * K + j];
+void blocked_matmul(float *RESTRICT A,
+                    float *RESTRICT B,
+                    float *RESTRICT C, int N, int K) {
+    constexpr int blockSize = 64;
+    for (int ii = 0; ii < N; ii += blockSize)
+        for (int kk = 0; kk < N; kk += blockSize)
+            for (int jj = 0; jj < N; jj += blockSize)
+                for (int i = ii; i < std::min(ii + blockSize, N); ++i)
+                    for (int k = kk; k < std::min(kk + blockSize, N); ++k)
+                        for (int j = jj; j < std::min(jj + blockSize, N); ++j)
+                            C[i * K + j] += A[i * K + k] * B[k * K + j];
 }
