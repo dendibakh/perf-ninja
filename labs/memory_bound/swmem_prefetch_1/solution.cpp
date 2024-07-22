30fb228f6c40b49837f03981b733abf4ed84e4d3
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-07-22 23:30:04 +0200

    just changing random stuff

--- labs/memory_bound/swmem_prefetch_1/solution.cpp
+++ labs/memory_bound/swmem_prefetch_1/solution.cpp
@@ -14,7 +14,7 @@ int solution(const hash_map_t *hash_map, const std::vector<int> &lookups) {
 
 
     int i = 0;
-    const int prefetch_dist = 8;
+    const int prefetch_dist = 16;
     for (; i + prefetch_dist < lookups.size(); ++i) {
         hash_map->prefetch(lookups[i + prefetch_dist]);
         if (hash_map->find(lookups[i]))
