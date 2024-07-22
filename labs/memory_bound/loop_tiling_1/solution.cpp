12ea0bfb873fb66f295d4a8d41334ccd4b31bc2e
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-07-22 15:59:09 +0200

    tiling with size 16

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -2,11 +2,17 @@
 #include <algorithm>
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
-  int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
+    int size = in.size();
+    const int k = 16;
+    for (int lo_i = 0, hi_i = k; lo_i < size; lo_i = hi_i, hi_i = std::min(size, hi_i + k)) {
+        for (int lo_j = 0, hi_j = k; lo_j < size; lo_j = hi_j, hi_j = std::min(size, hi_j + k)) {
+            for (int i = lo_i; i < hi_i; i++) {
+                for (int j = lo_j; j < hi_j; j++) {
+                    out[i][j] = in[j][i];
+                }
+            }
+        }
     }
-  }
-  return out[0][size - 1];
+
+    return out[0][size - 1];
 }
