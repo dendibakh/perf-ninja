51520bb8d2c33a158f845cf5982ad0227295d882
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-07-22 21:23:54 +0200

    just prefetch next

--- labs/memory_bound/swmem_prefetch_1/solution.cpp
+++ labs/memory_bound/swmem_prefetch_1/solution.cpp
@@ -1,21 +1,28 @@
 #include "solution.hpp"
 
 static int getSumOfDigits(int n) {
-  int sum = 0;
-  while (n != 0) {
-    sum = sum + n % 10;
-    n = n / 10;
-  }
-  return sum;
+    int sum = 0;
+    while (n != 0) {
+        sum = sum + n % 10;
+        n = n / 10;
+    }
+    return sum;
 }
 
 int solution(const hash_map_t *hash_map, const std::vector<int> &lookups) {
-  int result = 0;
+    int result = 0;
 
-  for (int val : lookups) {
-    if (hash_map->find(val))
-      result += getSumOfDigits(val);
-  }
 
-  return result;
+    int i = 0;
+    for (; i + 1 < lookups.size(); ++i) {
+        hash_map->prefetch(lookups[i + 1]);
+        if (hash_map->find(lookups[i]))
+            result += getSumOfDigits(lookups[i]);
+    }
+    for (; i < lookups.size(); ++i) {
+        if (hash_map->find(lookups[i]))
+            result += getSumOfDigits(lookups[i]);
+    }
+
+    return result;
 }
--- labs/memory_bound/swmem_prefetch_1/solution.hpp
+++ labs/memory_bound/swmem_prefetch_1/solution.hpp
@@ -1,5 +1,5 @@
-#include <vector>
 #include <limits>
+#include <vector>
 
 static constexpr std::size_t HASH_MAP_SIZE = 32 * 1024 * 1024 - 5;
 static constexpr std::size_t NUMBER_OF_LOOKUPS = 1024 * 1024;
@@ -8,6 +8,7 @@ class hash_map_t {
     static constexpr int UNUSED = std::numeric_limits<int>::max();
     std::vector<int> m_vector;
     std::size_t N_Buckets;
+
 public:
     hash_map_t(std::size_t size) : m_vector(size, UNUSED), N_Buckets(size) {}
 
@@ -20,11 +21,16 @@ public:
         return false;
     }
 
+    void prefetch(int val) const {
+        int bucket = val % N_Buckets;
+        __builtin_prefetch(&m_vector[bucket]);
+    }
+
     bool find(int val) const {
         int bucket = val % N_Buckets;
         return m_vector[bucket] != UNUSED;
     }
 };
 
-void init(hash_map_t* hash_map, std::vector<int>& lookups);
-int solution(const hash_map_t* hash_map, const std::vector<int>& lookups);
+void init(hash_map_t *hash_map, std::vector<int> &lookups);
+int solution(const hash_map_t *hash_map, const std::vector<int> &lookups);
