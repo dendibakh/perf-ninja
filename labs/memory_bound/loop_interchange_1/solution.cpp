a706a772e5736efc5d6aa21f6e5236bdebb2be49
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-07-22 15:22:19 +0200

    matrix multiply

--- labs/memory_bound/loop_interchange_1/solution.cpp
+++ labs/memory_bound/loop_interchange_1/solution.cpp
@@ -5,66 +5,66 @@
 
 // Make zero matrix
 void zero(Matrix &result) {
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
-      result[i][j] = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            result[i][j] = 0;
+        }
     }
-  }
 }
 
 // Make identity matrix
 void identity(Matrix &result) {
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
-      result[i][j] = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            result[i][j] = 0;
+        }
+        result[i][i] = 1;
     }
-    result[i][i] = 1;
-  }
 }
 
 // Multiply two square matrices
 void multiply(Matrix &result, const Matrix &a, const Matrix &b) {
-  zero(result);
+    zero(result);
 
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
-      for (int k = 0; k < N; k++) {
-        result[i][j] += a[i][k] * b[k][j];
-      }
+    for (int i = 0; i < N; i++) {
+        for (int k = 0; k < N; k++) {
+            for (int j = 0; j < N; j++) {
+                result[i][j] += a[i][k] * b[k][j];
+            }
+        }
     }
-  }
 }
 
 // Compute integer power of a given square matrix
 Matrix power(const Matrix &input, const uint32_t k) {
-  // Temporary products
-  std::unique_ptr<Matrix> productCurrent(new Matrix());
-  std::unique_ptr<Matrix> productNext(new Matrix());
+    // Temporary products
+    std::unique_ptr<Matrix> productCurrent(new Matrix());
+    std::unique_ptr<Matrix> productNext(new Matrix());
 
-  // Temporary elements = a^(2^integer)
-  std::unique_ptr<Matrix> elementCurrent(new Matrix());
-  std::unique_ptr<Matrix> elementNext(new Matrix());
+    // Temporary elements = a^(2^integer)
+    std::unique_ptr<Matrix> elementCurrent(new Matrix());
+    std::unique_ptr<Matrix> elementNext(new Matrix());
 
-  // Initial values
-  identity(*productCurrent);
-  *elementCurrent = input;
+    // Initial values
+    identity(*productCurrent);
+    *elementCurrent = input;
 
-  // Use binary representation of k to be O(log(k))
-  for (auto i = k; i > 0; i /= 2) {
-    if (i % 2 != 0) {
-      // Multiply the product by element
-      multiply(*productNext, *productCurrent, *elementCurrent);
-      std::swap(productNext, productCurrent);
+    // Use binary representation of k to be O(log(k))
+    for (auto i = k; i > 0; i /= 2) {
+        if (i % 2 != 0) {
+            // Multiply the product by element
+            multiply(*productNext, *productCurrent, *elementCurrent);
+            std::swap(productNext, productCurrent);
 
-      // Exit early to skip next squaring
-      if (i == 1)
-        break;
+            // Exit early to skip next squaring
+            if (i == 1)
+                break;
+        }
+
+        // Square an element
+        multiply(*elementNext, *elementCurrent, *elementCurrent);
+        std::swap(elementNext, elementCurrent);
     }
 
-    // Square an element
-    multiply(*elementNext, *elementCurrent, *elementCurrent);
-    std::swap(elementNext, elementCurrent);
-  }
-
-  return std::move(*productCurrent);
+    return std::move(*productCurrent);
 }
