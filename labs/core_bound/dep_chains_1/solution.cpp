c35449859c5cfba305a12c63cc270c97d6a07bfe
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-07-23 15:02:07 +0200

    unroll da loop

--- labs/core_bound/dep_chains_1/solution.cpp
+++ labs/core_bound/dep_chains_1/solution.cpp
@@ -3,12 +3,12 @@
 #include <iostream>
 
 unsigned getSumOfDigits(unsigned n) {
-  unsigned sum = 0;
-  while (n != 0) {
-    sum = sum + n % 10;
-    n = n / 10;
-  }
-  return sum;
+    unsigned sum = 0;
+    while (n != 0) {
+        sum = sum + n % 10;
+        n = n / 10;
+    }
+    return sum;
 }
 
 // Task: lookup all the values from l2 in l1.
@@ -21,22 +21,33 @@ unsigned getSumOfDigits(unsigned n) {
 //       to get the node N+1 you need to retrieve the node N first.
 //       Think how you can execute multiple dependency chains in parallel.
 unsigned solution(List *l1, List *l2) {
-  unsigned retVal = 0;
+    unsigned retVal = 0;
 
-  List *head2 = l2;
-  // O(N^2) algorithm:
-  while (l1) {
-    unsigned v = l1->value;
-    l2 = head2;
-    while (l2) {
-      if (l2->value == v) {
-        retVal += getSumOfDigits(v);
-        break;
-      }
-      l2 = l2->next;
+    List *head1 = l1;
+    List *head2 = l2;
+    // O(N^2) algorithm:
+
+    const int unroll = 128;
+
+    while (l1) {
+        int vals[unroll];
+        int num_seen = 0;
+
+        for (int i = 0; i < unroll && l1; ++i, l1 = l1->next)
+            vals[num_seen++] = l1->value;
+
+
+        for (l2 = head2; l2 && num_seen > 0; l2 = l2->next) {
+            for (int i = 0; i < num_seen;) {
+                if (vals[i] == l2->value) {
+                    retVal += getSumOfDigits(vals[i]);
+                    std::swap(vals[i], vals[--num_seen]);
+                } else {
+                    ++i;
+                }
+            }
+        }
     }
-    l1 = l1->next;
-  }
 
-  return retVal;
+    return retVal;
 }
