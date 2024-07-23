870911b52eaaddb94e5363325c964c5f0645cd15
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-07-23 14:51:56 +0200

    use std::sort

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -2,26 +2,10 @@
 #include "solution.h"
 #include <algorithm>
 #include <stdlib.h>
-
-static int compare(const void *lhs, const void *rhs) {
-  auto &a = *reinterpret_cast<const S *>(lhs);
-  auto &b = *reinterpret_cast<const S *>(rhs);
-
-  if (a.key1 < b.key1)
-    return -1;
-
-  if (a.key1 > b.key1)
-    return 1;
-
-  if (a.key2 < b.key2)
-    return -1;
-
-  if (a.key2 > b.key2)
-    return 1;
-
-  return 0;
-}
+#include <tuple>
 
 void solution(std::array<S, N> &arr) {
-  qsort(arr.data(), arr.size(), sizeof(S), compare);
+    std::sort(arr.data(), arr.data() + arr.size(), [](auto lhs, auto rhs) {
+        return std::tie(lhs.key1, lhs.key2) < std::tie(rhs.key1, rhs.key2);
+    });
 }
