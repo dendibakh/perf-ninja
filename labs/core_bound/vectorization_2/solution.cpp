03afc847bdba0939f646bb914daa744d77e7a387
thanglegons <dthangnguyen19@gmail.com>
2024-07-23 11:29:09 +0700

    try vectorization_2

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,10 +1,14 @@
 #include "solution.hpp"
 
 uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
+  uint32_t acc = 0;
   for (auto value : blob) {
     acc += value;
-    acc += acc < value; // add carry
+  }
+  while ((acc >> 16) > 0) {
+    uint32_t lower = acc & ((1 << 16) - 1);
+    uint32_t upper = (acc >> 16);
+    acc = lower + upper;
   }
   return acc;
 }
