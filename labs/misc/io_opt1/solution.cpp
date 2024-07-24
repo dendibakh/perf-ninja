e3eb1b208894b36dc63ae869c7396be8692b28ed
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-07-24 20:20:13 +0200

    actually correct implementation

--- labs/misc/io_opt1/solution.cpp
+++ labs/misc/io_opt1/solution.cpp
@@ -2,35 +2,14 @@
 
 #include <fstream>
 #include <stdexcept>
-FILE *fd;
-constexpr bool likely(bool x) {
-#if defined(__clang__) || defined(__GNUC__)
-    return __builtin_expect(x, 1);
-#else
-    return x;
-#endif
-}
-constexpr bool unlikely(bool x) { return !likely(!x); }
-char buf[1 << 16]{};
-size_t bc = 0, be = 0;
-bool last_read = false;
-int gc() {
-    if (likely(bc < be)) {
-        return buf[bc++];
-    } else {
-        if (last_read) return EOF;
-        buf[0] = 0, bc = 0;
-        be = fread(buf, 1, sizeof(buf), fd);
-        if (unlikely(feof(fd))) {
-            last_read = true;
-            buf[be] = EOF;
-        }
-        return buf[bc++];
-    }
-}
 
+#ifdef _WIN32
+#define fread_unlocked _fread_nolock
+#endif
+
+char buf[1 << 20]{};
 uint32_t solution(const char *file_name) {
-    fd = fopen(file_name, "r");
+    FILE *fd = fopen(file_name, "r");
     if (fd == nullptr) {
         throw std::runtime_error{"The file could not be opened"};
     }
@@ -38,10 +17,10 @@ uint32_t solution(const char *file_name) {
     uint32_t crc = 0xff'ff'ff'ff;
 
     // Update the CRC32 value character by character
-    int c;
-    while ((c = gc()) != EOF) {
-
-        update_crc32(crc, static_cast<uint8_t>(c));
+    while (!feof(fd)) {
+        size_t read = fread_unlocked(buf, 1, sizeof(buf), fd);
+        for (int i = 0; i < read; ++i)
+            update_crc32(crc, static_cast<uint8_t>(buf[i]));
     }
 
     // Invert the bits
