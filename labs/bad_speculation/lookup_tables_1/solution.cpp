44ea8af13f2d9fc3427e1f80e46dbc884f22c793
thanglegons <dthangnguyen19@gmail.com>
2024-07-24 10:19:00 +0700

    try lookup_tables_1

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -1,14 +1,33 @@
 #include "solution.hpp"
 
+class Precalc {
+public:
+  constexpr Precalc() {
+    for (int v = 0; v < 100; v++) {
+      if (v >= 0 && v < 13) {
+        lookup[v] = 0;
+      } else if (v >= 13 && v < 29) {
+        lookup[v] = 1;
+      } else if (v >= 29 && v < 41) {
+        lookup[v] = 2;
+      } else if (v >= 41 && v < 53) {
+        lookup[v] = 3;
+      } else if (v >= 53 && v < 71) {
+        lookup[v] = 4;
+      } else if (v >= 71 && v < 83) {
+        lookup[v] = 5;
+      } else if (v >= 83 && v < 100) {
+        lookup[v] = 6;
+      }
+    }
+  }
+
+  size_t lookup[100]{0};
+};
+
+constexpr Precalc pre;
 static std::size_t mapToBucket(std::size_t v) {    // diff
-  if (v >= 0 && v < 13)        return 0; // 13
-  else if (v >= 13 && v < 29)  return 1; // 16
-  else if (v >= 29 && v < 41)  return 2; // 12
-  else if (v >= 41 && v < 53)  return 3; // 12
-  else if (v >= 53 && v < 71)  return 4; // 18
-  else if (v >= 71 && v < 83)  return 5; // 12
-  else if (v >= 83 && v < 100) return 6; // 17
-  return -1;  // let it crash
+  return v >= 100 ? -1 : pre.lookup[v];
 }
 
 std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
