666f816db9925e46e882e24f2b71afc6e101b992
David Martínez García <davidmg103@hotmail.com>
2024-07-25 13:43:10 +0200

    Loop tilling doesn't seem faster on my machine

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -2,10 +2,15 @@
 #include <algorithm>
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
+  static constexpr auto tillingSize = 16;
   int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
+
+  for (int i = 0; i < size; i+=tillingSize) {
+    for (int j = 0; j < size; j+=tillingSize) {
+      for(int k = i; k < std::min(i + tillingSize, size); ++k) {
+        for(int l = j; l < std::min(j + tillingSize, size); ++l)
+          out[k][l] = in[l][k];
+      }
     }
   }
   return out[0][size - 1];
