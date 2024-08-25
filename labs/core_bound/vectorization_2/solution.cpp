c06aaf7ef4c4e7d6adc740995dc2a49e91272365
JonathanHallstrom <lmj.hallstrom@gmail.com>
2024-08-25 03:51:44 +0200

    ew tabs

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -3,21 +3,21 @@
 
 uint16_t checksum(const Blob &blob) {
     constexpr int unroll = 64;
-	uint16_t accs[unroll]{};
-	std::size_t i = 0; 
-	for (; i + unroll - 1 < blob.size(); i += unroll) {
-		for (std::size_t j = 0; j < unroll; ++j) {
-			accs[j] += blob[i + j];
-			accs[j] += accs[j] < blob[i + j];
-		}
-	}
-	for (; i < blob.size(); ++i) {
-		accs[0] += blob[i];
-		accs[0] += accs[0] < blob[i];
-	}
-	for (std::size_t j = 1; j < unroll; ++j) {
-		accs[0] += accs[j];
-		accs[0] += accs[0] < accs[j];
-	}
-	return accs[0];
+    uint16_t accs[unroll]{};
+    std::size_t i = 0;
+    for (; i + unroll - 1 < blob.size(); i += unroll) {
+        for (std::size_t j = 0; j < unroll; ++j) {
+            accs[j] += blob[i + j];
+            accs[j] += accs[j] < blob[i + j];
+        }
+    }
+    for (; i < blob.size(); ++i) {
+        accs[0] += blob[i];
+        accs[0] += accs[0] < blob[i];
+    }
+    for (std::size_t j = 1; j < unroll; ++j) {
+        accs[0] += accs[j];
+        accs[0] += accs[0] < accs[j];
+    }
+    return accs[0];
 }
