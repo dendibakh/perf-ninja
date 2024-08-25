e64af2a2271d757c88b67b145dd66e249f0b276b
JonathanHallstrom <lmj.hallstrom@gmail.com>
2024-08-25 02:53:32 +0200

    solve vectorization_2

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,10 +1,23 @@
 #include "solution.hpp"
+#include <limits>
 
 uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
-  for (auto value : blob) {
-    acc += value;
-    acc += acc < value; // add carry
-  }
-  return acc;
+    constexpr int unroll = 16;
+	uint16_t accs[unroll]{};
+	std::size_t i = 0; 
+	for (; i + unroll - 1 < blob.size(); i += unroll) {
+		for (std::size_t j = 0; j < unroll; ++j) {
+			accs[j] += blob[i + j];
+			accs[j] += accs[j] < blob[i + j];
+		}
+	}
+	for (; i < blob.size(); ++i) {
+		accs[0] += blob[i];
+		accs[0] += accs[0] < blob[i];
+	}
+	for (std::size_t j = 1; j < unroll; ++j) {
+		accs[0] += accs[j];
+		accs[0] += accs[0] < accs[j];
+	}
+	return accs[0];
 }
