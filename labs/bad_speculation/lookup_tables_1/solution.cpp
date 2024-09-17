e347756ed5b63538e9c0cf8c99b068303c394fe8
Denis Bakhvalov <dendibakh@gmail.com>
2024-09-17 12:17:20 -0400

    [LookupTables1] Simplified comparisons

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -1,13 +1,14 @@
 #include "solution.hpp"
 
-static std::size_t mapToBucket(std::size_t v) {    // diff
-  if (v >= 0 && v < 13)        return 0; // 13
-  else if (v >= 13 && v < 29)  return 1; // 16
-  else if (v >= 29 && v < 41)  return 2; // 12
-  else if (v >= 41 && v < 53)  return 3; // 12
-  else if (v >= 53 && v < 71)  return 4; // 18
-  else if (v >= 71 && v < 83)  return 5; // 12
-  else if (v >= 83 && v < 100) return 6; // 17
+static std::size_t mapToBucket(std::size_t v) {
+                              //   size of a bucket
+  if      (v < 13)  return 0; //   13
+  else if (v < 29)  return 1; //   16
+  else if (v < 41)  return 2; //   12
+  else if (v < 53)  return 3; //   12
+  else if (v < 71)  return 4; //   18
+  else if (v < 83)  return 5; //   12
+  else if (v < 100) return 6; //   17
   return -1;  // let it crash
 }
 
