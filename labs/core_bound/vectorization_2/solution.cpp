83dd7e14f3f2c9e4cbe2b768a742c1da1d928e42
Igor Shovkun <igshov@gmail.com>
2024-10-25 21:13:58 +0000

    solved lab2

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,5 +1,23 @@
 #include "solution.hpp"
+#include <limits>
+#include <iostream>
 
+#define SOLUTION
+
+#ifdef SOLUTION
+uint16_t checksum(const Blob &blob) {
+  uint32_t acc{0};
+  for (auto value : blob) {
+    acc += value;
+  }
+  constexpr uint32_t div = UINT16_MAX + 1;
+  while (acc > UINT16_MAX) {
+    acc = acc % div + acc / div;
+  }
+  return int16_t(acc);
+}
+
+#else
 uint16_t checksum(const Blob &blob) {
   uint16_t acc = 0;
   for (auto value : blob) {
@@ -8,3 +26,5 @@ uint16_t checksum(const Blob &blob) {
   }
   return acc;
 }
+
+#endif
