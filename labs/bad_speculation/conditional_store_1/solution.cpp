68e071c254ac5815d3aba0eeeb895882fd081c33
Roma <furkoroman95@gmail.com>
2024-10-26 23:44:43 -0700

    Conditional store lab solution

--- labs/bad_speculation/conditional_store_1/solution.cpp
+++ labs/bad_speculation/conditional_store_1/solution.cpp
@@ -5,10 +5,11 @@
 std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
                    const std::uint32_t lower, const std::uint32_t upper) {
   std::size_t count = 0;
-  for (const auto item : input) {
-    if ((lower <= item.first) && (item.first <= upper)) {
-      output[count++] = item;
-    }
+  bool mask = false;
+  for (const auto& item : input) {
+    mask = (lower <= item.first) & (item.first <= upper);
+    output[count] = item;
+    count += mask;
   }
   return count;
 }
