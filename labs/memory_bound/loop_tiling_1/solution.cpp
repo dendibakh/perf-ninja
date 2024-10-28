62335e0a14b4ecced5bd138e757a2e8f2b630f57
Roman <furkoroman95@gmail.com>
2024-10-27 19:37:14 -0700

    Loop tiling 1

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -3,10 +3,25 @@
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
+  int tile_size = 32;
+  for (int i = 0; i < size; i += tile_size) {
+    for (int j = 0; j < size; j += tile_size) {
+      int max_ii = std::min(i + tile_size, size);
+      int max_jj = std::min(j + tile_size, size);
+
+      for (int jj = j; jj < max_jj; jj++) {
+        for (int ii = i; ii < max_ii; ii++) {
+          __builtin_prefetch(&in[jj][ii + 1]);
+        }
+      }
+
+      for (int ii = i; ii < max_ii; ii++) {
+        for (int jj = j; jj < max_jj; jj++) {
+          out[ii][jj] = in[jj][ii];
+        }
+      }
     }
   }
+
   return out[0][size - 1];
 }
