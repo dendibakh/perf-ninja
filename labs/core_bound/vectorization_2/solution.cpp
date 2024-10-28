7a1b6a2b6604a664955cc48bc7c7b67d78a6709d
Roman <furkoroman95@gmail.com>
2024-10-27 19:06:48 -0700

    Vectorization 2 solution

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,10 +1,11 @@
 #include "solution.hpp"
 
 uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
-  for (auto value : blob) {
-    acc += value;
-    acc += acc < value; // add carry
+  uint32_t acc32 = 0;
+  for (int i = 0; i < blob.size(); ++i) {
+    acc32 += blob[i];
   }
+
+  uint16_t acc = (uint16_t)(acc32 & 0xFFFF) + (uint16_t)(acc32 / 0xFFFF);
   return acc;
 }
