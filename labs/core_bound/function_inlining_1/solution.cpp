dd7a5d4e8c4183fff570be6e5fb7a215eed1ccba
Roman <furkoroman95@gmail.com>
2024-10-27 17:22:00 -0700

    Function inlining solution v2

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -5,6 +5,6 @@
 
 void solution(std::array<S, N> &arr) {
   std::sort(arr.begin(), arr.end(), [](const S &a, const S &b) {
-    return std::tie(a.key1, a.key2) < std::tie(b.key1, b.key2);
+    return std::tuple(a.key1, a.key2) < std::tuple(b.key1, b.key2);
   });
 }
