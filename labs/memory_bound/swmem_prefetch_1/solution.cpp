84247c2cc9a87251ab49ba22fdff13982614b736
Igor Shovkun <igshov@gmail.com>
2024-10-30 21:15:43 +0000

    17% speedup prefetching

--- labs/memory_bound/swmem_prefetch_1/solution.cpp
+++ labs/memory_bound/swmem_prefetch_1/solution.cpp
@@ -12,10 +12,27 @@ static int getSumOfDigits(int n) {
 int solution(const hash_map_t *hash_map, const std::vector<int> &lookups) {
   int result = 0;
 
+#ifdef SOLUTION
+  for (size_t i = 0; i < lookups.size(); ++i) {
+    auto val = lookups[i];
+    if (i + 1 < lookups.size()) {
+      __builtin_prefetch(hash_map->getPtr(lookups[i + 1]));
+    }
+    if (hash_map->find(val)) {
+      result += getSumOfDigits(val);
+    }
+  }
+
+  // for (int val : lookups) {
+  //   if (hash_map->find(val))
+  //     result += getSumOfDigits(val);
+  // }
+#else
   for (int val : lookups) {
     if (hash_map->find(val))
       result += getSumOfDigits(val);
   }
+#endif
 
   return result;
 }
--- labs/memory_bound/swmem_prefetch_1/solution.hpp
+++ labs/memory_bound/swmem_prefetch_1/solution.hpp
@@ -1,23 +1,30 @@
 #include <vector>
 #include <limits>
 
+#define SOLUTION
+
 static constexpr std::size_t HASH_MAP_SIZE = 32 * 1024 * 1024 - 5;
 static constexpr std::size_t NUMBER_OF_LOOKUPS = 1024 * 1024;
 
 class hash_map_t {
     static constexpr int UNUSED = std::numeric_limits<int>::max();
+  public:
     std::vector<int> m_vector;
     std::size_t N_Buckets;
 public:
     hash_map_t(std::size_t size) : m_vector(size, UNUSED), N_Buckets(size) {}
 
-    bool insert(int val) {
-        int bucket = val % N_Buckets;
-        if (m_vector[bucket] == UNUSED) {
-            m_vector[bucket] = val;
-            return true;
-        }
-        return false;
+  inline int const* getPtr(int val) const {
+    return &m_vector[val % N_Buckets];
+  }
+
+   bool insert(int val) {
+   int bucket = val % N_Buckets;
+      if (m_vector[bucket] == UNUSED) {
+          m_vector[bucket] = val;
+          return true;
+      }
+      return false;
     }
 
     bool find(int val) const {
