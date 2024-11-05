7a297e52d82d1c7e26fbce3fa54c2d178d417772
Igor Shovkun <igshov@gmail.com>
2024-11-05 05:25:40 +0000

    conditional store 2x speedup

--- labs/bad_speculation/conditional_store_1/solution.cpp
+++ labs/bad_speculation/conditional_store_1/solution.cpp
@@ -1,7 +1,25 @@
 
 #include "solution.h"
 
-// Select items which have S.first in range [lower..upper]
+#define SOLUTION
+
+#ifdef SOLUTION
+std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
+                   const std::uint32_t lower, const std::uint32_t upper) {
+  std::size_t count = 0;
+  for (const auto item : input) {
+    uint32_t cmp = (lower <= item.first) && (item.first <= upper);
+    S x{};
+    if (__builtin_unpredictable(cmp)) {
+        x = item;
+    }
+    output[count] = x;
+    count += cmp;
+  }
+  return count;
+}
+
+#else
 std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
                    const std::uint32_t lower, const std::uint32_t upper) {
   std::size_t count = 0;
@@ -12,3 +30,4 @@ std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
   }
   return count;
 }
+#endif
