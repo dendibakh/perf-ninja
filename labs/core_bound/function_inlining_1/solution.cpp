4e436ffb92b02d001e86e5a079014b37d347d4b0
Evgenii Stankevich <evgenii.st@miro.com>
2024-11-25 21:23:55 +0100

    Lab: core_bound/function_inlining_1

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -1,27 +1,9 @@
-
 #include "solution.h"
 #include <algorithm>
 #include <stdlib.h>
 
-static int compare(const void *lhs, const void *rhs) {
-  auto &a = *reinterpret_cast<const S *>(lhs);
-  auto &b = *reinterpret_cast<const S *>(rhs);
-
-  if (a.key1 < b.key1)
-    return -1;
-
-  if (a.key1 > b.key1)
-    return 1;
-
-  if (a.key2 < b.key2)
-    return -1;
-
-  if (a.key2 > b.key2)
-    return 1;
-
-  return 0;
-}
-
 void solution(std::array<S, N> &arr) {
-  qsort(arr.data(), arr.size(), sizeof(S), compare);
+  std::sort(arr.begin(), arr.end(), [](const S& a, const S& b) {
+    return a.key1 < b.key1 || (a.key1 == b.key1) && (a.key2 < b.key2);
+  });
 }
