b099d75cf16b3900c22e7788136447ee94e025bf
Evgenii Stankevich <evgenii.st@miro.com>
2024-11-30 23:02:48 +0100

    Lab: core_bound/dep_chains_1

--- labs/core_bound/dep_chains_1/solution.cpp
+++ labs/core_bound/dep_chains_1/solution.cpp
@@ -2,6 +2,9 @@
 #include <array>
 #include <iostream>
 
+#define N 4
+#define SOLUTION
+
 unsigned getSumOfDigits(unsigned n) {
   unsigned sum = 0;
   while (n != 0) {
@@ -20,6 +23,39 @@ unsigned getSumOfDigits(unsigned n) {
 // Hint: Traversing a linked list is a long data dependency chain:
 //       to get the node N+1 you need to retrieve the node N first.
 //       Think how you can execute multiple dependency chains in parallel.
+
+#ifdef SOLUTION
+unsigned solution(List *l1, List *l2) {
+  unsigned retVal = 0;
+  List *head2 = l2;
+  std::array<unsigned,N> elements;
+  while (l1) {
+    for (int i = 0; i < N; i++) {
+      if (l1) {
+        elements[i] = l1->value;
+        l1 = l1->next;
+      } else {
+        elements[i] = 0;
+      }
+    }
+    l2 = head2;
+    int found = 0;
+    while (l2) {
+      for (int i = 0; i < N; i++) {
+        if (l2->value == elements[i]) {
+          retVal += getSumOfDigits(elements[i]);
+          found++;
+          if (found == N) {
+            break;
+          }
+        }
+      }
+      l2 = l2->next;
+    }
+  }
+  return retVal;
+}
+#else
 unsigned solution(List *l1, List *l2) {
   unsigned retVal = 0;
 
@@ -40,3 +76,4 @@ unsigned solution(List *l1, List *l2) {
 
   return retVal;
 }
+#endif
