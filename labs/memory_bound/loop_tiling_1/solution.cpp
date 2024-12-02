a8f7b5e42b42ab6049112db0570318d058b5eae7
Rasmus Bons <rasmusbons@gmail.com>
2024-12-02 22:14:40 +0100

    Loop tiling 1

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -1,12 +1,24 @@
 #include "solution.hpp"
 #include <algorithm>
+#include <cstdio>
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
+  int count = 0;
+  int chunkSize = 32;
+  int chunkCount = size/chunkSize;
+  int stride = size/chunkCount;
+
+  for(int x = 0; x < size; x+=stride)
+  {
+    for (int i = 0; i < size; i++) 
+    {
+      for (int j = x; j < std::min(size, x+stride); j++) 
+      {
+        out[i][j] = in[j][i];
+      }
     }
   }
+
   return out[0][size - 1];
 }
