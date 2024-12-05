cc6b45aadb29a17742f8fba905e4acf1fe9093b5
Rasmus Bons <rasmusbons@gmail.com>
2024-12-05 11:16:15 +0100

    False sharing fix
    seems like the lib doesnt have the hardware_interference_size so just using 64 bytes instead

--- labs/memory_bound/false_sharing_1/solution.cpp
+++ labs/memory_bound/false_sharing_1/solution.cpp
@@ -3,13 +3,12 @@
 #include <cstring>
 #include <omp.h>
 #include <vector>
-#include <new>
 
 std::size_t solution(const std::vector<uint32_t> &data, int thread_count) {
   // Using std::atomic counters to disallow compiler to promote `target`
   // memory location into a register. This way we ensure that the store
   // to `target` stays inside the loop.
-  struct alignas(std::hardware_constructive_interference_size) Accumulator {
+  struct alignas(64) Accumulator {
     std::atomic<uint32_t> value = 0;
 
   };
