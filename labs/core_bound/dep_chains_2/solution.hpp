35596c4b4445894cbd8b7f301df48059d9c285c4
Evgenii Stankevich <evgenii.st@miro.com>
2024-12-07 18:48:10 +0100

    Lab: core_bound/dep_chains_2

--- labs/core_bound/dep_chains_2/solution.hpp
+++ labs/core_bound/dep_chains_2/solution.hpp
@@ -3,6 +3,8 @@
 #include <cstdint>
 #include <array>
 
+#define SOLUTION
+
 // The number of motion simulation steps.
 constexpr uint32_t STEPS = 10000;
 // The number of paticles to simulate.
@@ -53,6 +55,55 @@ constexpr float DEGREE_TO_RADIAN = (2 * PI_D) / UINT32_MAX;
 // Simulate the motion of the particles.
 // For every particle, we generate a random angle and move the particle
 // in the corresponding direction.
+#ifdef SOLUTION
+
+template <class RNG>
+void randomParticleMotion(std::vector<Particle> &particles, uint32_t seed1, uint32_t seed2) {
+  RNG rng1(seed1);
+  RNG rng2(seed2);
+  for (int i = 0; i < STEPS; i++)
+    for (int j = 0; j < particles.size() - 1; j += 2) {
+      uint32_t angle1 = rng1.gen();
+      float angle_rad1 = angle1 * DEGREE_TO_RADIAN;
+      particles[j].x += cosine(angle_rad1) * particles[j].velocity;
+      particles[j].y += sine(angle_rad1) * particles[j].velocity;
+      uint32_t angle2 = rng2.gen();
+      float angle_rad2 = angle2 * DEGREE_TO_RADIAN;
+      particles[j+1].x += cosine(angle_rad2) * particles[j+1].velocity;
+      particles[j+1].y += sine(angle_rad2) * particles[j+1].velocity;
+    }
+}
+
+template <class RNG>
+void randomParticleMotion(std::vector<Particle> &particles, uint32_t seed) {
+  randomParticleMotion<RNG>(particles, seed, seed+1);
+}
+
+
+// template <class RNG>
+// void randomParticleMotion(std::vector<Particle> &particles, uint32_t seed1, uint32_t seed2) {
+//   RNG rng1(seed1);
+//   RNG rng2(seed2);
+//   for (int i = 0; i < STEPS; i++)
+//     for (int j = 0; j < particles.size() - 1; j += 2) {
+//       moveParticle(particles[j], rng1.gen());
+//       moveParticle(particles[j+1], rng2.gen());
+//     }
+// }
+
+// inline void moveParticle(Particle &particle, uint32_t angle) {
+//   float angle_rad = angle * DEGREE_TO_RADIAN;
+//   particle.x += cosine(angle_rad) * particle.velocity;
+//   particle.y += sine(angle_rad) * particle.velocity;
+// }
+
+// template <class RNG>
+// void randomParticleMotion(std::vector<Particle> &particles, uint32_t seed) {
+//   randomParticleMotion<RNG>(particles, seed, seed+1);
+// }
+
+#else
+
 template <class RNG>
 void randomParticleMotion(std::vector<Particle> &particles, uint32_t seed) {
   RNG rng(seed);  
@@ -64,3 +115,4 @@ void randomParticleMotion(std::vector<Particle> &particles, uint32_t seed) {
       p.y += sine(angle_rad) * p.velocity;
     }
 }
+#endif
