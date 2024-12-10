c4d271f5f291f608a6fb0e3c851b2e69f8c1854c
Stephen Doyle <stephen.doyle@intel.com>
2024-12-10 14:45:14 +0000

    Refactor warmup solution

--- labs/misc/warmup/solution.cpp
+++ labs/misc/warmup/solution.cpp
@@ -2,10 +2,13 @@
 #include "solution.h"
 
 int solution(int *arr, int N) {
-  // int res = 0;
-  // for (int i = 0; i < N; i++) {
-  //   res += arr[i];
-  // }
-  // return res;
+#ifdef SOLUTION
   return (N * (N + 1)) / 2;
+#else
+  int res = 0;
+  for (int i = 0; i < N; i++) {
+    res += arr[i];
+  }
+  return res;
+#endif
 }
