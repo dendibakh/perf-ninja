d566ec745ebdabe57f0e3d13316439d0ca402d62
Evgenii Stankevich <evgenii.st@miro.com>
2024-12-15 20:10:16 +0100

    Lab: core_bound/compiler_intrinsics_1

--- labs/core_bound/compiler_intrinsics_1/solution.cpp
+++ labs/core_bound/compiler_intrinsics_1/solution.cpp
@@ -2,8 +2,16 @@
 #include "solution.h"
 #include <memory>
 
+#if defined(__ARM_NEON) || defined(__aarch64__)
+#include <arm_neon.h>
+#elif defined(__x86_64__) || defined(_M_X64) || defined(__i386__) || defined(_M_IX86)
+#include <emmintrin.h>
+#include <immintrin.h>
+#endif
+
 void imageSmoothing(const InputVector &input, uint8_t radius,
                     OutputVector &output) {
+
   int pos = 0;
   int currentSum = 0;
   int size = static_cast<int>(input.size());
@@ -22,6 +30,73 @@ void imageSmoothing(const InputVector &input, uint8_t radius,
 
   // 2. main loop.
   limit = size - radius;
+
+#if defined(__ARM_NEON) || defined(__aarch64__)
+  int i = 0;
+  const uint8_t* subPtr = input.data() + pos - radius - 1;
+  const uint8_t* addPtr = input.data() + pos + radius;
+  const uint16_t* outputPtr = output.data() + pos;
+
+  int16x8_t current = vdupq_n_s16(currentSum);
+  int16x8_t mask = vdupq_n_s16(0);
+
+  for (; i + 7 < limit - pos; i += 8) {
+    // 1. Calculate vector diff: input[i+radius] - input[i-radius-1]
+    uint8x8_t subtract = vld1_u8(subPtr + i);
+    uint8x8_t add = vld1_u8(addPtr + i);
+    int16x8_t sub_s = vreinterpretq_s16_u16(vmovl_u8(subtract));
+    int16x8_t add_s = vreinterpretq_s16_u16(vmovl_u8(add));
+
+    int16x8_t diff = vsubq_s16(add_s, sub_s);
+
+    // 2. Calculate vector prefix sum for 8 elements
+    int16x8_t s = diff;
+    s = vaddq_s16(s, vextq_s16(mask, s, 7));
+    s = vaddq_s16(s, vextq_s16(mask, s, 6));
+    s = vaddq_s16(s, vextq_s16(mask, s, 4));
+
+    // 3. Store the result
+    int16x8_t result = vaddq_s16(current, s);
+    vst1q_s16((int16_t*)(outputPtr + i), result);
+
+    // 4. Broadcast currentSum for the next iteration
+    currentSum = (uint16_t) vgetq_lane_s16(result, 7);
+    current = vdupq_n_s16(currentSum);
+  }
+  pos += i;
+#elif defined(__x86_64__) || defined(_M_X64) || defined(__i386__) || defined(_M_IX86)
+  const uint8_t* subtractPtr = input.data() + pos - radius - 1;
+  const uint8_t* addPtr = input.data() + pos + radius;
+  const uint16_t* outputPtr = output.data() + pos;
+  __m128i current = _mm_set1_epi16(currentSum);
+
+  int i = 0;
+  // loop processes 8 elements per iteration
+  for (; i + 7 < limit - pos; i += 8) {
+    // 1. Calculate vector diff: input[i+radius] - input[i-radius-1]
+    __m128i sub_u8 = _mm_loadu_si64(subtractPtr + i);
+    __m128i sub = _mm_cvtepu8_epi16(sub_u8);
+    __m128i add_u8 = _mm_loadu_si64(addPtr + i);
+    __m128i add = _mm_cvtepu8_epi16(add_u8);
+
+    __m128i diff = _mm_sub_epi16(add, sub);
+
+    // 2. Calculate vector prefix sum for 8 elements
+    __m128i s = _mm_add_epi16(diff, _mm_slli_si128(diff, 2));
+    s = _mm_add_epi16(s, _mm_slli_si128(s, 4));
+    s = _mm_add_epi16(s, _mm_slli_si128(s, 8));
+
+    // 3. Store the result
+    __m128i result = _mm_add_epi16(s, current);
+    _mm_storeu_si128((__m128i*)(outputPtr + i), result);
+
+    // 4. Broadcast currentSum for the next iteration
+    currentSum = (uint16_t)_mm_extract_epi16(result, 7);
+    current = _mm_set1_epi16(currentSum);
+  }
+  pos += i;
+#endif
+
   for (; pos < limit; ++pos) {
     currentSum -= input[pos - radius - 1];
     currentSum += input[pos + radius];
