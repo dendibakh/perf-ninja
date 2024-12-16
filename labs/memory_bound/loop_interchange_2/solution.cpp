efa1137bdbe0b322951c8bb59b85a82783420bbc
Simon Veitner <simp.veitner@gmail.com>
2024-12-16 23:22:46 +0100

    Used tip from lecture to process problematic code in 3 stages.

--- labs/memory_bound/loop_interchange_2/solution.cpp
+++ labs/memory_bound/loop_interchange_2/solution.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <fstream>
 #include <ios>
+#include <vector> 
 
 // Applies Gaussian blur in independent vertical lines
 static void filterVertically(uint8_t *output, const uint8_t *input,
@@ -29,19 +30,6 @@ static void filterVertically(uint8_t *output, const uint8_t *input,
       output[r * width + c] = static_cast<uint8_t>(value);
     }
 
-    // Middle part of computations with full kernel
-    for (int r = radius; r < height - radius; r++) {
-      // Accumulation
-      int dot = 0;
-      for (int i = 0; i < radius + 1 + radius; i++) {
-        dot += input[(r - radius + i) * width + c] * kernel[i];
-      }
-
-      // Fast shift instead of division
-      int value = (dot + rounding) >> shift;
-      output[r * width + c] = static_cast<uint8_t>(value);
-    }
-
     // Bottom part of line, partial kernel
     for (int r = std::max(radius, height - radius); r < height; r++) {
       // Accumulation
@@ -58,6 +46,31 @@ static void filterVertically(uint8_t *output, const uint8_t *input,
       int value = static_cast<int>(dot / static_cast<float>(sum) + 0.5f);
       output[r * width + c] = static_cast<uint8_t>(value);
     }
+  }
+
+  int* dot = new int[width * (height - 2 * radius)];
+
+  // Init
+  for (int i = 0; i < width * (height - 2 * radius); i++) {
+      dot[i] = 0;
+  }
+
+  // Accum
+  for (int r = radius; r < height - radius; r++) {
+      for (int i = 0; i < radius + 1 + radius; i++) {
+          for (int c = 0; c < width; c++) { 
+              dot[(r - radius) * width + c] += input[(r - radius + i) * width + c] * kernel[i];
+          }
+      }
+  }
+
+  // Output
+  for (int r = radius; r < height - radius; r++) {
+      for (int c = 0; c < width; c++) {
+          int dotIndex = (r - radius) * width + c;
+          int value = (dot[dotIndex] + rounding) >> shift;
+          output[r * width + c] = static_cast<uint8_t>(value);
+      }
   }
 }
 
