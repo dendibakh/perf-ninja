ce077526090aae9d4aef433bccdbd5e9d6246b4f
Simon Veitner <simp.veitner@gmail.com>
2024-12-19 22:31:27 +0100

    Lab function inlining 1

--- labs/core_bound/function_inlining_1/solution.h
+++ labs/core_bound/function_inlining_1/solution.h
@@ -9,6 +9,22 @@ constexpr size_t N = 10000;
 struct S {
   uint32_t key1;
   uint32_t key2;
+
+  bool operator<(const S &other) const {
+          if (key1 < other.key1) {
+            return true;
+          }
+          if (key1 > other.key1) {
+            return false;
+          }
+          if (key2 < other.key2) {
+            return true;
+          }
+          if (key2 > other.key2) {
+            return false;
+          }
+      }
+
 };
 
 void init(std::array<S, N> &arr);
