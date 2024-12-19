b5514b8221d4040e47b61dd4b3efd16c5815ec7b
Simon Veitner <simp.veitner@gmail.com>
2024-12-19 18:37:33 +0100

    Lab vectorization 2

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -2,9 +2,14 @@
 
 uint16_t checksum(const Blob &blob) {
   uint16_t acc = 0;
-  for (auto value : blob) {
+  Carry carry = {};
+  for (int i = 0; i < blob.size(); i++) {
+    uint16_t value = blob[i];
     acc += value;
-    acc += acc < value; // add carry
+    carry[i] = acc < value; // save carry
+  }
+  for (int i = 0; i < blob.size(); i++) {
+    acc += carry[i];
   }
   return acc;
 }
--- labs/core_bound/vectorization_2/solution.hpp
+++ labs/core_bound/vectorization_2/solution.hpp
@@ -6,6 +6,7 @@
 constexpr std::size_t N = 64 * 1024;
 
 using Blob = std::array<uint16_t, N>;
+using Carry = std::array<uint16_t, N>;
 
 void init(Blob &blob);
 uint16_t checksum(const Blob &blob);
