80bd2f1497caeea5afbe056ac47c2442704a3aee
Gco <bjgiacoppo@gmail.com>
2024-12-29 19:48:37 +1100

    lab core_bound vectorisation 2 - a lot faster not sure if fast enough yet

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -2,9 +2,12 @@
 
 uint16_t checksum(const Blob &blob) {
   uint16_t acc = 0;
+  uint32_t acc32 = 0;
   for (auto value : blob) {
-    acc += value;
-    acc += acc < value; // add carry
+    acc32 += value;
   }
+
+  acc = acc32 & 0xFFFF;
+  acc += acc32 >> 16;
   return acc;
 }
