60a9fd6eed695b82016a269c2966f63ad559616a
Gco <bjgiacoppo@gmail.com>
2024-12-29 21:01:11 +1100

    lab core_bound vectorisation 2 - fixup account for final overflow

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,13 +1,13 @@
 #include "solution.hpp"
 
 uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
   uint32_t acc32 = 0;
   for (auto value : blob) {
     acc32 += value;
   }
 
-  acc = acc32 & 0xFFFF;
-  acc += acc32 >> 16;
-  return acc;
+  while ((acc32 >> 16) != 0){
+    acc32 = (acc32 & 0xFFFF) + (acc32 >> 16);
+  }
+  return acc32;
 }
