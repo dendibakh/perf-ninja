d2c44ce3d9a557e43913be7627ace96ce2148f40
Christian Oliveros <christianol_01@hotmail.com>
2024-12-31 21:02:04 -0400

    Data Packing Done

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -11,11 +11,25 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
+#ifdef SOLUTION
+  double d;
+  long long l;
+  int i;
+  short s;
+  bool b;
+#elif defined(SOLUTION2)
+  float d;
+  unsigned long long l:16;
+  unsigned int i:7;
+  unsigned short s:7;
+  bool b:1;
+#else
   int i;
   long long l;
   short s;
   double d;
   bool b;
+#endif
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
