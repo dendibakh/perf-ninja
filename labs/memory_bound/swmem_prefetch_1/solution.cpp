7171d47d32eec50c16924ddb760fdcc1a74a396c
Christian Oliveros <christianol_01@hotmail.com>
2025-01-01 15:37:53 -0400

    SW Mem Prefetch 1

--- labs/memory_bound/swmem_prefetch_1/solution.cpp
+++ labs/memory_bound/swmem_prefetch_1/solution.cpp
@@ -12,10 +12,26 @@ static int getSumOfDigits(int n) {
 int solution(const hash_map_t *hash_map, const std::vector<int> &lookups) {
   int result = 0;
 
+#ifdef SOLUTION
+  const auto begin = lookups.cbegin();
+  const auto final_iter = --lookups.cend();
+  for (auto iter = begin; iter != final_iter; ++iter)
+  {
+    const auto next = std::next(iter);
+    hash_map->prefetch(*next);
+    const int val = *iter;
+    if (hash_map->find(val))
+      result += getSumOfDigits(val);
+  }
+  const int val = *final_iter;
+  if (hash_map->find(val))
+      result += getSumOfDigits(val);
+#else
   for (int val : lookups) {
     if (hash_map->find(val))
       result += getSumOfDigits(val);
   }
+#endif
 
   return result;
 }
--- labs/memory_bound/swmem_prefetch_1/solution.hpp
+++ labs/memory_bound/swmem_prefetch_1/solution.hpp
@@ -4,6 +4,28 @@
 static constexpr std::size_t HASH_MAP_SIZE = 32 * 1024 * 1024 - 5;
 static constexpr std::size_t NUMBER_OF_LOOKUPS = 1024 * 1024;
 
+// Force CI to compile solution
+#define SOLUTION
+
+#ifdef SOLUTION
+
+#ifdef _MSC_VER 
+// This means windows as well
+#define VC_EXTRALEAN
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+#include <Winnt.h>
+#undef VC_EXTRALEAN
+#undef WIN32_LEAN_AND_MEAN
+#undef NOMINMAX
+#define PREFETCH(a) PreFetchCacheLine(PF_TEMPORAL_LEVEL_1, (void *)(a))
+#else
+// For us this means GNU or Clang
+#define PREFETCH(a) __builtin_prefetch((void *)(a))
+#endif
+#endif
+
 class hash_map_t {
     static constexpr int UNUSED = std::numeric_limits<int>::max();
     std::vector<int> m_vector;
@@ -24,6 +46,16 @@ public:
         int bucket = val % N_Buckets;
         return m_vector[bucket] != UNUSED;
     }
+
+#ifdef SOLUTION
+
+    void prefetch(int val) const {
+        const int bucket = val % N_Buckets;
+        PREFETCH(m_vector.data() + bucket);
+    }
+
+#endif
+
 };
 
 void init(hash_map_t* hash_map, std::vector<int>& lookups);
