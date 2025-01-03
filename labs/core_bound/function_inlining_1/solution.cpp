38d9c77a2b8c6a9576b30b157aa1922b8a2b20df
Christian Oliveros <christianol_01@hotmail.com>
2025-01-02 21:50:10 -0400

    Function inlining 1

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -3,6 +3,25 @@
 #include <algorithm>
 #include <stdlib.h>
 
+// MSVC __forceinline
+// GNU [[gnu::always_inline]]
+
+#define SOLUTION
+#ifdef SOLUTION
+
+void solution(std::array<S, N> &arr) {
+  std::sort(
+    arr.begin(),
+    arr.end(),
+    [](const S& a, const S&b)
+    {
+      return a.key1 < b.key1 || (a.key1 == b.key1 && a.key2 < b.key2);
+    }
+  );
+}
+
+#else
+
 static int compare(const void *lhs, const void *rhs) {
   auto &a = *reinterpret_cast<const S *>(lhs);
   auto &b = *reinterpret_cast<const S *>(rhs);
@@ -25,3 +44,5 @@ static int compare(const void *lhs, const void *rhs) {
 void solution(std::array<S, N> &arr) {
   qsort(arr.data(), arr.size(), sizeof(S), compare);
 }
+
+#endif
