6c2a1b80f2c0785ae4b522171d5ed374a3efbe17
badguy <bad@guy.com>
2025-01-03 19:51:24 +1100

    virtual_call_mispredict sort so virtual calls are predictable.. I guess

--- labs/bad_speculation/virtual_call_mispredict/solution.cpp
+++ labs/bad_speculation/virtual_call_mispredict/solution.cpp
@@ -1,6 +1,8 @@
 #include "solution.h"
 
 #include <random>
+#include <algorithm>
+#include <typeindex>
 
 void generateObjects(InstanceArray& array) {
     std::default_random_engine generator(0);
@@ -16,10 +18,17 @@ void generateObjects(InstanceArray& array) {
             array.push_back(std::make_unique<ClassC>());
         }
     }
+
+    //seems bit werid this as the solution,
+    //if can do this might as well remove the pointers as well, and make this not virtual classes
+    std::sort(array.begin(), array.end(), [](auto& left, auto & right){
+      return std::type_index(typeid(*(left.get()))) < std::type_index(typeid(*(right.get())));
+    });
 }
 
 // Invoke the `handle` method on all instances in `output`
 void invoke(InstanceArray& array, std::size_t& data) {
+
     for (const auto& item: array) {
         item->handle(data);
     }
