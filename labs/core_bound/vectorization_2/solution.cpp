1cf4d687284af0e1e3f76ead24e85a71fa6a3eec
Christian Oliveros <christianol_01@hotmail.com>
2025-01-03 20:13:28 -0400

    Vectorization 2

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,5 +1,20 @@
 #include "solution.hpp"
 
+#define SOLUTION
+#ifdef SOLUTION
+uint16_t checksum(const Blob &blob) {
+  uint64_t acc = 0;
+  for (auto value : blob) {
+    acc += value;
+  }
+
+  constexpr uint64_t mask = (1 << 16) - 1;
+
+  acc = (acc & mask) + (acc >> 16);
+  acc = (acc & mask) + (acc >> 16);
+  return (uint16_t)acc;
+}
+#else
 uint16_t checksum(const Blob &blob) {
   uint16_t acc = 0;
   for (auto value : blob) {
@@ -8,3 +23,4 @@ uint16_t checksum(const Blob &blob) {
   }
   return acc;
 }
+#endif
