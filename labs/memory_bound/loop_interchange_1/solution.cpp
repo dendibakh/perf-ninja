0aa5cebe0c6392ec20683da8c0205115137c8bcb
Oleg Makovski <oleg.makovski@gmail.com>
2025-01-26 03:18:00 +0200

    loop_interchange_1 assignment

--- labs/memory_bound/loop_interchange_1/solution.cpp
+++ labs/memory_bound/loop_interchange_1/solution.cpp
@@ -27,8 +27,8 @@ void multiply(Matrix &result, const Matrix &a, const Matrix &b) {
   zero(result);
 
   for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
-      for (int k = 0; k < N; k++) {
+    for (int k = 0; k < N; k++) {
+      for (int j = 0; j < N; j++) {
         result[i][j] += a[i][k] * b[k][j];
       }
     }
