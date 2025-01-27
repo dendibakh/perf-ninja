770bb56a2636314d785f4a515854fc37babf65fa
Oleg Makovski <omakovski@yandex-team.ru>
2025-01-27 03:25:14 +0200

    loop_tiling

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -3,9 +3,14 @@
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
+  const int tile_size = 32;
+  for (int i = 0; i < size; i += tile_size) {
+    for (int j = 0; j < size; j += tile_size) {
+      for (int r = i; r < std::min(size, i + tile_size); ++r) {
+        for (int c = j; c < std::min(size, j + tile_size); ++c) {
+          out[r][c] = in[c][r];
+        }
+      }
     }
   }
   return out[0][size - 1];
