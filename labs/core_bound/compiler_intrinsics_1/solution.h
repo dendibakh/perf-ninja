7cde85bc5a3bbe8bf98d5f00b383b166bab68c6a
Rasmus Bons <rasmusbons@gmail.com>
2025-01-31 12:45:00 +0100

    Solution for Compiler intrinsic
    The point was to know there was an algoirthm called prefix sum and then making it into an SIMD version by taking 8 at a time

--- labs/core_bound/compiler_intrinsics_1/solution.h
+++ labs/core_bound/compiler_intrinsics_1/solution.h
@@ -6,9 +6,6 @@ using InputVector = std::vector<uint8_t>;
 using OutputVector = std::vector<uint16_t>;
 constexpr uint8_t radius = 15; // assume diameter (2 * radius + 1) to be less
                                // than 256 so results fits in uint16_t
-constexpr uint8_t diameter = 2 * radius + 1;
-constexpr uint8_t load_size = 32;
-constexpr uint8_t diff = load_size - diameter;
 
 void init(InputVector &data);
 void zero(OutputVector &data, std::size_t size);
