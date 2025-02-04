0b18db4302cf74f8b7619477e1de6cefa048a75f
Alan Richardson <arichar@tcd.ie>
2025-02-04 15:08:13 +0000

    swmem_prefetch_1

--- labs/memory_bound/swmem_prefetch_1/solution.cpp
+++ labs/memory_bound/swmem_prefetch_1/solution.cpp
@@ -1,5 +1,7 @@
 #include "solution.hpp"
 
+#define PREFETCH_DIST 16
+
 static int getSumOfDigits(int n) {
   int sum = 0;
   while (n != 0) {
@@ -12,7 +14,9 @@ static int getSumOfDigits(int n) {
 int solution(const hash_map_t *hash_map, const std::vector<int> &lookups) {
   int result = 0;
 
-  for (int val : lookups) {
+  for (int i{}; i < lookups.size(); ++i) {
+    int val = lookups[i];
+    if (i + PREFETCH_DIST < lookups.size()) hash_map->prefetch(lookups[i+PREFETCH_DIST]);
     if (hash_map->find(val))
       result += getSumOfDigits(val);
   }
