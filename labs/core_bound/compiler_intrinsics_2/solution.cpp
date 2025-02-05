e9426eb780dbbff8f331209b142a601bd76e382a
Rasmus Bons <rasmusbons@gmail.com>
2025-02-05 12:01:10 +0100

    Compiler intrinsic 2

--- labs/core_bound/compiler_intrinsics_2/solution.cpp
+++ labs/core_bound/compiler_intrinsics_2/solution.cpp
@@ -1,5 +1,35 @@
 #include "solution.hpp"
-#include <iostream>
+
+#include <immintrin.h>
+#include <cstdio>
+
+
+
+void print_epi8(__m256i vec)
+{
+  uint8_t vals[32];
+  _mm256_storeu_si256(reinterpret_cast<__m256i*>(vals), vec);
+  for(int i = 0; i < 32;i++)
+  {
+    printf("%u, ", vals[i]);
+  }
+  printf("\n");
+}
+
+void print_binary(unsigned int value)
+{
+  
+  for(unsigned int i= 1 << 31; i > 0; i /= 2)
+  {
+    if(value & i)
+    {
+      printf("1, ");
+    }
+    else
+      printf("0, ");
+  }
+  printf("\n");
+}
 
 // Find the longest line in a file.
 // Implementation uses ternary operator with a hope that compiler will
@@ -12,14 +42,48 @@ if (s == '\n') {
 } else {
   curLineLength++;
 }*/
+
+
 unsigned solution(const std::string &inputContents) {
   unsigned longestLine = 0;
   unsigned curLineLength = 0;
+  
+  #define SOLUTION
 
-  for (auto s : inputContents) {
+  #ifdef SOLUTION
+  unsigned int index = 0;
+
+
+  for(; index+31 < inputContents.size(); index+= 32)
+  {
+      __m256i data = _mm256_loadu_si256 ((__m256i*)&inputContents[index]);
+      __m256i mask = _mm256_set1_epi8('\n');
+
+      __m256i mask_off = _mm256_cmpeq_epi8(data, mask);
+
+      // Dumping the mask
+      unsigned int dumped_mask = _mm256_movemask_epi8(mask_off);
+      unsigned int trailing_zeros = 0;
+      unsigned int o = 0;
+      for(unsigned int shifted_mask = dumped_mask; shifted_mask > 0; shifted_mask >>= (trailing_zeros + 1))
+      {
+        trailing_zeros = _tzcnt_u32(shifted_mask);
+        longestLine = std::max(curLineLength + trailing_zeros, longestLine);
+        curLineLength = 0;
+      }
+      curLineLength += _lzcnt_u32(dumped_mask);
+  }
+  for (;index < inputContents.size(); index++) 
+  {
+    curLineLength = (inputContents[index] == '\n') ? 0 : curLineLength + 1;
+    longestLine = std::max(curLineLength, longestLine);
+  }
+  #else
+  for (auto s : inputContents) 
+  {
     curLineLength = (s == '\n') ? 0 : curLineLength + 1;
     longestLine = std::max(curLineLength, longestLine);
   }
-
+  #endif
   return longestLine;
 }
