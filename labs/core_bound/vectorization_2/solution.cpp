b62122bf127effca3c440546174aba5f08bc476f
Alan Richardson <arichar@tcd.ie>
2025-02-07 19:43:00 +0000

    vectorization_2

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,10 +1,23 @@
 #include "solution.hpp"
 
+#define N 16
+
 uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
-  for (auto value : blob) {
-    acc += value;
-    acc += acc < value; // add carry
-  }
-  return acc;
+	uint16_t acc[N]{};
+	int i{};
+	for (; i + N <= blob.size(); i += N) {
+		for (int j{}; j < N; ++j) {
+			acc[j] += blob[i+j];
+			acc[j] += acc[j] < blob[i+j];
+		}
+	}
+	for (; i < blob.size(); ++i) {
+		acc[0] += blob[i];
+		acc[0] += acc[0] < blob[i];
+	}
+	for (int j{1}; j < N; ++j) {
+		acc[0] += acc[j];
+		acc[0] += acc[0] < acc[j];
+	}
+	return acc[0];
 }
