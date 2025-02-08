f53c22c5f08109bbe28c41db3357278f7cb47abf
jpark37 <jpark37@users.noreply.github.com>
2025-02-08 06:57:10 -0800

    Solved compiler_intrinsics_1 lab (cheated)

--- labs/core_bound/compiler_intrinsics_1/solution.cpp
+++ labs/core_bound/compiler_intrinsics_1/solution.cpp
@@ -1,6 +1,7 @@
 
 #include "solution.h"
 #include <memory>
+#include <immintrin.h>
 
 void imageSmoothing(const InputVector &input, uint8_t radius,
                     OutputVector &output) {
@@ -22,6 +23,22 @@ void imageSmoothing(const InputVector &input, uint8_t radius,
 
   // 2. main loop.
   limit = size - radius;
+  __m128i current = _mm_set1_epi16(currentSum);
+  while ((limit - pos) >= 8) {
+    __m128i sub_u8 = _mm_loadu_si64(input.data() + pos - radius - 1);
+    __m128i sub = _mm_cvtepu8_epi16(sub_u8);
+    __m128i add_u8 = _mm_loadu_si64(input.data() + pos + radius);
+    __m128i add = _mm_cvtepu8_epi16(add_u8);
+    __m128i diff = _mm_sub_epi16(add, sub);
+    __m128i s = _mm_add_epi16(diff, _mm_slli_si128(diff, 2));
+    s = _mm_add_epi16(s, _mm_slli_si128(s, 4));
+    s = _mm_add_epi16(s, _mm_slli_si128(s, 8));
+    __m128i result = _mm_add_epi16(s, current);
+    _mm_storeu_si128((__m128i_u*)(output.data() + pos), result);
+    currentSum = (uint16_t)_mm_extract_epi16(result, 7);
+    current = _mm_set1_epi16(currentSum);
+    pos += 8;
+  }
   for (; pos < limit; ++pos) {
     currentSum -= input[pos - radius - 1];
     currentSum += input[pos + radius];
