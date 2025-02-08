ff49398524343371e24e76ade774f51681c6bc42
Alan Richardson <arichar@tcd.ie>
2025-02-08 11:09:03 +0000

    dep_chains_1

--- labs/core_bound/dep_chains_1/solution.cpp
+++ labs/core_bound/dep_chains_1/solution.cpp
@@ -3,12 +3,12 @@
 #include <iostream>
 
 unsigned getSumOfDigits(unsigned n) {
-  unsigned sum = 0;
-  while (n != 0) {
-    sum = sum + n % 10;
-    n = n / 10;
-  }
-  return sum;
+	unsigned sum = 0;
+	while (n != 0) {
+		sum = sum + n % 10;
+		n = n / 10;
+	}
+	return sum;
 }
 
 // Task: lookup all the values from l2 in l1.
@@ -20,23 +20,29 @@ unsigned getSumOfDigits(unsigned n) {
 // Hint: Traversing a linked list is a long data dependency chain:
 //       to get the node N+1 you need to retrieve the node N first.
 //       Think how you can execute multiple dependency chains in parallel.
+#define L1N 64
 unsigned solution(List *l1, List *l2) {
-  unsigned retVal = 0;
+	unsigned retVal = 0;
+	List *head2 = l2;
+	while (l1) {
+		unsigned v1[L1N]{};
+		for (int i{}; i < L1N and l1; ++i) {
+			v1[i] = l1->value;
+			l1 = l1->next;
+		}
+		l2 = head2;
+		int n_found{};
+		while (l2 and n_found < L1N) {
+			for (int i{}; i < L1N; ++i) {
+				if (l2->value == v1[i]) {
+					retVal += getSumOfDigits(l2->value);
+					++n_found;
+					break;
+				}
+			}
+			l2 = l2->next;
+		}
+	}
 
-  List *head2 = l2;
-  // O(N^2) algorithm:
-  while (l1) {
-    unsigned v = l1->value;
-    l2 = head2;
-    while (l2) {
-      if (l2->value == v) {
-        retVal += getSumOfDigits(v);
-        break;
-      }
-      l2 = l2->next;
-    }
-    l1 = l1->next;
-  }
-
-  return retVal;
+	return retVal;
 }
