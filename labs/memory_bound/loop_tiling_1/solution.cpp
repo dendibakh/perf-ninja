ef1a79247e7dab49d4f0612bed49b0650e398c71
jpark37 <jpark37@users.noreply.github.com>
2025-02-08 10:24:12 -0800

    Solved loop_tiling_1 lab

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -1,11 +1,26 @@
 #include "solution.hpp"
 #include <algorithm>
+#include <cstdio>
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
+  constexpr int TILE_SIZE = 64;
+  const int sizeInTiles = (size + TILE_SIZE - 1) / TILE_SIZE;
+  for (int tileY = 0; tileY < sizeInTiles; ++tileY) {
+    for (int tileX = 0; tileX < sizeInTiles; ++tileX) {
+      for (int y = 0; y < TILE_SIZE; ++y) {
+        const int finalY = tileY * TILE_SIZE + y;
+        if (finalY == size) {
+          break;
+        }
+        for (int x = 0; x < TILE_SIZE; ++x) {
+          const int finalX = tileX * TILE_SIZE + x;
+          if (finalX == size) {
+            break;
+          }
+          out[finalY][finalX] = in[finalX][finalY];
+        }
+      }
     }
   }
   return out[0][size - 1];
