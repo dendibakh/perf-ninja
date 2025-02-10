e9130c9fc8da33594354f92ae439626a66830632
Alan Richardson <arichar@tcd.ie>
2025-02-10 20:20:54 +0000

    compiler_intrinsics_1

--- labs/core_bound/compiler_intrinsics_1/solution.cpp
+++ labs/core_bound/compiler_intrinsics_1/solution.cpp
@@ -1,6 +1,26 @@
 
 #include "solution.h"
 #include <memory>
+#include <emmintrin.h>
+#include <immintrin.h>
+#include <iostream>
+
+static int accumulate(uint16_t *output, int currentSum) {
+  // init all elements of current to currentSum
+  __m128i current{_mm_set1_epi16(currentSum)};
+
+	// Load 4 uint16 to add
+	__m128i add16{_mm_loadu_si128((__m128i const*)output)};
+
+	// add prefix sum to current
+	add16 = _mm_add_epi16(current, add16);
+
+	// store
+	_mm_storeu_si128((__m128i *)output, add16);
+
+	// extract final element for next iter
+	return _mm_extract_epi16(add16, 7);
+}
 
 void imageSmoothing(const InputVector &input, uint8_t radius,
                     OutputVector &output) {
@@ -22,6 +42,33 @@ void imageSmoothing(const InputVector &input, uint8_t radius,
 
   // 2. main loop.
   limit = size - radius;
+  
+  for (; pos + 15 < limit; pos += 16) {
+	  // Load 16 uint8 to add and sub
+	  __m128i add8{_mm_loadu_si128((__m128i const*)&input[pos + radius])};
+	  __m128i sub8{_mm_loadu_si128((__m128i const*)&input[pos - radius - 1])};
+
+          // convert to uint16
+	  __m256i add16{_mm256_cvtepu8_epi16(add8)};
+	  __m256i sub16{_mm256_cvtepu8_epi16(sub8)};
+
+	  // sub subs from adds
+	  __m256i diff{_mm256_sub_epi16(add16, sub16)};
+
+	  // prefix sum (add after shifting 1, 2, 4 elements)
+	  diff = _mm256_add_epi16(diff, _mm256_slli_si256(diff, 2));
+	  diff = _mm256_add_epi16(diff, _mm256_slli_si256(diff, 4));
+	  diff = _mm256_add_epi16(diff, _mm256_slli_si256(diff, 8));
+
+	// store
+	_mm256_storeu_si256((__m256i *)&output[pos], diff);
+
+	// accumulate to compensate for lack of shift between two 128 bit
+	// sections
+	currentSum = accumulate(&output[pos], currentSum);
+	currentSum = accumulate(&output[pos + 8], currentSum);
+  }
+
   for (; pos < limit; ++pos) {
     currentSum -= input[pos - radius - 1];
     currentSum += input[pos + radius];
@@ -40,4 +87,5 @@ void imageSmoothing(const InputVector &input, uint8_t radius,
     currentSum -= input[pos - radius - 1];
     output[pos] = currentSum;
   }
+
 }
