d5906fd47059533bfb6833b6ac3c07bbfa334b09
Alan Richardson <arichar@tcd.ie>
2025-02-11 14:55:38 +0000

    lookup_tables_1

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -1,6 +1,18 @@
 #include "solution.hpp"
 
+static unsigned char maparray[100]{
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+	3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+	4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+	5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+	6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6
+};
+
 static std::size_t mapToBucket(std::size_t v) {
+  return v < 100 ? maparray[v] : DEFAULT_BUCKET;
+#ifdef ORIGINAL
                               //   size of a bucket
   if      (v < 13)  return 0; //   13
   else if (v < 29)  return 1; //   16
@@ -10,6 +22,7 @@ static std::size_t mapToBucket(std::size_t v) {
   else if (v < 83)  return 5; //   12
   else if (v < 100) return 6; //   17
   return DEFAULT_BUCKET;
+#endif
 }
 
 std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
