f20d395a4d7d4681bfc516f99cdef7a5717f2575
Alan Richardson <arichar@tcd.ie>
2025-02-11 14:17:22 +0000

    branches_to_cmov_1 v2

--- labs/bad_speculation/branches_to_cmov_1/solution.hpp
+++ labs/bad_speculation/branches_to_cmov_1/solution.hpp
@@ -8,66 +8,185 @@ constexpr int NumberOfSims = 10;
 
 class Life {
 
-public:
-    using Grid = std::vector<std::vector<int>>;
-private:    
-    Grid current;
-    Grid future;
+	public:
+		using Grid = std::vector<std::vector<int>>;
+	private:    
+		Grid current;
+		Grid future;
 
-public:
+	public:
 
-    void reset(const Grid& grid) {
-        current = future = grid;
-    }
+		void reset(const Grid& grid) {
+			current = future = grid;
+		}
 
-    int getPopulationCount() {
-        int populationCount = 0;
-        for (auto& row: current)
-            for (auto& item: row)
-                populationCount += item;
-        return populationCount;
-    }
+		int getPopulationCount() {
+			int populationCount = 0;
+			for (auto& row: current)
+				for (auto& item: row)
+					populationCount += item;
+			return populationCount;
+		}
 
-    void printCurrentGrid() {
-        for (auto& row: current) {
-            for (auto& item: row)
-                item ? std::cout << "x " : std::cout << ". ";
-            std::cout << "\n";
-        }
-        std::cout << "\n";
-    }    
+		void printCurrentGrid() {
+			for (auto& row: current) {
+				for (auto& item: row)
+					item ? std::cout << "x " : std::cout << ". ";
+				std::cout << "\n";
+			}
+			std::cout << "\n";
+		}    
 
-    // Simulate the next generation of life
-    void simulateNext() {
-        //printCurrentGrid();
-        int M = current.size();
-        int N = current[0].size();
-        
-        // Loop through every cell
-        for(int i = 0; i < M; i++) {
-            for(int j = 0; j < N; j++) {
-                int aliveNeighbours = 0;      
-                // finding the number of neighbours that are alive                  
-                for(int p = -1; p <= 1; p++) {              // row-offet (-1,0,1)
-                    for(int q = -1; q <= 1; q++) {          // col-offset (-1,0,1)
-                        if((i + p < 0) ||                   // if row offset less than UPPER boundary
-                           (i + p > M - 1) ||               // if row offset more than LOWER boundary
-                           (j + q < 0) ||                   // if column offset less than LEFT boundary
-                           (j + q > N - 1))                 // if column offset more than RIGHT boundary
-                            continue;
-                        aliveNeighbours += current[i + p][j + q];
-                    }
-                }
-                // The cell needs to be subtracted from
-                // its neighbours as it was counted before
-                aliveNeighbours -= current[i][j];
+		// Simulate the next generation of life
+		void simulateNext() {
+			//printCurrentGrid();
+			int M = current.size();
+			int N = current[0].size();
 
-                // Implementing the Rules of Life:
-		future[i][j] = 1 < ((unsigned)aliveNeighbours - 2) ? 0 : aliveNeighbours == 2 ? current[i][j] : 1;
-            }
-        }
-        std::swap(current, future);
-    }
+			// Loop through every cell
+			int i = 0;
+			int j = 0;
+			int aliveNeighbours = 0;      
+			// finding the number of neighbours that are alive                  
+			for(int p = 0; p <= 1; p++) {              // row-offet (-1,0,1)
+				for(int q = 0; q <= 1; q++) {          // col-offset (-1,0,1)
+					aliveNeighbours += current[i + p][j + q];
+				}
+			}
+			// The cell needs to be subtracted from
+			// its neighbours as it was counted before
+			aliveNeighbours -= current[i][j];
+
+			// Implementing the Rules of Life:
+			future[i][j] = 1 < ((unsigned)aliveNeighbours - 2) ? 0 : aliveNeighbours == 2 ? current[i][j] : 1;
+
+			for(j = 1; j < N-1; j++) {
+				aliveNeighbours = 0;      
+				// finding the number of neighbours that are alive                  
+				for(int p = 0; p <= 1; p++) {              // row-offet (-1,0,1)
+					for(int q = -1; q <= 1; q++) {          // col-offset (-1,0,1)
+						aliveNeighbours += current[i + p][j + q];
+					}
+				}
+				// The cell needs to be subtracted from
+				// its neighbours as it was counted before
+				aliveNeighbours -= current[i][j];
+
+				// Implementing the Rules of Life:
+				future[i][j] = 1 < ((unsigned)aliveNeighbours - 2) ? 0 : aliveNeighbours == 2 ? current[i][j] : 1;
+			}
+			j = N - 1;
+			aliveNeighbours = 0;      
+			// finding the number of neighbours that are alive                  
+			for(int p = 0; p <= 1; p++) {              // row-offet (-1,0,1)
+				for(int q = -1; q <= 0; q++) {          // col-offset (-1,0,1)
+					aliveNeighbours += current[i + p][j + q];
+				}
+			}
+			// The cell needs to be subtracted from
+			// its neighbours as it was counted before
+			aliveNeighbours -= current[i][j];
+
+			// Implementing the Rules of Life:
+			future[i][j] = 1 < ((unsigned)aliveNeighbours - 2) ? 0 : aliveNeighbours == 2 ? current[i][j] : 1;
+
+
+			for(i = 1; i < M-1; i++) {
+				j = 0;
+				aliveNeighbours = 0;      
+				// finding the number of neighbours that are alive                  
+				for(int p = -1; p <= 1; p++) {              // row-offet (-1,0,1)
+					for(int q = 0; q <= 1; q++) {          // col-offset (-1,0,1)
+						aliveNeighbours += current[i + p][j + q];
+					}
+				}
+				// The cell needs to be subtracted from
+				// its neighbours as it was counted before
+				aliveNeighbours -= current[i][j];
+
+				// Implementing the Rules of Life:
+				future[i][j] = 1 < ((unsigned)aliveNeighbours - 2) ? 0 : aliveNeighbours == 2 ? current[i][j] : 1;
+
+				for(j = 1; j < N-1; j++) {
+					aliveNeighbours = 0;      
+					// finding the number of neighbours that are alive                  
+					for(int p = -1; p <= 1; p++) {              // row-offet (-1,0,1)
+						for(int q = -1; q <= 1; q++) {          // col-offset (-1,0,1)
+							aliveNeighbours += current[i + p][j + q];
+						}
+					}
+					// The cell needs to be subtracted from
+					// its neighbours as it was counted before
+					aliveNeighbours -= current[i][j];
+
+					// Implementing the Rules of Life:
+					future[i][j] = 1 < ((unsigned)aliveNeighbours - 2) ? 0 : aliveNeighbours == 2 ? current[i][j] : 1;
+				}
+				j = N - 1;
+				aliveNeighbours = 0;      
+				// finding the number of neighbours that are alive                  
+				for(int p = -1; p <= 1; p++) {              // row-offet (-1,0,1)
+					for(int q = -1; q <= 0; q++) {          // col-offset (-1,0,1)
+						aliveNeighbours += current[i + p][j + q];
+					}
+				}
+				// The cell needs to be subtracted from
+				// its neighbours as it was counted before
+				aliveNeighbours -= current[i][j];
+
+				// Implementing the Rules of Life:
+				future[i][j] = 1 < ((unsigned)aliveNeighbours - 2) ? 0 : aliveNeighbours == 2 ? current[i][j] : 1;
+
+			}
+			i = M-1;
+			j = 0;
+			aliveNeighbours = 0;      
+			// finding the number of neighbours that are alive                  
+			for(int p = -1; p <= 0; p++) {              // row-offet (-1,0,1)
+				for(int q = 0; q <= 1; q++) {          // col-offset (-1,0,1)
+					aliveNeighbours += current[i + p][j + q];
+				}
+			}
+			// The cell needs to be subtracted from
+			// its neighbours as it was counted before
+			aliveNeighbours -= current[i][j];
+
+			// Implementing the Rules of Life:
+			future[i][j] = 1 < ((unsigned)aliveNeighbours - 2) ? 0 : aliveNeighbours == 2 ? current[i][j] : 1;
+
+			for(j = 1; j < N-1; j++) {
+				aliveNeighbours = 0;      
+				// finding the number of neighbours that are alive                  
+				for(int p = -1; p <= 0; p++) {              // row-offet (-1,0,1)
+					for(int q = -1; q <= 1; q++) {          // col-offset (-1,0,1)
+						aliveNeighbours += current[i + p][j + q];
+					}
+				}
+				// The cell needs to be subtracted from
+				// its neighbours as it was counted before
+				aliveNeighbours -= current[i][j];
+
+				// Implementing the Rules of Life:
+				future[i][j] = 1 < ((unsigned)aliveNeighbours - 2) ? 0 : aliveNeighbours == 2 ? current[i][j] : 1;
+			}
+			j = N - 1;
+			aliveNeighbours = 0;      
+			// finding the number of neighbours that are alive                  
+			for(int p = -1; p <= 0; p++) {              // row-offet (-1,0,1)
+				for(int q = -1; q <= 0; q++) {          // col-offset (-1,0,1)
+					aliveNeighbours += current[i + p][j + q];
+				}
+			}
+			// The cell needs to be subtracted from
+			// its neighbours as it was counted before
+			aliveNeighbours -= current[i][j];
+
+			// Implementing the Rules of Life:
+			future[i][j] = 1 < ((unsigned)aliveNeighbours - 2) ? 0 : aliveNeighbours == 2 ? current[i][j] : 1;
+
+
+			std::swap(current, future);
+		}
 };
 
 // Init random starting grid of the game
