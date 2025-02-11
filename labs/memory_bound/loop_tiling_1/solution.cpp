bda480dcf14f4c39abfda392b68c7cd3fc2212c0
Alan Richardson <arichar@tcd.ie>
2025-02-11 12:55:31 +0000

    loop_tiling_1

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -1,12 +1,31 @@
 #include "solution.hpp"
 #include <algorithm>
 
+#define B 8
+
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
-  int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
-    }
-  }
-  return out[0][size - 1];
+	int size = in.size();
+	int i = 0;
+	for (; i + B - 1 < size; i+=B) {
+		int j = 0;
+		for (; j + B - 1 < size; j+=B) {
+			for (int ib = i; ib < i + B; ++ib) {
+				for (int jb = j; jb < j + B; ++jb) {
+					out[ib][jb] = in[jb][ib];
+				}
+			}
+		}
+		for (int ib = i; ib < i + B; ++ib) {
+			for (int jb{j}; jb < size; jb++) {
+				out[ib][jb] = in[jb][ib];
+			}
+		}
+	}
+	for (; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			out[i][j] = in[j][i];
+		}
+	}
+
+	return out[0][size - 1];
 }
