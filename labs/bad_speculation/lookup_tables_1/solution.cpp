959a4b382c56259c0c04ab9953ec68582114bb81
Alan Richardson <arichar@tcd.ie>
2025-02-11 15:32:26 +0000

    lookup_tables_1 v2

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -1,6 +1,6 @@
 #include "solution.hpp"
 
-static unsigned char maparray[100]{
+static constexpr std::size_t maparray[100]{
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 	2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
@@ -12,17 +12,6 @@ static unsigned char maparray[100]{
 
 static std::size_t mapToBucket(std::size_t v) {
   return v < 100 ? maparray[v] : DEFAULT_BUCKET;
-#ifdef ORIGINAL
-                              //   size of a bucket
-  if      (v < 13)  return 0; //   13
-  else if (v < 29)  return 1; //   16
-  else if (v < 41)  return 2; //   12
-  else if (v < 53)  return 3; //   12
-  else if (v < 71)  return 4; //   18
-  else if (v < 83)  return 5; //   12
-  else if (v < 100) return 6; //   17
-  return DEFAULT_BUCKET;
-#endif
 }
 
 std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
