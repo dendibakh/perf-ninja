88c7ca38158c7aaa9c17f033ba4716e13b3efdfc
Rasmus Bons <rasmusbons@gmail.com>
2025-02-12 09:36:46 +0100

    Update solution.cpp
    Moving if statement clause down to the count addition so count is incremented if the clause if true

--- labs/bad_speculation/conditional_store_1/solution.cpp
+++ labs/bad_speculation/conditional_store_1/solution.cpp
@@ -1,14 +1,20 @@
 
 #include "solution.h"
-
+#define SOLUTION
 // Select items which have S.first in range [lower..upper]
 std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
                    const std::uint32_t lower, const std::uint32_t upper) {
   std::size_t count = 0;
-  for (const auto item : input) {
+  for (const auto item : input) 
+  {
+    #ifdef SOLUTION
+    output[count] = item;
+    count += static_cast<uint32_t>((lower <= item.first) && (item.first <= upper));
+    #else
     if ((lower <= item.first) && (item.first <= upper)) {
       output[count++] = item;
     }
+    #endif
   }
   return count;
 }
