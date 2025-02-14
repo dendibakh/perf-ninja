f45e0c6c259ee4338abb5fcebac750197f06180c
Christian Oliveros <christianol_01@hotmail.com>
2025-02-14 16:57:48 +0100

    Virtual Call Missprediction

--- labs/bad_speculation/virtual_call_mispredict/solution.cpp
+++ labs/bad_speculation/virtual_call_mispredict/solution.cpp
@@ -2,25 +2,43 @@
 
 #include <random>
 
-void generateObjects(InstanceArray& array) {
+#ifdef SOLUTION
+#include <algorithm>
+#endif
+
+void generateObjects(InstanceArray &array)
+{
     std::default_random_engine generator(0);
     std::uniform_int_distribution<std::uint32_t> distribution(0, 2);
 
-    for (std::size_t i = 0; i < N; i++) {
+    for (std::size_t i = 0; i < N; i++)
+    {
         int value = distribution(generator);
-        if (value == 0) {
+        if (value == 0)
+        {
             array.push_back(std::make_unique<ClassA>());
-        } else if (value == 1) {
+        }
+        else if (value == 1)
+        {
             array.push_back(std::make_unique<ClassB>());
-        } else {
+        }
+        else
+        {
             array.push_back(std::make_unique<ClassC>());
         }
     }
+
+#ifdef SOLUTION
+    std::sort(array.begin(), array.end(), [](const auto &lhs, const auto &rhs)
+              { return lhs->type() < rhs->type(); });
+#endif
 }
 
 // Invoke the `handle` method on all instances in `output`
-void invoke(InstanceArray& array, std::size_t& data) {
-    for (const auto& item: array) {
+void invoke(InstanceArray &array, std::size_t &data)
+{
+    for (const auto &item : array)
+    {
         item->handle(data);
     }
 }
--- labs/bad_speculation/virtual_call_mispredict/solution.h
+++ labs/bad_speculation/virtual_call_mispredict/solution.h
@@ -2,34 +2,68 @@
 #include <vector>
 #include <memory>
 
+#define SOLUTION
+
 // Assume this constant never changes
 constexpr std::size_t N = 64 * 1024;
 
-struct BaseClass {
+struct BaseClass
+{
     virtual ~BaseClass() = default;
 
-    virtual void handle(std::size_t& data) const = 0;
+    virtual void handle(std::size_t &data) const = 0;
+
+#ifdef SOLUTION
+    virtual int type() const = 0;
+#endif
 };
 
-struct ClassA : public BaseClass {
-    void handle(std::size_t& data) const override {
+struct ClassA : public BaseClass
+{
+    void handle(std::size_t &data) const override
+    {
         data += 1;
     }
+
+#ifdef SOLUTION
+    int type() const override
+    {
+        return 0;
+    }
+#endif
 };
 
-struct ClassB : public BaseClass {
-    void handle(std::size_t& data) const override {
+struct ClassB : public BaseClass
+{
+    void handle(std::size_t &data) const override
+    {
         data += 2;
     }
+
+#ifdef SOLUTION
+    int type() const override
+    {
+        return 1;
+    }
+#endif
 };
 
-struct ClassC : public BaseClass {
-    void handle(std::size_t& data) const override {
+struct ClassC : public BaseClass
+{
+    void handle(std::size_t &data) const override
+    {
         data += 3;
     }
+
+#ifdef SOLUTION
+    int type() const override
+    {
+        return 2;
+    }
+#endif
 };
 
 using InstanceArray = std::vector<std::unique_ptr<BaseClass>>;
 
-void generateObjects(InstanceArray& array);
-void invoke(InstanceArray& array, std::size_t& data);
+void generateObjects(InstanceArray &array);
+void invoke(InstanceArray &array, std::size_t &data);
