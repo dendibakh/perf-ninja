f2e2b2b09db8295a22b082c56ae9331938bc7c91
Christian Oliveros <christianol_01@hotmail.com>
2025-02-14 17:32:56 +0100

    Fixed for GCC

--- labs/bad_speculation/conditional_store_1/solution.cpp
+++ labs/bad_speculation/conditional_store_1/solution.cpp
@@ -23,7 +23,7 @@ std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
                    const std::uint32_t lower, const std::uint32_t upper)
 {
   std::size_t count = 0;
-  for (size_t i = 0; i < input.size(); i++)
+  for (std::size_t i = 0; i < input.size(); i++)
   {
     const std::uint32_t cond = (lower <= input[i].first) * (input[i].first <= upper);
 
