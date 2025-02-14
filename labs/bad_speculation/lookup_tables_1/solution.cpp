8a3ca648291c0ae2efa120ec3b47cbf50e161158
Christian Oliveros <christianol_01@hotmail.com>
2025-02-14 01:26:06 +0100

    Lookup tables

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -1,5 +1,7 @@
 #include "solution.hpp"
 
+#define SOLUTION
+#ifndef SOLUTION
 static std::size_t mapToBucket(std::size_t v) {
                               //   size of a bucket
   if      (v < 13)  return 0; //   13
@@ -11,6 +13,41 @@ static std::size_t mapToBucket(std::size_t v) {
   else if (v < 100) return 6; //   17
   return DEFAULT_BUCKET;
 }
+#else
+
+#include <array>
+
+static constexpr std::size_t mapToBucket_ori(const std::size_t v) {
+  //   size of a bucket
+  if      (v < 13)  return 0; //   13
+  else if (v < 29)  return 1; //   16
+  else if (v < 41)  return 2; //   12
+  else if (v < 53)  return 3; //   12
+  else if (v < 71)  return 4; //   18
+  else if (v < 83)  return 5; //   12
+  else if (v < 100) return 6; //   17
+  return DEFAULT_BUCKET;
+}
+
+static constexpr std::array<std::size_t, 100> gen_lookup()
+{
+  std::array<std::size_t, 100> arr{};
+  for (size_t i = 0; i < 100; i++)
+  {
+    arr[i] = mapToBucket_ori(i);
+  }
+  
+  return arr;
+}
+
+static constexpr std::array<std::size_t, 100> lookup = gen_lookup();
+
+
+static std::size_t mapToBucket(std::size_t v) {
+  if (v < 100) return lookup[v];
+  return DEFAULT_BUCKET;
+}
+#endif
 
 std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
   std::array<std::size_t, NUM_BUCKETS> retBuckets{0};
