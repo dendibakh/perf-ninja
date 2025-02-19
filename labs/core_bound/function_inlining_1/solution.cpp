cdd6b7edbead708f572db324759f5c30c0b635df
Oleg Makovski <omakovski@yandex-team.ru>
2025-02-19 19:37:15 +0200

    add std::

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 void solution(std::array<S, N> &arr) {
-  sort(arr.begin(), arr.end(), [](const S a, const S b) {
+  std::sort(arr.begin(), arr.end(), [](const S a, const S b) {
     return a.key1 == b.key1 ? a.key2 < b.key2 : a.key1 < b.key1;
   });
 }
