a2da15df3183344314c1716ec1f5dba20b8c7aad
Oleg Makovski <omakovski@yandex-team.ru>
2025-02-19 19:00:52 +0200

    vectorization_2 assignment

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,10 +1,11 @@
 #include "solution.hpp"
 
 uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
+  uint32_t acc = 0;
   for (auto value : blob) {
     acc += value;
-    acc += acc < value; // add carry
   }
-  return acc;
+  const uint16_t val = acc & 0xFFFF;
+  const uint16_t carry = acc >> 16;
+  return val + carry;
 }
