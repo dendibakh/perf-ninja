96572a3971dba3891fb02f716483b2ed32ae74e1
Christian Oliveros <christianol_01@hotmail.com>
2025-02-20 01:31:00 +0100

    IO OPT 1

--- labs/misc/io_opt1/solution.cpp
+++ labs/misc/io_opt1/solution.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <stdexcept>
 
+#define SOLUTION
+#ifndef SOLUTION
 uint32_t solution(const char *file_name) {
   std::fstream file_stream{file_name};
   if (!file_stream.is_open())
@@ -25,3 +27,25 @@ uint32_t solution(const char *file_name) {
 
   return crc;
 }
+#else
+
+#include "MappedFile.hpp"
+
+uint32_t solution(const char *file_name) {
+  MappedFile file{file_name};
+  std::string_view data = file.getContents();
+
+  // Initial value has all bits set to 1
+  uint32_t crc = 0xff'ff'ff'ff;
+
+  // Update the CRC32 value character by character
+  for(char c : data) {
+    update_crc32(crc, static_cast<uint8_t>(c));
+  }
+
+  // Invert the bits
+  crc ^= 0xff'ff'ff'ff;
+
+  return crc;
+}
+#endif
