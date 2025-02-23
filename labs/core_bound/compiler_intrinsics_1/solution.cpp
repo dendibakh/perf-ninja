83cd95120089ab431da04fc85287663cf0f3f4f9
Oleg Makovski <omakovski@yandex-team.ru>
2025-02-23 23:37:30 +0200

    compiler_intrinsics_1 assignment

--- labs/core_bound/compiler_intrinsics_1/solution.cpp
+++ labs/core_bound/compiler_intrinsics_1/solution.cpp
@@ -1,6 +1,10 @@
 
 #include "solution.h"
 #include <memory>
+#include <array>
+#include <iostream>
+
+#include <x86intrin.h>
 
 void imageSmoothing(const InputVector &input, uint8_t radius,
                     OutputVector &output) {
@@ -21,7 +25,37 @@ void imageSmoothing(const InputVector &input, uint8_t radius,
   }
 
   // 2. main loop.
+  constexpr int vec_sz = 8;
+  //using vec = int16_t __attribute__((ext_vector_type(vec_sz)));
   limit = size - radius;
+  for (; pos + vec_sz <= limit; pos += vec_sz) {
+    //vec psum;
+    //for (int i = 0; i < vec_sz; ++i) {
+    //  psum[i] = (int16_t)input[i + pos + radius] - (int16_t)input[i + pos - radius - 1];
+    //}
+    //for (int i = 1; i < vec_sz; ++i) {
+    //  psum[i] += psum[i - 1];
+    //}
+    //for (int i = 0; i < vec_sz; ++i) {
+    //  psum[i] += currentSum;
+    //}
+    //for (int i = 0; i < vec_sz; ++i) {
+    //  output[pos + i] = psum[i];
+    //}
+   
+    auto d = _mm_sub_epi16(
+      _mm_cvtepu8_epi16(_mm_loadu_si64(&input[pos + radius])),
+      _mm_cvtepu8_epi16(_mm_loadu_si64(&input[pos - radius - 1]))
+    );
+    //__m128i d = _mm_loadu_si128((const __m128i*)&psum);
+    d = _mm_add_epi16(d, _mm_slli_si128(d, 1 * sizeof(int16_t)));
+    d = _mm_add_epi16(d, _mm_slli_si128(d, 2 * sizeof(int16_t)));
+    d = _mm_add_epi16(d, _mm_slli_si128(d, 4 * sizeof(int16_t)));
+    d = _mm_add_epi16(d, _mm_set1_epi16(currentSum));
+    _mm_storeu_si128((__m128i*)&output[pos], d);
+    //currentSum = output[pos + vec_sz - 1];
+    currentSum = _mm_extract_epi16(d, 7);
+  }
   for (; pos < limit; ++pos) {
     currentSum -= input[pos - radius - 1];
     currentSum += input[pos + radius];
