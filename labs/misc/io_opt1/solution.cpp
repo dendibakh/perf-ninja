ca3ea55013bcd2ed187e5b438763c75866b22691
0legmak <omakovski@yandex-team.ru>
2025-02-28 19:43:05 +0200

    io_opt1 assignment

--- labs/misc/io_opt1/solution.cpp
+++ labs/misc/io_opt1/solution.cpp
@@ -3,20 +3,17 @@
 #include <fstream>
 #include <stdexcept>
 
+#include "MappedFile.hpp"
+
 uint32_t solution(const char *file_name) {
-  std::fstream file_stream{file_name};
-  if (!file_stream.is_open())
-    throw std::runtime_error{"The file could not be opened"};
+  MappedFile file_stream{file_name};
+  const auto file_data = file_stream.getContents();
 
   // Initial value has all bits set to 1
   uint32_t crc = 0xff'ff'ff'ff;
 
   // Update the CRC32 value character by character
-  char c;
-  while (true) {
-    file_stream.read(&c, 1);
-    if (file_stream.eof())
-      break;
+  for (const char c : file_data) {
     update_crc32(crc, static_cast<uint8_t>(c));
   }
 
