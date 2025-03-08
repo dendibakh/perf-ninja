1ade960474f56b3bdd6d8ac9496ba02fb279342c
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-03-08 19:02:24 +0100

    update formatting and remove dead code

--- labs/core_bound/compiler_intrinsics_3/solution.hpp
+++ labs/core_bound/compiler_intrinsics_3/solution.hpp
@@ -5,22 +5,22 @@
 
 template<class T>
 struct Position {
-    T x;
-    T y;
-    T z;
+  T x;
+  T y;
+  T z;
 
-    constexpr bool operator==(Position const &other) const {
-        return x == other.x and y == other.y and z == other.z;
-    }
+  constexpr bool operator==(Position const &other) const {
+    return x == other.x and y == other.y and z == other.z;
+  }
 
-    constexpr bool operator!=(Position const &other) const {
-        return !(*this == other);
-    }
+  constexpr bool operator!=(Position const &other) const {
+    return !(*this == other);
+  }
 };
 
 template<class T>
 constexpr std::ostream &operator<<(std::ostream &oss, Position<T> position) {
-    return oss << '(' << position.x << ", " << position.y << ", " << position.z << ')';
+  return oss << '(' << position.x << ", " << position.y << ", " << position.z << ')';
 }
 
 Position<std::uint32_t> solution(std::vector<Position<std::uint32_t>> const &input);
