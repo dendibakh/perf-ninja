a2b0465cf6a9bf93dc20ecff20111e0507d48e4a
Rail Mazgutov <rail.mazgutov@gmail.com>
2025-03-11 19:26:13 +0100

    data packing

--- labs/memory_bound/data_packing/init.cpp
+++ labs/memory_bound/data_packing/init.cpp
@@ -5,26 +5,24 @@
 #include "solution.h"
 #include <random>
 
-S create_entry(int first_value, int second_value) {
-  S entry;
-
-  entry.i = first_value;
-  entry.s = static_cast<short>(second_value);
-  entry.l = static_cast<long long>(first_value * second_value);
-  entry.d = static_cast<double>(first_value) / maxRandom;
-  entry.b = first_value < second_value;
-
-  return entry;
+S create_entry(const int first_value, const int second_value) {
+    return {
+        static_cast<float>(first_value) / maxRandom,
+        static_cast<long long>(first_value * second_value),
+        static_cast<char>(second_value),
+        static_cast<char>(second_value),
+        first_value < second_value
+    };
 }
 
 void init(std::array<S, N> &arr) {
-  std::default_random_engine generator;
-  std::uniform_int_distribution<int> distribution(minRandom, maxRandom - 1);
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(minRandom, maxRandom - 1);
 
-  for (int i = 0; i < N; i++) {
-    int random_int1 = distribution(generator);
-    int random_int2 = distribution(generator);
+    for (int i = 0; i < N; i++) {
+        const int random_int1 = distribution(generator);
+        const int random_int2 = distribution(generator);
 
-    arr[i] = create_entry(random_int1, random_int2);
-  }
+        arr[i] = create_entry(random_int1, random_int2);
+    }
 }
