6770e7a9b58abcc1fbd20ea19f15602515edd9f5
Matveich <matmuh316@gmail.com>
2025-03-16 21:58:24 +0300

    Resize prefetch window by several iterations

--- labs/memory_bound/swmem_prefetch_1/solution.cpp
+++ labs/memory_bound/swmem_prefetch_1/solution.cpp
@@ -15,15 +15,22 @@ int solution(const hash_map_t *hash_map, const std::vector<int> &lookups) {
   int result = 0;
 
 #ifdef SOLUTION
+  // if the one loop prefetch window is too small to give any benefit
+  // (the data hasn't been fetched when it's needed, computations can't hide latency)
+  // we can just request data for lookAhead iterations forward.
+  // several iterations are more likely to hide latency
+  constexpr int lookAhead = 16;
+
   for (int idx = 0; idx < lookups.size(); ++idx) {
     int val = lookups[idx];
-    hash_map->preload(lookups[idx + 1]);
+    hash_map->preload(lookups[idx + lookAhead]);
 #else
   for (int val : lookups) {
 #endif
   
-    if (hash_map->find(val))
+    if (hash_map->find(val)) {
       result += getSumOfDigits(val);
+    }
   }
 
   return result;
