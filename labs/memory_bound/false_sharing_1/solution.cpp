d9615ee588c630aacc5b4ecc504a6a24628f9c5c
Adarsh Agrawal <adarsh@gmail.com>
2025-03-17 00:50:27 +0000

    Assignment: memory_bound/false_sharing

--- labs/memory_bound/false_sharing_1/solution.cpp
+++ labs/memory_bound/false_sharing_1/solution.cpp
@@ -4,12 +4,20 @@
 #include <omp.h>
 #include <vector>
 
+#if defined(__APPLE__) && defined(__MACH__)
+  // In Apple processors (such as M1, M2 and later), L2 cache operates on 128B cache lines.
+  #define ON_MACOS
+  #define CACHELINE_SIZE 128
+#else
+  #define CACHELINE_SIZE 64
+#endif
+
 std::size_t solution(const std::vector<uint32_t> &data, int thread_count) {
   // Using std::atomic counters to disallow compiler to promote `target`
   // memory location into a register. This way we ensure that the store
   // to `target` stays inside the loop.
-  struct Accumulator {
-    std::atomic<uint32_t> value = 0;
+  struct alignas(CACHELINE_SIZE) Accumulator {
+    uint32_t value = 0;
   };
   std::vector<Accumulator> accumulators(thread_count);
 
