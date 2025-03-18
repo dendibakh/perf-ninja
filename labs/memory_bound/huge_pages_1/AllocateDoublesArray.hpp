fdac932d770d9acefd656a99c99e4c76120f9b6d
Matveich <matmuh316@gmail.com>
2025-03-18 22:52:13 +0300

    Cook Huge Page Tables in the right way

--- labs/memory_bound/huge_pages_1/AllocateDoublesArray.hpp
+++ labs/memory_bound/huge_pages_1/AllocateDoublesArray.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <string.h>
 
 #if defined(__linux__) || defined(__linux) || defined(linux) ||                \
     defined(__gnu_linux__)
@@ -149,6 +150,32 @@ inline bool setRequiredPrivileges() {
 
 #endif
 
+#include <errno.h>
+#include <fcntl.h>
+#include <linux/kernel-page-flags.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#define fail(...) do { fprintf(stderr, __VA_ARGS__); } while (0)
+
+// normal page, 4KiB
+#define PAGE_SIZE (1 << 12)
+// huge page, 2MiB
+#define HPAGE_SIZE (1 << 21)
+
+// See <https://www.kernel.org/doc/Documentation/vm/pagemap.txt> for
+// format which these bitmasks refer to
+#define PAGEMAP_PRESENT(ent) (((ent) & (1ull << 63)) != 0)
+#define PAGEMAP_PFN(ent) ((ent) & ((1ull << 55) - 1))
+
+static void check_huge_page(void* ptr);
+
+
 constexpr static std::size_t huge_page_size = 1 << 21;
 size_t round_to_huge_page_size(size_t n) {
   return (((n - 1) / huge_page_size) + 1) * huge_page_size;
@@ -159,24 +186,33 @@ size_t round_to_huge_page_n(size_t n) {
 }
 
 
+// [HERE IS NICE TUTORIAL HOW TO COOK HUGE PAGE TABLES]
+// https://mazzo.li/posts/check-huge-page.html
+
 // Allocate an array of doubles of size `size`, return it as a
 // std::unique_ptr<double[], D>, where `D` is a custom deleter type
 inline auto allocateDoublesArray(size_t size) {
   // Allocate memory
   // double *alloc = new double[size];
+  size *= sizeof(double);
+  // float mb = size / (10e6);
+  // printf("size: %f Mb\n", size / (10e6));
+  size = round_to_huge_page_size(size);
 
-  void* ptr = nullptr;
-  // void* ptr = mmap(nullptr,
-  //                 round_to_huge_page_size(size * sizeof(double)),
-  //                 PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS,
-  //                 -1,
-  //                 0);
-  posix_memalign(&ptr, huge_page_size, round_to_huge_page_size(size * sizeof(double)));
-
-  if (ptr == MAP_FAILED)
+  void* alloc = nullptr;
+  alloc = aligned_alloc(huge_page_size, size);
+  if (alloc == NULL)
     throw std::bad_alloc{};
 
-  madvise(ptr, size, MADV_HUGEPAGE);
+  madvise(alloc, size, MADV_HUGEPAGE);
+
+  char* it = (char*) alloc;
+  for (char* end = it + size; it < end; it += huge_page_size) {
+    memset(it, 0, 1);
+
+    // uncomment to check if it's really a huge page
+    // check_huge_page(it);
+  }
 
   // remember to cast the pointer to double* if your allocator returns void*
 
@@ -184,13 +220,54 @@ inline auto allocateDoublesArray(size_t size) {
   // define a class if you're not comfortable with the syntax
   auto deleter = [size](double *ptr) { munmap(ptr, size); };
 
-  // auto deleter = [/* state = ... */](double *ptr) { delete[] ptr; };
-
-  return std::unique_ptr<double[], decltype(deleter)>(static_cast<double*>(ptr),
+  return std::unique_ptr<double[], decltype(deleter)>(static_cast<double*>(alloc),
                                                       std::move(deleter));
 
   // The above is equivalent to:
   // return std::make_unique<double[]>(size);
   // The more verbose version is meant to demonstrate the use of a custom
   // (potentially stateful) deleter
+}
+
+
+// Checks if the page pointed at by `ptr` is huge. Assumes that `ptr` has already
+// been allocated.
+static void check_huge_page(void* ptr) {
+  int pagemap_fd = open("/proc/self/pagemap", O_RDONLY);
+  if (pagemap_fd < 0) {
+    fail("could not open /proc/self/pagemap: %s", strerror(errno));
+  }
+  int kpageflags_fd = open("/proc/kpageflags", O_RDONLY);
+  if (kpageflags_fd < 0) {
+    fail("could not open /proc/kpageflags: %s", strerror(errno));
+  }
+
+  // each entry is 8 bytes long
+  uint64_t ent;
+  if (pread(pagemap_fd, &ent, sizeof(ent), ((uintptr_t) ptr) / PAGE_SIZE * 8) != sizeof(ent)) {
+    fail("could not read from pagemap\n");
+  }
+
+  if (!PAGEMAP_PRESENT(ent)) {
+    fail("page not present in /proc/self/pagemap, did you allocate it?\n");
+  }
+  if (!PAGEMAP_PFN(ent)) {
+    fail("page frame number not present, run this program as root\n");
+  }
+
+  uint64_t flags;
+  if (pread(kpageflags_fd, &flags, sizeof(flags), PAGEMAP_PFN(ent) << 3) != sizeof(flags)) {
+    fail("could not read from kpageflags\n");
+  }
+
+  if (!(flags & (1ull << KPF_THP))) {
+    fail("could not allocate huge page\n");
+  }
+
+  if (close(pagemap_fd) < 0) {
+    fail("could not close /proc/self/pagemap: %s", strerror(errno));
+  }
+  if (close(kpageflags_fd) < 0) {
+    fail("could not close /proc/kpageflags: %s", strerror(errno));
+  }
 }
