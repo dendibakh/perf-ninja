dbe661ef0dd59eac7c148a2b6dd2579dfc44d62f
Arnav-Negi <arnav.negi@students.iiit.ac.in>
2025-03-20 19:53:04 +0530

    matmul loop swap

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -21,16 +21,6 @@ struct S
   bool operator<(const S &s) const { return this->i < s.i; }
 };
 
-//struct S {
-//  int i;
-//  long long l;
-//  short s;
-//  double d;
-//  bool b;
-//
-//  bool operator<(const S &s) const { return this->i < s.i; }
-//};
-
 void init(std::array<S, N> &arr);
 S create_entry(int first_value, int second_value);
 void solution(std::array<S, N> &arr);
