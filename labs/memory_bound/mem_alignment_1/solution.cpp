136f4151679128396f28a01dc068d64b1004a8bc
Matveich <matmuh316@gmail.com>
2025-03-23 12:54:26 +0300

    Solve memory alignment

--- labs/memory_bound/mem_alignment_1/solution.cpp
+++ labs/memory_bound/mem_alignment_1/solution.cpp
@@ -1,6 +1,7 @@
 
 #include "solution.h"
 #include <random>
+#include <cstdio>
 
 // ******************************************
 // Change this function
@@ -8,7 +9,73 @@
 // This function allows you to change the number of columns in a matrix. 
 // In other words, it defines how many elements are in each row.
 // hint: you need to allocate dummy columns to achieve proper data alignment.
-int n_columns(int N) {  
+
+int n_columns(int N) {
+  
+  const int size = N * sizeof(float);
+  const int N_old = N;
+
+// #define SOLUTION
+/*
+bench1/_63         34.9 us         34.9 us        20063
+bench1/_64         15.6 us         15.6 us        49786
+bench1/_65         17.5 us         17.5 us        39933
+bench1/_128         127 us          127 us         5666
+bench1/_256        1202 us         1200 us          580
+bench1/_511       11378 us        11333 us           61
+bench1/_512        9854 us         9839 us           71
+bench1/_513       10881 us        10859 us           65
+bench1/_1024      89534 us        89528 us            6
+*/
+
+#define SOLUTION_1
+/*
+CacheLine = 64
+
+bench1/_63         35.4 us         35.4 us        19992
+bench1/_64         12.1 us         12.1 us        57606
+bench1/_65         13.4 us         13.4 us        52066
+bench1/_128        93.1 us         92.9 us         7329
+bench1/_256         749 us          747 us          957
+bench1/_511       10799 us        10791 us           61
+bench1/_512        6202 us         6201 us          113
+bench1/_513        8486 us         8486 us           80
+bench1/_1024      66050 us        65952 us            9
+
+CacheLine = 128
+
+bench1/_63         35.3 us         35.3 us        20538
+bench1/_64         15.7 us         15.7 us        44524
+bench1/_65         17.8 us         17.8 us        39351
+bench1/_128         126 us          126 us         5525
+bench1/_256        1012 us         1012 us          687
+bench1/_511       11870 us        11869 us           58
+bench1/_512        8124 us         8123 us           86
+bench1/_513        8643 us         8642 us           80
+bench1/_1024      67572 us        67567 us            8
+*/
+
+#ifdef SOLUTION // align size by power-of-two boundary
+  int aligned_size = 1;
+  int tmp_size = size;
+
+  while (tmp_size > 0) {
+    tmp_size = tmp_size >> 1;
+    aligned_size = aligned_size << 1;
+  }
+
+  aligned_size = aligned_size % size == 0 ? size : aligned_size;
+  N = aligned_size / sizeof(float);
+#endif
+
+#ifdef SOLUTION_1 // align by cache line size
+  constexpr int CacheLineSize = 64;
+  int aligned_size = ((size / CacheLineSize) + 1) * CacheLineSize;
+  N = aligned_size / sizeof(float);
+#endif
+
+  // printf("N_old = %d, N = %d\n", N_old, N);
+
   return N;
 }
 // ******************************************
