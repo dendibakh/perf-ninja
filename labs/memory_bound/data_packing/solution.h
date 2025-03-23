964c8f738be227030c07f77e06ba27f7efb47433
Matveich <matmuh316@gmail.com>
2025-03-23 15:57:24 +0300

    Solve data packing lab

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -9,6 +9,23 @@ constexpr int N = 10000;
 constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
+#define SOLUTION
+#ifdef SOLUTION
+
+#pragma pack(push, 1) // by default align by width of the fattest type
+// FIXME: this data structure can be reduced in size
+struct S {
+  float d;
+  unsigned int l : 14;
+  unsigned short i : 7;
+  unsigned short s : 7;
+  bool b : 1;
+
+  bool operator<(const S &s) const { return this->i < s.i; }
+};
+#pragma pack(pop)
+
+#else
 // FIXME: this data structure can be reduced in size
 struct S {
   int i;
@@ -19,6 +36,9 @@ struct S {
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+#endif
+
+// static_assert (sizeof(S) == 1024, "Size is not correct");
 
 void init(std::array<S, N> &arr);
 S create_entry(int first_value, int second_value);
