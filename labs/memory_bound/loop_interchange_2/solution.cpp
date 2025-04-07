f847a3847d3f7a64be8a8d263759643d727a78bf
Your Name <you@example.com>
2025-04-07 14:01:06 +0300

    solution to loop_interchange2;

--- labs/memory_bound/loop_interchange_2/solution.cpp
+++ labs/memory_bound/loop_interchange_2/solution.cpp
@@ -28,20 +28,35 @@ static void filterVertically(uint8_t *output, const uint8_t *input,
       int value = static_cast<int>(dot / static_cast<float>(sum) + 0.5f);
       output[r * width + c] = static_cast<uint8_t>(value);
     }
+  }
 
+  
     // Middle part of computations with full kernel
-    for (int r = radius; r < height - radius; r++) {
-      // Accumulation
-      int dot = 0;
-      for (int i = 0; i < radius + 1 + radius; i++) {
-        dot += input[(r - radius + i) * width + c] * kernel[i];
-      }
+  for (int r = radius; r < height - radius; r++) {
+    
+    int dot[width];
+    // Initialization
+    for (int c = 0; c < width; c++) 
+      dot[c] = 0;
 
-      // Fast shift instead of division
-      int value = (dot + rounding) >> shift;
-      output[r * width + c] = static_cast<uint8_t>(value);
+    // Accumulation
+    for (int i = 0; i < radius + 1 + radius; i++) {
+      for (int c = 0; c < width; c++) 
+      {
+        dot[c] += input[(r - radius + i) * width + c] * kernel[i];
+      }
     }
 
+    // Output
+    for (int c = 0; c < width; c++) 
+    {
+      // Fast shift instead of division
+        int value = (dot[c] + rounding) >> shift;
+        output[r * width + c] = static_cast<uint8_t>(value);
+    }
+  }
+
+  for (int c = 0; c < width; c++) {
     // Bottom part of line, partial kernel
     for (int r = std::max(radius, height - radius); r < height; r++) {
       // Accumulation
