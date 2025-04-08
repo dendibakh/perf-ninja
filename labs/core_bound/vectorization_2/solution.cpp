c57c6e4f284f94e9bfe04febd9abb20b858c3f9f
Your Name <you@example.com>
2025-04-08 16:18:48 +0300

    solution to vectorization_2

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,10 +1,20 @@
 #include "solution.hpp"
+#include <array>
 
 uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
-  for (auto value : blob) {
+  uint32_t acc { 0 }; // 32-bit accumulator to avoid overflow
+
+  #pragma GCC ivdep
+  #pragma omp for 
+  for (auto value : blob) { 
     acc += value;
-    acc += acc < value; // add carry
   }
-  return acc;
+
+  // exctract overflow (sum above 0xFFFF) with acc >> 16
+  // retain the lower 16 bits with acc & 0xFFFF
+  acc = ((acc & 0xFFFF) + (acc >> 16));
+  // incase the previous addition caused overflow
+  acc += (acc >> 16);
+
+  return static_cast<uint16_t>(acc);
 }
