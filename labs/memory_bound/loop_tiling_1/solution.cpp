2aa8c4323a8be5880458fec18d49ef18fdacccaf
Dan Hansen <dhansen@nvidia.com>
2025-04-29 11:37:47 -0700

    loop_tiling_1: initial solution

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -3,9 +3,13 @@
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
+  for (int ii = 0; ii < size; ii += i_stride) {
+    for (int jj = 0; jj < size; jj += j_stride) {
+      for (int i = ii; i < ii + i_stride; i++) {
+        for (int j = jj; j < jj + j_stride; j++) {
+          out[i][j] = in[j][i];
+        }
+      }
     }
   }
   return out[0][size - 1];
--- labs/memory_bound/loop_tiling_1/solution.hpp
+++ labs/memory_bound/loop_tiling_1/solution.hpp
@@ -1,5 +1,7 @@
 #include <vector>
 
+constexpr int i_stride = 80;
+constexpr int j_stride = 80;
 using MatrixOfDoubles = std::vector<std::vector<double>>;
 void initMatrix(MatrixOfDoubles& m);
 bool solution(MatrixOfDoubles& in, MatrixOfDoubles& out);
