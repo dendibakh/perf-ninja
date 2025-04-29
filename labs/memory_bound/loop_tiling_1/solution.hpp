9b533577c49d9dae8ee656fc03f8f33c7d208ac6
Dan Hansen <dhansen@nvidia.com>
2025-04-29 11:53:51 -0700

    loop_tiling_1: tune block size again

--- labs/memory_bound/loop_tiling_1/solution.hpp
+++ labs/memory_bound/loop_tiling_1/solution.hpp
@@ -1,7 +1,7 @@
 #include <vector>
 
-constexpr int i_stride = 40;
-constexpr int j_stride = 40;
+constexpr int i_stride = 20;
+constexpr int j_stride = 20;
 using MatrixOfDoubles = std::vector<std::vector<double>>;
 void initMatrix(MatrixOfDoubles& m);
 bool solution(MatrixOfDoubles& in, MatrixOfDoubles& out);
