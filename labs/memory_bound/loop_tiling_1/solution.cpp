a399692a256e1db02dde183ab7fb0f2b41dd17fa
Dan Hansen <dhansen@nvidia.com>
2025-04-29 11:40:58 -0700

    loop_tiling_1: fix errors with inner loop bounds

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -4,9 +4,11 @@
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
   for (int ii = 0; ii < size; ii += i_stride) {
+    int i_limit = std::min(size, ii + i_stride);
     for (int jj = 0; jj < size; jj += j_stride) {
-      for (int i = ii; i < ii + i_stride; i++) {
-        for (int j = jj; j < jj + j_stride; j++) {
+      int j_limit = std::min(size, jj + j_stride);
+      for (int i = ii; i < i_limit; i++) {
+        for (int j = jj; j < j_limit; j++) {
           out[i][j] = in[j][i];
         }
       }
