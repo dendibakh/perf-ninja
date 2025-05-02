fa547a4b80d9a52163cc3a6483439c51c5c1c55f
Dan Hansen <dhansen@nvidia.com>
2025-05-02 16:24:01 -0700

    branches_to_cmov_1: distribute the loops to eliminate the branches from the inner loop in the common case

--- labs/bad_speculation/branches_to_cmov_1/solution.hpp
+++ labs/bad_speculation/branches_to_cmov_1/solution.hpp
@@ -1,3 +1,4 @@
+#include <variant>
 #include <vector>
 #include <iostream>
 
@@ -37,6 +38,36 @@ public:
         std::cout << "\n";
     }    
 
+    void visitNeighboursEdges(int i, int j) {
+      int M = current.size();
+      int N = current[0].size();
+      int aliveNeighbours = 0;
+      for(int p = -1; p <= 1; p++) {              // row-offet (-1,0,1)
+          for(int q = -1; q <= 1; q++) {          // col-offset (-1,0,1)
+              if((i + p < 0) ||                   // if row offset less than UPPER boundary
+                 (i + p > M - 1) ||               // if row offset more than LOWER boundary
+                 (j + q < 0) ||                   // if column offset less than LEFT boundary
+                 (j + q > N - 1))                 // if column offset more than RIGHT boundary
+                  continue;
+              aliveNeighbours += current[i + p][j + q];
+          }
+      }
+
+      aliveNeighbours -= current[i][j];
+
+      // Implementing the Rules of Life:
+      future[i][j] =
+        __builtin_unpredictable(aliveNeighbours == 2) ?
+          // 2. Remains the same
+          current[i][j] :
+          __builtin_unpredictable(aliveNeighbours == 3) ?
+            // 3. A new cell is born
+            1 :
+            // 1. Cell is lonely and dies (or)
+            // 4. Cell dies due to over population
+            0;
+    }
+
     // Simulate the next generation of life
     void simulateNext() {
         //printCurrentGrid();
@@ -44,19 +75,24 @@ public:
         int N = current[0].size();
         
         for(int i = 0; i < M; i++) {
-            for(int j = 0; j < N; j++) {
-                int aliveNeighbours = 0;      
-                // finding the number of neighbours that are alive                  
+          visitNeighboursEdges(i, 0);
+          visitNeighboursEdges(i, N - 1);
+        }
+        for (int j = 1; j < N - 1; j++) {
+          visitNeighboursEdges(0, j);
+          visitNeighboursEdges(M - 1, j);
+        }
+
+        for(int i = 1; i < M - 1; i++) {
+            for(int j = 1; j < N - 1; j++) {
+                int aliveNeighbours = 0;
+
                 for(int p = -1; p <= 1; p++) {              // row-offet (-1,0,1)
-                    for(int q = -1; q <= 1; q++) {          // col-offset (-1,0,1)
-                        if((i + p < 0) ||                   // if row offset less than UPPER boundary
-                           (i + p > M - 1) ||               // if row offset more than LOWER boundary
-                           (j + q < 0) ||                   // if column offset less than LEFT boundary
-                           (j + q > N - 1))                 // if column offset more than RIGHT boundary
-                            continue;
-                        aliveNeighbours += current[i + p][j + q];
-                    }
+                  for(int q = -1; q <= 1; q++) {           // col-offset (-1,0,1)
+                    aliveNeighbours += current[i + p][j + q];
+                  }
                 }
+                // finding the number of neighbours that are alive                  
                 // The cell needs to be subtracted from
                 // its neighbours as it was counted before
                 aliveNeighbours -= current[i][j];
