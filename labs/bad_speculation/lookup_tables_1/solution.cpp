6bc2360b3bfb4c33d0361d00c0fec4d06546e800
Your Name <you@example.com>
2025-05-03 21:28:28 +0300

    solution to lookup_tables1

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -1,21 +1,49 @@
 #include "solution.hpp"
+#define SOL
 
-static std::size_t mapToBucket(std::size_t v) {
-                              //   size of a bucket
-  if      (v < 13)  return 0; //   13
-  else if (v < 29)  return 1; //   16
-  else if (v < 41)  return 2; //   12
-  else if (v < 53)  return 3; //   12
-  else if (v < 71)  return 4; //   18
-  else if (v < 83)  return 5; //   12
-  else if (v < 100) return 6; //   17
-  return DEFAULT_BUCKET;
+#ifdef SOL
+alignas(64) constexpr int buckets[100] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                          1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+                                          3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5,
+                                          5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6};
+
+static size_t mapToBucket(size_t v)
+{
+
+    if (v < (sizeof(buckets) / sizeof(int)))
+
+        return buckets[v];
+
+    return -1; // let it crash
 }
+#else
+static std::size_t mapToBucket(std::size_t v)
+{
+    //   size of a bucket
+    if (v < 13)
+        return 0; //   13
+    else if (v < 29)
+        return 1; //   16
+    else if (v < 41)
+        return 2; //   12
+    else if (v < 53)
+        return 3; //   12
+    else if (v < 71)
+        return 4; //   18
+    else if (v < 83)
+        return 5; //   12
+    else if (v < 100)
+        return 6; //   17
+    return DEFAULT_BUCKET;
+}
+#endif
 
-std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
-  std::array<std::size_t, NUM_BUCKETS> retBuckets{0};
-  for (auto v : values) {
-    retBuckets[mapToBucket(v)]++;
-  }
-  return retBuckets;
+std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values)
+{
+    std::array<std::size_t, NUM_BUCKETS> retBuckets{0};
+    for (auto v : values)
+    {
+        retBuckets[mapToBucket(v)]++;
+    }
+    return retBuckets;
 }
