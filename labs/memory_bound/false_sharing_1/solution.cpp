e39c7d5afa8fdbf238ff263ee3aef7ddc58e3d81
Osman Yasal <osmyasal@gmail.com>
2025-05-04 21:20:49 +0300

    false sharing solved

--- labs/memory_bound/false_sharing_1/solution.cpp
+++ labs/memory_bound/false_sharing_1/solution.cpp
@@ -4,23 +4,28 @@
 #include <omp.h>
 #include <vector>
 
-std::size_t solution(const std::vector<uint32_t> &data, int thread_count) {
+std::size_t solution(const std::vector<uint32_t> &data, int thread_count)
+{
   // Using std::atomic counters to disallow compiler to promote `target`
   // memory location into a register. This way we ensure that the store
   // to `target` stays inside the loop.
-  struct Accumulator {
+  
+  #define CACHELINE_ALIGN alignas(64)
+  struct CACHELINE_ALIGN Accumulator
+  {
     std::atomic<uint32_t> value = 0;
   };
   std::vector<Accumulator> accumulators(thread_count);
 
-#pragma omp parallel num_threads(thread_count) default(none)                   \
+#pragma omp parallel num_threads(thread_count) default(none) \
     shared(accumulators, data)
   {
     int target_index = omp_get_thread_num();
     auto &target = accumulators[target_index];
 
 #pragma omp for
-    for (int i = 0; i < data.size(); i++) {
+    for (int i = 0; i < data.size(); i++)
+    {
       // Perform computation on each input
       auto item = data[i];
       item += 1000;
@@ -33,7 +38,8 @@ std::size_t solution(const std::vector<uint32_t> &data, int thread_count) {
   }
 
   std::size_t result = 0;
-  for (const auto &accumulator : accumulators) {
+  for (const auto &accumulator : accumulators)
+  {
     result += accumulator.value;
   }
   return result;
