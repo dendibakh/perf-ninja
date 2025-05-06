42c623ef0e464da1a930c9769e3e8b2f713ce445
Your Name <you@example.com>
2025-05-06 13:31:46 +0300

    fix: solution to lookup_tables1, hardcoded values, change alignas constexpr std::array to int buckets[100], manual loop unroll 4

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -1,67 +1,32 @@
 #include "solution.hpp"
-#define SOL
 
-#ifdef SOL
-// #include <array>
+#include <cstdint>
 
-// constexpr int makeLUT(int i)
-// {
-//     return (i < 13) ? 0 : (i < 29) ? 1 : (i < 41) ? 2 : (i < 53) ? 3 : (i < 71) ? 4 : (i < 83) ? 5 : (i < 100) ? 6 :
-//     -1;
-// }
-
-// constexpr std::array<int, 100> generateBuckets()
-// {
-//     std::array<int, 100> b = {};
-//     for (int i = 0; i < 100; ++i)
-//         b[i] = makeLUT(i);
-//     return b;
-// }
-
-// alignas(64) constexpr std::array<int, 100> buckets = { // generateBuckets();
-int buckets[100] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                // 13
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,       // 16
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,                   // 12
-    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,                   // 12
-    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, // 18
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,                   // 12
-    6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6     // 16
+alignas(64) static constexpr uint8_t buckets[100] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                 // 13
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,        // 16
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,                    // 12
+    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,                    // 12
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,  // 18
+    5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,                    // 12
+    6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,     // 16
 };
-static size_t mapToBucket(size_t v)
-{
-    if (v < 100)
-        return buckets[v];
-    return DEFAULT_BUCKET;
+inline static constexpr size_t mapToBucket(size_t v) {
+  if (v < (sizeof(buckets) / sizeof(uint8_t))) return buckets[v];
+  return DEFAULT_BUCKET;  // let it crash
 }
-#else
-static std::size_t mapToBucket(std::size_t v)
-{
-    //   size of a bucket
-    if (v < 13)
-        return 0; //   13
-    else if (v < 29)
-        return 1; //   16
-    else if (v < 41)
-        return 2; //   12
-    else if (v < 53)
-        return 3; //   12
-    else if (v < 71)
-        return 4; //   18
-    else if (v < 83)
-        return 5; //   12
-    else if (v < 100)
-        return 6; //   17
-    return DEFAULT_BUCKET;
-}
-#endif
 
-std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values)
-{
-    std::array<std::size_t, NUM_BUCKETS> retBuckets{0};
-    for (auto v : values)
-    {
-        retBuckets[mapToBucket(v)]++;
-    }
-    return retBuckets;
+std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
+  alignas(64) std::array<std::size_t, NUM_BUCKETS> retBuckets{0};
+
+  for (std::size_t i = 0; i + 3 < values.size(); i += 4) {
+    int v0 = values[i], v1 = values[i + 1], v2 = values[i + 2],
+        v3 = values[i + 3];
+    retBuckets[mapToBucket(v0)]++;
+    retBuckets[mapToBucket(v1)]++;
+    retBuckets[mapToBucket(v2)]++;
+    retBuckets[mapToBucket(v3)]++;
+  }
+
+  return retBuckets;
 }
