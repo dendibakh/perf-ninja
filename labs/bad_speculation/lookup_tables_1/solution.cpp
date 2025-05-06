f84dd299ac04d38a4242bb0db76c1a7b1e5c321f
Your Name <you@example.com>
2025-05-06 14:03:51 +0300

    fix: solution to lookup_tables1, hardcoded values, change alignas constexpr std::array to int buckets[100], manual loop unroll 4, change mapToBucket to return bucket[ min comp]

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -2,31 +2,35 @@
 
 #include <cstdint>
 
-alignas(64) static constexpr uint8_t buckets[100] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                 // 13
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,        // 16
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,                    // 12
-    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,                    // 12
-    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,  // 18
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,                    // 12
-    6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,     // 16
+alignas(64) static constexpr std::array<std::uint8_t, 101> buckets = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                            // 13
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,                   // 16
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,                               // 12
+    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,                               // 12
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,             // 18
+    5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,                               // 12
+    6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, DEFAULT_BUCKET // 16
 };
-inline static constexpr size_t mapToBucket(size_t v) {
-  if (v < (sizeof(buckets) / sizeof(uint8_t))) return buckets[v];
-  return DEFAULT_BUCKET;  // let it crash
+inline static constexpr size_t mapToBucket(int v)
+{
+    // const size_t mask = -(v < 100); // 0xFFFFFFFF... if true, 0x0 if false
+    // size_t clamped = (v & mask) | (100 & ~mask);
+
+    return buckets[(v < 100) ? v : 100]; // comparison as int before implicit conversion
 }
 
-std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
-  alignas(64) std::array<std::size_t, NUM_BUCKETS> retBuckets{0};
+std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values)
+{
+    alignas(64) std::array<std::size_t, NUM_BUCKETS> retBuckets{0};
 
-  for (std::size_t i = 0; i + 3 < values.size(); i += 4) {
-    int v0 = values[i], v1 = values[i + 1], v2 = values[i + 2],
-        v3 = values[i + 3];
-    retBuckets[mapToBucket(v0)]++;
-    retBuckets[mapToBucket(v1)]++;
-    retBuckets[mapToBucket(v2)]++;
-    retBuckets[mapToBucket(v3)]++;
-  }
+    for (std::size_t i = 0; i + 3 < values.size(); i += 4)
+    {
+        int v0 = values[i], v1 = values[i + 1], v2 = values[i + 2], v3 = values[i + 3];
+        retBuckets[mapToBucket(v0)]++;
+        retBuckets[mapToBucket(v1)]++;
+        retBuckets[mapToBucket(v2)]++;
+        retBuckets[mapToBucket(v3)]++;
+    }
 
-  return retBuckets;
+    return retBuckets;
 }
