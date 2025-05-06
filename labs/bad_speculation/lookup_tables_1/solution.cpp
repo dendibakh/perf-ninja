d99ca6b6d9186cb08a919c26ce783294d1ed28fc
Your Name <you@example.com>
2025-05-06 12:05:41 +0300

    fix: solution to lookup_tables1, hardcoded values

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -4,21 +4,30 @@
 #ifdef SOL
 #include <array>
 
-constexpr int makeLUT(int i)
-{
-    return (i < 13) ? 0 : (i < 29) ? 1 : (i < 41) ? 2 : (i < 53) ? 3 : (i < 71) ? 4 : (i < 83) ? 5 : (i < 100) ? 6 : -1;
-}
+// constexpr int makeLUT(int i)
+// {
+//     return (i < 13) ? 0 : (i < 29) ? 1 : (i < 41) ? 2 : (i < 53) ? 3 : (i < 71) ? 4 : (i < 83) ? 5 : (i < 100) ? 6 :
+//     -1;
+// }
 
-constexpr std::array<int, 100> generateBuckets()
-{
-    std::array<int, 100> b = {};
-    for (int i = 0; i < 100; ++i)
-        b[i] = makeLUT(i);
-    return b;
-}
+// constexpr std::array<int, 100> generateBuckets()
+// {
+//     std::array<int, 100> b = {};
+//     for (int i = 0; i < 100; ++i)
+//         b[i] = makeLUT(i);
+//     return b;
+// }
 
-alignas(64) constexpr std::array<int, 100> buckets = generateBuckets();
-
+alignas(64) constexpr std::array<int, 100> buckets = {
+    // generateBuckets();
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                // 13
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,       // 16
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,                   // 12
+    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,                   // 12
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, // 18
+    5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,                   // 12
+    6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6     // 16
+};
 static size_t mapToBucket(size_t v)
 {
     if (v < 100)
