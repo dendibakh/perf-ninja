bdc7de441fd737e042c16d03c73752db0caf16b5
Denys Makoviichuk <trrrrr97@gmail.com>
2025-05-26 12:40:24 -0700

    lookup tables 1

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -1,7 +1,17 @@
 #include "solution.hpp"
-
+#define SOLUTION
 static std::size_t mapToBucket(std::size_t v) {
                               //   size of a bucket
+#ifdef SOLUTION
+  constexpr std::array<std::size_t, 7> buckets = {13, 29, 41, 53, 71, 83, 100};
+  std::size_t res = 0;
+  for (auto threshold : buckets) {
+    if (v >= threshold) {
+      ++res;
+    }
+  }
+  return res;
+#else
   if      (v < 13)  return 0; //   13
   else if (v < 29)  return 1; //   16
   else if (v < 41)  return 2; //   12
@@ -10,6 +20,7 @@ static std::size_t mapToBucket(std::size_t v) {
   else if (v < 83)  return 5; //   12
   else if (v < 100) return 6; //   17
   return DEFAULT_BUCKET;
+#endif
 }
 
 std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
