7e7098d01ee785aa2feb3d3c8b2fea777d3a4e2b
Denys Makoviichuk <trrrrr97@gmail.com>
2025-05-26 15:45:00 -0700

    function_inling_1

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -21,7 +21,22 @@ static int compare(const void *lhs, const void *rhs) {
 
   return 0;
 }
-
+#define SOLUTION
 void solution(std::array<S, N> &arr) {
+#ifdef SOLUTION
+  std::sort(arr.begin(), arr.end(),
+  [](const S &a, const S &b) {
+    if (a.key1 < b.key1)   return true;
+    if (a.key1 > b.key1)   return false;
+    // key1 equal ⇒ compare key2
+    if (a.key2 < b.key2)   return true;
+    if (a.key2 > b.key2)   return false;
+    // key1 == key2 ⇒ neither is less
+    return false;
+  }
+  );
+#else
   qsort(arr.data(), arr.size(), sizeof(S), compare);
+#endif
+
 }
