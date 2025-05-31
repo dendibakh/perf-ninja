267ef01cc9969df08543d37e660738251888b1b0
nmnguyen <nguyen2001ag@gmail.com>
2025-05-31 10:06:17 +0800

    Solved Loop tiling 1 final

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -2,10 +2,13 @@
 #include <algorithm>
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
+  static constexpr int TILE_SIZE = 32;
   int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      out[i][j] = in[j][i];
+  for (int i = 0; i < size; i+=TILE_SIZE) {
+    for (int j = 0; j < size; j+=TILE_SIZE) {
+      for (int k = i; k < std::min(size, i + TILE_SIZE); k++) 
+        for (int l = j; l < std::min(size, j + TILE_SIZE); l++)
+          out[k][l] = in[l][k];
     }
   }
   return out[0][size - 1];
