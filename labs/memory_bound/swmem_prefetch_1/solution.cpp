ceef70537b12eb0f6b042c214559eebba73fc664
Shobhit <shobhit32t@gmail.com>
2025-06-11 16:36:48 +0530

    sw_prefetch try

--- labs/memory_bound/swmem_prefetch_1/solution.cpp
+++ labs/memory_bound/swmem_prefetch_1/solution.cpp
@@ -12,9 +12,11 @@ static int getSumOfDigits(int n) {
 int solution(const hash_map_t *hash_map, const std::vector<int> &lookups) {
   int result = 0;
 
-  for (int val : lookups) {
-    if (hash_map->find(val))
-      result += getSumOfDigits(val);
+  for (int idx = 0; idx < lookups.size(); idx++) {
+    if (hash_map->find(lookups[idx]))
+      result += getSumOfDigits(lookups[idx]);
+
+    __builtin_prefetch(&hash_map->m_vector[lookups[idx + 1]]);
   }
 
   return result;
--- labs/memory_bound/swmem_prefetch_1/solution.hpp
+++ labs/memory_bound/swmem_prefetch_1/solution.hpp
@@ -5,10 +5,10 @@ static constexpr std::size_t HASH_MAP_SIZE = 32 * 1024 * 1024 - 5;
 static constexpr std::size_t NUMBER_OF_LOOKUPS = 1024 * 1024;
 
 class hash_map_t {
+public:
     static constexpr int UNUSED = std::numeric_limits<int>::max();
     std::vector<int> m_vector;
     std::size_t N_Buckets;
-public:
     hash_map_t(std::size_t size) : m_vector(size, UNUSED), N_Buckets(size) {}
 
     bool insert(int val) {
