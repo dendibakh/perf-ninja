2d33d1ad1ba329ea2ab3434eb435802c36f8e062
Quinniboi10 <itagakiq@gmail.com>
2025-06-26 16:28:51 -0500

    Data packing attempt 1

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -7,10 +7,10 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
+  unsigned long long l : 14;
+  unsigned i : 7;
+  short s : 7;
+  float d;
   bool b;
 
   bool operator<(const S &s) const { return this->i < s.i; }
