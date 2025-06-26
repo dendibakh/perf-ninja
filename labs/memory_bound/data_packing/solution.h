6db2f7df5c8f6f88c5ab7e5d077278aadf3e2034
nnjankovic <shone23jankovic@gmail.com>
2025-06-26 20:22:14 +0200

    data_packing 3

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -5,16 +5,17 @@ constexpr int N = 1000000;
 constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
+//#pragma pack(push, 4)
 struct S {
   int i       :8;
   short s     :8;
   long long l :16;
+  float d;
   bool b; 
-  double d;
-
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+//#pragma pack(pop)
 
 void init(std::vector<S> &arr);
 S create_entry(int first_value, int second_value);
