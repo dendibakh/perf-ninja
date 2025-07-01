8e81d79a845ece33cb9cab96e2f16a4e5dd5d760
Jackie Dong <dongjackie1999@gmail.com>
2025-07-01 00:33:33 -0400

    data packing

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -6,6 +6,22 @@ constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
+#ifdef SOLUTION
+struct S {
+  float d;  // only need 3 decimal places precision
+  union {
+    struct {
+      bool b : 1;
+      unsigned short l : 15;
+    };
+    unsigned short packed;
+  };
+  char i;
+  char s;
+
+  bool operator<(const S &s) const { return this->i < s.i; }
+};
+#else
 struct S {
   int i;
   long long l;
@@ -15,6 +31,7 @@ struct S {
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+#endif
 
 void init(std::vector<S> &arr);
 S create_entry(int first_value, int second_value);
