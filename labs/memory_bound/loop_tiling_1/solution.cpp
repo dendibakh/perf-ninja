d88ea65a95bb90eae20a8716d82ca2b65f84d12d
nnjankovic <shone23jankovic@gmail.com>
2025-07-12 19:02:59 +0200

    loop_tiling f

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -1,18 +1,17 @@
 #include "solution.hpp"
 #include <algorithm>
 
-#ifdef _WIN32
-    const int TILE_SIZE = 3;
-#else
-    const int TILE_SIZE = 8;
-#endif
-bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
+
+const int TILE_SIZE = 16;
+
+bool solution1(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
   int n = size / TILE_SIZE;
   if(size % TILE_SIZE != 0) {
     n++;
   }
 
+  //can be rewritten better
   for(int tile_i = 0; tile_i < n; tile_i++) {
     for(int tile_j = 0; tile_j < n; tile_j++) {
         for(int i = tile_i*TILE_SIZE; i<(tile_i+1)*TILE_SIZE && i < size; i++) {
@@ -27,8 +26,26 @@ bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   return out[0][size - 1];
 }
 
+bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
+  int size = in.size();
+  
+  for (int ii = 0; ii < size; ii+=TILE_SIZE) {
+    for (int jj = 0; jj < size; jj+=TILE_SIZE) {
 
-bool solution1(MatrixOfDoubles &in, MatrixOfDoubles &out) {
+      for(int i = ii; i < std::min(ii + TILE_SIZE, size); i++) {
+        for(int j = jj; j < std::min(jj + TILE_SIZE, size); j++) {
+          out[j][i] = in[i][j];
+        }
+      }
+
+    }
+  }
+
+  return out[0][size - 1];
+}
+
+
+bool base_solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size; j++) {
