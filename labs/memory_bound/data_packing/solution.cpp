124f8c99ccab010d0b91f80eef0421867891301e
Johannes Mono <midao@asdf.one>
2025-08-03 12:14:29 +0900

    [data_packing] version 00

--- labs/memory_bound/data_packing/solution.cpp
+++ labs/memory_bound/data_packing/solution.cpp
@@ -4,23 +4,5 @@
 #include <random>
 
 void solution(std::vector<S> &arr) {
-  // 1. shuffle
-  static std::random_device rd;
-  static std::mt19937 g(rd());
-  std::shuffle(arr.begin(), arr.end(), g);
-
-  // 2. counting sort
-  constexpr int cntSize = maxRandom - minRandom + 1;
-  std::array<int, cntSize> cnt{};
-  for (const auto& v : arr) {
-    ++cnt[v.i - minRandom + 1];
-  }
-  for (int i = 1; i < cntSize; ++i) {
-    cnt[i] += cnt[i - 1];
-  }
-  std::vector<S> sorted(N);
-  for (const auto& v : arr) {
-    sorted[cnt[v.i - minRandom]++] = v;
-  }
-  arr = sorted;
+  std::sort(arr.begin(), arr.end());
 }
--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -7,10 +7,10 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
   double d;
+  unsigned short l;
+  unsigned char i;
+  unsigned char s;
   bool b;
 
   bool operator<(const S &s) const { return this->i < s.i; }
