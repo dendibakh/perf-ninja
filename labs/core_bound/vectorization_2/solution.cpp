a7073595e16d9eb6da5a6b57d0bafc515775eaac
hanyuhang <18700873675@163.com>
2025-08-12 00:44:35 +0800

    test for Vectorization 2

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,5 +1,27 @@
 #include "solution.hpp"
 
+#define SOLUTION
+
+#ifdef SOLUTION
+uint16_t checksum(const Blob &blob) {
+  uint32_t acc = 0;
+  for (auto value : blob) {
+    acc += value;
+  }
+
+  // sum up two halves of uint32
+  auto high = acc >> 16;
+  auto low = acc & 0xFFFFu;
+  acc = low + high;
+
+  // account for potential overflow
+  high = acc >> 16;
+  low = acc & 0xFFFFu;
+  acc = low + high;
+
+  return static_cast<uint16_t>(acc);
+}
+#else
 uint16_t checksum(const Blob &blob) {
   uint16_t acc = 0;
   for (auto value : blob) {
@@ -8,3 +30,4 @@ uint16_t checksum(const Blob &blob) {
   }
   return acc;
 }
+#endif
