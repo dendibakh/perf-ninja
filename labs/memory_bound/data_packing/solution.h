774f17347740692cc7c4d96460c03cf7f41ee69c
hanyuhang <18700873675@163.com>
2025-08-28 22:23:24 +0800

    test for Data packing

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -5,6 +5,19 @@ constexpr int N = 1000000;
 constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
+#define SOLUTION
+
+#ifdef SOLUTION
+struct S {
+  float d;
+  unsigned long long l:16;
+  unsigned int i:8;
+  unsigned short s:7;
+  bool b:1;
+
+  bool operator<(const S &s) const { return this->i < s.i; }
+};
+#else
 // FIXME: this data structure can be reduced in size
 struct S {
   int i;
@@ -15,6 +28,12 @@ struct S {
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+#endif
+
+template <int N>
+class TD;
+// never complies but shows the value of sizeof(S)
+// TD<sizeof(S)> td;
 
 void init(std::vector<S> &arr);
 S create_entry(int first_value, int second_value);
