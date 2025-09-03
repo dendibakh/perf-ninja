770f5d132a49e3faad852990fbbe420d518730b5
CrazyRoka <rokarostuk@gmail.com>
2025-09-03 23:38:55 +0100

    Add solution for function inlining

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -3,25 +3,21 @@
 #include <algorithm>
 #include <stdlib.h>
 
-static int compare(const void *lhs, const void *rhs) {
-  auto &a = *reinterpret_cast<const S *>(lhs);
-  auto &b = *reinterpret_cast<const S *>(rhs);
-
+static bool compare(const S& a, const S& b) {
   if (a.key1 < b.key1)
-    return -1;
+    return true;
 
   if (a.key1 > b.key1)
-    return 1;
+    return false;
 
   if (a.key2 < b.key2)
-    return -1;
+    return true;
 
-  if (a.key2 > b.key2)
-    return 1;
-
-  return 0;
+  return false;
 }
 
 void solution(std::array<S, N> &arr) {
-  qsort(arr.data(), arr.size(), sizeof(S), compare);
+  std::sort(arr.data(), arr.data() + arr.size(), [](const S& a, const S& b) {
+    return compare(a, b);
+  });
 }
