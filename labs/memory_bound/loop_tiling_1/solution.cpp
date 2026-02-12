0ceb02e476e0687f63950644b1b591f7f170026c
dahroug <hassan.dahroog04@eng-st.cu.edu.eg>
2026-02-12 12:10:21 +0200

    tilling

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -3,8 +3,23 @@
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
+  for (int i = 0; i < size/2; i++) {
+    for (int j = 0; j < size/2; j++) {
+      out[i][j] = in[j][i];
+    }
+  }
+    for (int i = 0; i < size/2; i++) {
+    for (int j = size/2; j < size; j++) {
+      out[i][j] = in[j][i];
+    }
+  }
+    for (int i = size/2; i < size; i++) {
+    for (int j = 0; j < size/2; j++) {
+      out[i][j] = in[j][i];
+    }
+  }
+    for (int i = size/2; i < size; i++) {
+    for (int j = size/2; j < size; j++) {
       out[i][j] = in[j][i];
     }
   }
