fb1d947b52af931171127759f46da3baf6c58d65
ben :) <52897202+benC1232@users.noreply.github.com>
2026-03-04 19:57:58 +0200

    trying the official solution to see if im the problem :P

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -1,19 +1,24 @@
 #include "solution.hpp"
 
-static std::size_t mapToBucket(std::size_t v) {
-                              //   size of a bucket
-  if      (v < 13)  return 0; //   13
-  else if (v < 29)  return 1; //   16
-  else if (v < 41)  return 2; //   12
-  else if (v < 53)  return 3; //   12
-  else if (v < 71)  return 4; //   18
-  else if (v < 83)  return 5; //   12
-  else if (v < 100) return 6; //   17
-  return DEFAULT_BUCKET;
+int buckets[100] = {
+// 1  2  3  4  5  6  7  8  9  0  1  2  3  4  5  6  7  8  9  0
+   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+   2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+   3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+   4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+   5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+   6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6
+};
+
+static size_t mapToBucket(size_t v) {
+  if (v < (sizeof (buckets) / sizeof (int)))
+    return buckets[v];
+  return -1; // let it crash
 }
 
-std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
-  std::array<std::size_t, NUM_BUCKETS> retBuckets{0};
+std::array<size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
+  std::array<size_t, NUM_BUCKETS> retBuckets{0};
   for (auto v : values) {
     retBuckets[mapToBucket(v)]++;
   }
