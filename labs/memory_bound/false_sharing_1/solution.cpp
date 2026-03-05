f628ca87128acb639cf91af3b9b99e8a31ae9fe3
בר אברמוביץ <abramovichido@gmail.com>
2026-03-05 09:55:36 +0200

    false sharing

--- labs/memory_bound/false_sharing_1/solution.cpp
+++ labs/memory_bound/false_sharing_1/solution.cpp
@@ -20,6 +20,7 @@ std::size_t solution(const std::vector<uint32_t> &data, int thread_count) {
     auto &target = accumulators[target_index];
 
 #pragma omp for
+    int sum = 0;
     for (int i = 0; i < data.size(); i++) {
       // Perform computation on each input
       auto item = data[i];
@@ -28,8 +29,9 @@ std::size_t solution(const std::vector<uint32_t> &data, int thread_count) {
       item |= (item >> 24);
 
       // Write result to accumulator
-      target.value += item % 13;
+      sum += item % 13;
     }
+    target.value = sum;
   }
 
   std::size_t result = 0;
