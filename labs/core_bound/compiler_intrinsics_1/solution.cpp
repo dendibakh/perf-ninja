3ab2142a365690317e432b47abbc92e494cdf4e2
בר אברמוביץ <abramovichido@gmail.com>
2026-03-09 16:32:36 +0200

    compilier intrinsics1

--- labs/core_bound/compiler_intrinsics_1/solution.cpp
+++ labs/core_bound/compiler_intrinsics_1/solution.cpp
@@ -3,29 +3,6 @@
 #include <immintrin.h>
 #include <memory>
 
-static inline __m128i prefix_sum_4_epi32(__m128i v) {
-    __m128i t;
-
-    // shift by 1 int (4 bytes)
-    t = _mm_slli_si128(v, 4);
-    v = _mm_add_epi32(v, t);
-
-    // shift by 2 ints (8 bytes)
-    t = _mm_slli_si128(v, 8);
-    v = _mm_add_epi32(v, t);
-
-    return v;
-}
-
-static inline __m128i load_u8x4_to_i32(const uint8_t* ptr) {
-    return _mm_setr_epi32(
-        static_cast<int>(ptr[0]),
-        static_cast<int>(ptr[1]),
-        static_cast<int>(ptr[2]),
-        static_cast<int>(ptr[3])
-    );
-}
-
 void imageSmoothing(const InputVector &input, uint8_t radius,
                     OutputVector &output) {
   int pos = 0;
@@ -46,26 +23,32 @@ void imageSmoothing(const InputVector &input, uint8_t radius,
 
   // 2. main loop.
   limit = size - radius;
-  for (; pos + 4 <= limit; pos += 4) {
-    __m128i entering = load_u8x4_to_i32(input.data() + pos + radius);
-    __m128i leaving  = load_u8x4_to_i32(input.data() + pos - radius - 1);
+  const uint8_t *substract_ptr = input.data() + pos - radius - 1;
+  const uint8_t *add_ptr = input.data() + pos + radius;
+  const uint16_t *outputPtr = output.data() + pos;
 
-    __m128i delta = _mm_sub_epi32(entering, leaving);
-    __m128i scan  = prefix_sum_4_epi32(delta);
+  __m128i currentSum_m128i = _mm_set1_epi16(currentSum);
 
-    __m128i base = _mm_set1_epi32(static_cast<int>(currentSum));
-    __m128i output_block = _mm_add_epi32(scan, base);
+  int i = 0;
+  for (; i + 8 <= limit - pos; i += 8) {
+    __m128i substract_m128i = _mm_loadu_si64(substract_ptr + i);
+    __m128i add_m128i = _mm_loadu_si64(add_ptr + i);
+    __m128i sub = _mm_cvtepu8_epi16(substract_m128i);
+    __m128i add = _mm_cvtepu8_epi16(add_m128i);
 
-    alignas(16) int tmp[4];
-    _mm_store_si128(reinterpret_cast<__m128i*>(tmp), output_block);
+    __m128i diff = _mm_sub_epi16(add, sub);
 
-    output[pos + 0] = static_cast<uint16_t>(tmp[0]);
-    output[pos + 1] = static_cast<uint16_t>(tmp[1]);
-    output[pos + 2] = static_cast<uint16_t>(tmp[2]);
-    output[pos + 3] = static_cast<uint16_t>(tmp[3]);
+    __m128i s = _mm_add_epi16(diff, _mm_slli_si128(diff, 2));
+    s = _mm_add_epi16(s, _mm_slli_si128(s, 4));
+    s = _mm_add_epi16(s, _mm_slli_si128(s, 8));
 
-    currentSum = static_cast<uint32_t>(tmp[3]);
+    __m128i result = _mm_add_epi16(s, currentSum_m128i);
+    _mm_storeu_si128((__m128i*)(outputPtr + i), result);
+
+    currentSum = (uint16_t)_mm_extract_epi16(result, 7);
+    currentSum_m128i = _mm_set1_epi16(currentSum);
   }
+  pos += i;
 
   // שארית scalar
   for (; pos < limit; ++pos) {
@@ -74,7 +57,6 @@ void imageSmoothing(const InputVector &input, uint8_t radius,
     output[pos] = currentSum;
   }
 
-  currentSum = output[pos - 1];
   // 3. special case, executed only if size <= 2*radius + 1
   limit = std::min(radius + 1, size);
   for (; pos < limit; pos++) {
