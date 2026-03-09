9705f4a05f6dd0dedce022cbe55fa6fbeb851779
בר אברמוביץ <abramovichido@gmail.com>
2026-03-09 17:23:57 +0200

    vectorization2

--- labs/core_bound/vectorization_2/solution.cpp
+++ labs/core_bound/vectorization_2/solution.cpp
@@ -1,17 +1,11 @@
 #include "solution.hpp"
 
 uint16_t checksum(const Blob &blob) {
-  uint16_t acc = 0;
-  uint16_t carry = 0;
+  uint32_t acc = 0;
   for (auto value : blob) {
     acc += value;
-    carry += acc < value; // add carry
   }
-  acc += carry;
-  if (acc < carry)
-  {
-    acc += 1;
-  }
-  
-  return acc;
+  uint16_t low  = acc & 0xFFFF;      // last 16 bits
+  uint16_t high = acc >> 16;         // first 16 bits
+  return low + high + (low + high > UINT16_MAX ? 1 : 0);
 }
