b978f976557741dd1d6faf0bbeaaf5f8da76bfc5
ben :) <52897202+benC1232@users.noreply.github.com>
2026-03-09 11:16:15 +0200

    solved function inlining

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -23,5 +23,10 @@ static int compare(const void *lhs, const void *rhs) {
 }
 
 void solution(std::array<S, N> &arr) {
-  qsort(arr.data(), arr.size(), sizeof(S), compare);
+  std::sort(arr.begin(), arr.end(), [](const S &a, const S &b) {
+    if (a.key1 != b.key1) {
+      return a.key1 < b.key1;
+    }
+    return a.key2 < b.key2;
+  });
 }
