581686631117351dfe4e76228e06fdc68b8a593c
Ju5t1nL3 <justinle2006@gmail.com>
2026-03-10 02:15:59 -0500

    Remove if tags for solution

--- labs/misc/warmup/solution.cpp
+++ labs/misc/warmup/solution.cpp
@@ -3,12 +3,5 @@
 
 int solution(int *arr, int N) {
   int res = 0;
-#ifdef SOLUTION
   return (N * (N + 1)) / 2;
-#else
-  for (int i = 0; i < N; i++) {
-    res += arr[i];
-  }
-  return res;
-#endif
 }
