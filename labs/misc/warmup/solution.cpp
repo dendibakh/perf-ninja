9087689e4b2dd544543aaaf5bc3fb7395ed542e7
Ju5t1nL3 <justinle2006@gmail.com>
2026-03-10 02:12:38 -0500

    Add solution

--- labs/misc/warmup/solution.cpp
+++ labs/misc/warmup/solution.cpp
@@ -3,8 +3,12 @@
 
 int solution(int *arr, int N) {
   int res = 0;
+#ifdef SOLUTION
+  return (N * (N + 1)) / 2;
+#else
   for (int i = 0; i < N; i++) {
     res += arr[i];
   }
   return res;
+#endif
 }
