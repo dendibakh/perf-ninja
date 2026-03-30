9bfca6930cbed70f4130a0d10178975560519f3a
RoopamTaneja <roopamtaneja999@gmail.com>
2026-03-30 19:03:22 +0530

    finishing remaining flaws

--- labs/core_bound/compiler_intrinsics_1/solution.cpp
+++ labs/core_bound/compiler_intrinsics_1/solution.cpp
@@ -56,6 +56,22 @@ void imageSmoothing(const InputVector &input, uint8_t radius,
     currentSum = _mm_extract_epi16(result2, 7);
   }
 
+  for (; pos <= limit - 8; pos += 8)
+  {
+    auto sub_vec = _mm_loadu_si64(inPtr + pos - radius - 1);
+    auto sub = _mm_cvtepu8_epi16(sub_vec);
+    auto add_vec = _mm_loadu_si64(inPtr + pos + radius);
+    auto add = _mm_cvtepu8_epi16(add_vec);
+
+    auto diff = _mm_sub_epi16(add, sub);
+    auto sum = _mm_add_epi16(diff, _mm_slli_si128(diff, 2));
+    sum = _mm_add_epi16(sum, _mm_slli_si128(sum, 4));
+    sum = _mm_add_epi16(sum, _mm_slli_si128(sum, 8));
+    auto result = _mm_add_epi16(_mm_set1_epi16(currentSum), sum);
+    _mm_storeu_si128(reinterpret_cast<__m128i *>(outPtr + pos), result);
+    currentSum = _mm_extract_epi16(result, 7);
+  }
+
   for (; pos < limit; ++pos) {
     currentSum -= input[pos - radius - 1];
     currentSum += input[pos + radius];
