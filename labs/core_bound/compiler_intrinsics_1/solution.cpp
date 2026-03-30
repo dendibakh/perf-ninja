a43e5d6cf54283e76cac09e1823f78b7645a9f67
RoopamTaneja <roopamtaneja999@gmail.com>
2026-03-30 18:55:22 +0530

    intrinsics 1, try again

--- labs/core_bound/compiler_intrinsics_1/solution.cpp
+++ labs/core_bound/compiler_intrinsics_1/solution.cpp
@@ -25,13 +25,13 @@ void imageSmoothing(const InputVector &input, uint8_t radius,
   // 2. main loop.
   limit = size - radius;
 
+  const auto *inPtr = input.data();
+  auto* outPtr = output.data();
   for (; pos <= limit - 8; pos += 8)
   {
-    const auto *sub_ptr = input.data() + pos - radius - 1;
-    auto sub_vec = _mm_loadu_si64(sub_ptr);
+    auto sub_vec = _mm_loadu_si64(inPtr + pos - radius - 1);
     auto sub = _mm_cvtepu8_epi16(sub_vec);
-    const auto *add_ptr = input.data() + pos + radius;
-    auto add_vec = _mm_loadu_si64(add_ptr);
+    auto add_vec = _mm_loadu_si64(inPtr + pos + radius);
     auto add = _mm_cvtepu8_epi16(add_vec);
 
     auto diff = _mm_sub_epi16(add, sub);
@@ -39,8 +39,7 @@ void imageSmoothing(const InputVector &input, uint8_t radius,
     sum = _mm_add_epi16(sum, _mm_slli_si128(sum, 4));
     sum = _mm_add_epi16(sum, _mm_slli_si128(sum, 8));
     auto result = _mm_add_epi16(_mm_set1_epi16(currentSum), sum);
-    auto *outPtr = reinterpret_cast<__m128i *>(output.data() + pos);
-    _mm_storeu_si128(outPtr, result);
+    _mm_storeu_si128(reinterpret_cast<__m128i *>(outPtr + pos), result);
     currentSum = _mm_extract_epi16(result, 7);
   }
 
