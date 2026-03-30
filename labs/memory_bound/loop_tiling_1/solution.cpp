baa3705ad95f5cf26353a9060d8c92d9672903d1
RoopamTaneja <roopamtaneja999@gmail.com>
2026-03-30 23:59:09 +0530

    loop tiling 1 done again

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -3,7 +3,7 @@
 
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  constexpr int TILE_SIZE = 32;
+  constexpr int TILE_SIZE = 16;
   for (int i = 0; i < size; i+=TILE_SIZE) {
     for (int j = 0; j < size; j+=TILE_SIZE) {
       int bi = std::min(i + TILE_SIZE, size);
