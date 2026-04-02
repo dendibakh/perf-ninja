d5c0055ef7153e9966c8031d68e16009cdd4b575
RoopamTaneja <roopamtaneja999@gmail.com>
2026-04-02 21:43:24 +0530

    virtual function, attempt 1

--- labs/bad_speculation/virtual_call_mispredict/solution.cpp
+++ labs/bad_speculation/virtual_call_mispredict/solution.cpp
@@ -5,17 +5,22 @@
 void generateObjects(InstanceArray& array) {
     std::default_random_engine generator(0);
     std::uniform_int_distribution<std::uint32_t> distribution(0, 2);
-
+    uint32_t cnt0 = 0, cnt1 = 0, cnt2 = 0;
     for (std::size_t i = 0; i < N; i++) {
         int value = distribution(generator);
-        if (value == 0) {
-            array.push_back(std::make_unique<ClassA>());
-        } else if (value == 1) {
-            array.push_back(std::make_unique<ClassB>());
-        } else {
-            array.push_back(std::make_unique<ClassC>());
-        }
+        if (value == 0)
+            cnt0++;
+        else if (value == 1)
+            cnt1++;
+        else
+            cnt2++;
     }
+    for (auto i = 0; i < cnt0; i++)
+        array.push_back(std::make_unique<ClassA>());
+    for (auto i = 0; i < cnt1; i++)
+        array.push_back(std::make_unique<ClassB>());
+    for (auto i = 0; i < cnt2; i++)
+        array.push_back(std::make_unique<ClassC>());
 }
 
 // Invoke the `handle` method on all instances in `output`
