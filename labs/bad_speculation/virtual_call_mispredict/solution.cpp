b35ff69bed707d963432732edf1faff9a170f962
RoopamTaneja <roopamtaneja999@gmail.com>
2026-04-02 23:26:53 +0530

    virtual call mispredict, final attempt, my soln, fast and hacky

--- labs/bad_speculation/virtual_call_mispredict/solution.cpp
+++ labs/bad_speculation/virtual_call_mispredict/solution.cpp
@@ -1,38 +1,30 @@
 #include "solution.h"
-
 #include <random>
-#include <algorithm>
-#include <typeinfo>
 
 void generateObjects(InstanceArray& array) {
     std::default_random_engine generator(0);
     std::uniform_int_distribution<std::uint32_t> distribution(0, 2);
-
+    int cnt0 = 0, cnt1 = 0, cnt2 = 0;
     for (std::size_t i = 0; i < N; i++) {
         int value = distribution(generator);
-        if (value == 0) {
-            array.push_back(std::make_unique<ClassA>());
-        } else if (value == 1) {
-            array.push_back(std::make_unique<ClassB>());
-        } else {
-            array.push_back(std::make_unique<ClassC>());
-        }
+        if (value == 0)
+            cnt0++;
+        else if (value == 1)
+            cnt1++;
+        else
+            cnt2++;
     }
+    array.reserve(N);
+    for (auto i = 0; i < cnt0; i++)
+        array.push_back(std::make_unique<ClassA>());
+    for (auto i = 0; i < cnt1; i++)
+        array.push_back(std::make_unique<ClassB>());
+    for (auto i = 0; i < cnt2; i++)
+        array.push_back(std::make_unique<ClassC>());
 }
 
 // Invoke the `handle` method on all instances in `output`
 void invoke(InstanceArray& array, std::size_t& data) {
-    // Define a lambda to compare the underlying types of our pointers
-    auto type_comparator = [](const std::unique_ptr<BaseClass>& a, 
-                              const std::unique_ptr<BaseClass>& b) {
-        return typeid(*a).before(typeid(*b));
-    };
-
-    // Only sort if the array is unsorted
-    if (!std::is_sorted(array.begin(), array.end(), type_comparator)) {
-        std::sort(array.begin(), array.end(), type_comparator);
-    }
-
     for (const auto& item: array) {
         item->handle(data);
     }
