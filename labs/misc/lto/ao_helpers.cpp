0cd9ba0113f644e4452a4e7868c8a6268f021a0a
RoopamTaneja <roopamtaneja999@gmail.com>
2026-04-03 16:16:10 +0530

    lto, attempt 2, flto in cmake

---
+++ labs/misc/lto/ao_helpers.cpp
@@ -0,0 +1,51 @@
+#include "ao.h"
+
+double vdot(vec v0, vec v1)
+{
+    return v0.x * v1.x + v0.y * v1.y + v0.z * v1.z;
+}
+
+void vcross(vec *c, vec v0, vec v1)
+{
+    
+    c->x = v0.y * v1.z - v0.z * v1.y;
+    c->y = v0.z * v1.x - v0.x * v1.z;
+    c->z = v0.x * v1.y - v0.y * v1.x;
+}
+
+void vnormalize(vec *c)
+{
+    double length = sqrt(vdot((*c), (*c)));
+
+    if (fabs(length) > 1.0e-17) {
+        c->x /= length;
+        c->y /= length;
+        c->z /= length;
+    }
+}
+
+unsigned char
+clamp(double f)
+{
+  int i = (int)(f * 255.5);
+
+  if (i < 0) i = 0;
+  if (i > 255) i = 255;
+
+  return (unsigned char)i;
+}
+
+void
+saveppm(const char *fname, int w, int h, unsigned char *img)
+{
+    FILE *fp;
+
+    fp = fopen(fname, "wb");
+    assert(fp);
+
+    fprintf(fp, "P6\n");
+    fprintf(fp, "%d %d\n", w, h);
+    fprintf(fp, "255\n");
+    fwrite(img, w * h * 3, 1, fp);
+    fclose(fp);
+}
