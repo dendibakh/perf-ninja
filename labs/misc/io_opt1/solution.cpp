5b3a629914673620358fa9e72bc6cc8a11260e8a
RoopamTaneja <roopamtaneja999@gmail.com>
2026-04-03 08:10:50 +0530

    io opt 1, attempt 2

--- labs/misc/io_opt1/solution.cpp
+++ labs/misc/io_opt1/solution.cpp
@@ -1,18 +1,26 @@
 #include "solution.hpp"
-#include "MappedFile.hpp"
-
+#include <array>
 #include <fstream>
 #include <stdexcept>
 
 uint32_t solution(const char *file_name) {
+  std::fstream file_stream{file_name};
+  if (!file_stream.is_open())
+    throw std::runtime_error{"The file could not be opened"};
 
   // Initial value has all bits set to 1
   uint32_t crc = 0xff'ff'ff'ff;
 
-  MappedFile f(file_name);
-  auto sv = f.getContents();
-  for (auto c : sv) {
-    update_crc32(crc, static_cast<uint8_t>(c));
+  static constexpr int N = 4096;
+  std::array<char, N> buffer;
+  
+  while (true) {
+    file_stream.read(buffer.data(), N);
+    auto bytes_read = file_stream.gcount();
+    for (std::streamsize i = 0; i < bytes_read; i++)
+      update_crc32(crc, static_cast<uint8_t>(buffer[i]));
+    if (file_stream.eof())
+      break;
   }
 
   // Invert the bits
