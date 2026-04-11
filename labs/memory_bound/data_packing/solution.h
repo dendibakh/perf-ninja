37206872d65817081c6ec8918f244d9ef47bbb8a
Sh0g0-1758 <shouryagoel10000@gmail.com>
2026-04-11 05:34:11 +0530

    data packing solution

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -6,12 +6,24 @@ constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
+/*
+4 + pad(4) + 8 + 2 + pad(6) + 8 + 1 + pad(7) = 40 [original]
+8 + 8 + 4 + 2 + 1 + 1 = 24 [after rearrangement]
+4 + 4 = 8 [bit packing]
+
+NOTE: For bit packing, l needs 14 bits, i needs 7 bits and s needs 7 bits. 
+But they are all signed, hence we should reserve one extra bit for all of them.
+l -> 15, i -> 8, s -> 8
+the final 1 bit can then be used for b.  
+double is changed to float because according to the input, the value that was
+being stored in double could be represented by a float just as well. 
+*/
 struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
-  bool b;
+  float d;
+  long long l : 15;
+  int i : 8;
+  short s : 8;
+  bool b : 1;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
