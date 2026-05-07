da19514917c57e363fd17f7dae360a8e2642edfe
Stanko Kovacevic <stkovacevic1994@gmail.com>
2026-05-07 02:06:09 +0100

    Data Packing - Solution 2

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -7,14 +7,19 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  long long l;
-  double d;
-  int i;
-  short s;
-  bool b;
+  float d;
+  long long l:16;
+  int i:8;
+  short s:7;
+  bool b:1;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+
+template <int N>
+class TD;
+
+TD<sizeof(S)>; // compile-time assertion to check the size of S
 
 void init(std::vector<S> &arr);
 S create_entry(int first_value, int second_value);
