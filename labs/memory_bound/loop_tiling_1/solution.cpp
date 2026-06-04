1a16429efac507d7a480a54378e835ea19361aeb
John Ziamos <iziamos@gmail.com>
2026-06-04 23:48:22 +0100

    loop tiling 1

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -1,11 +1,16 @@
 #include "solution.hpp"
 #include <algorithm>
-
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
-  for (int i = 0; i < size; i++) {
+  for (int i = 0; i < size - 1; i+=2) {
     for (int j = 0; j < size; j++) {
       out[i][j] = in[j][i];
+      out[i + 1][j] = in[j][i + 1];
+    }
+  }
+  if (size % 2 == 1) {
+    for (int j = 0; j < size; j++) {
+      out[size - 1][j] = in[j][size -1];
     }
   }
   return out[0][size - 1];
