9448ef3cee89a1f71d06af7743d0f9cf88154c69
Artem Brezhnev <aebrezhnev@edu.hse.ru>
2026-08-12 21:52:39 +0300

    COMMON

--- labs/bad_speculation/branches_to_cmov_1/solution.hpp
+++ labs/bad_speculation/branches_to_cmov_1/solution.hpp
@@ -12,10 +12,10 @@ constexpr int GridYDimension = 1025;
 constexpr int NumberOfSims = 10;
 
 
-const std::array<int, 8> dx{-1, 0, 1, -1, 1,-1, 0, 1};
-const std::array<int, 8> dy{-1, -1, -1, 0, 0, 1, 1, 1};
+const std::vector<int> dx{-1, 0, 1, -1, 1,-1, 0, 1};
+const std::vector<int> dy{-1, -1, -1, 0, 0, 1, 1, 1};
 
-const std::array<std::array<int, 9>, 2> transition{{
+const std::vector<std::vector<int>> transition{{
     {0, 0, 0, 1, 0, 0, 0, 0, 0},
     {0, 0, 1, 1, 0, 0, 0, 0, 0}
 }};
