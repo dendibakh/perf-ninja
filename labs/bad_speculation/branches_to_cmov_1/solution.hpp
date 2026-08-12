edb5c477f24d0b1523593451c487bd8564210262
Artem Brezhnev <aebrezhnev@edu.hse.ru>
2026-08-12 13:50:28 +0300

    save

--- labs/bad_speculation/branches_to_cmov_1/solution.hpp
+++ labs/bad_speculation/branches_to_cmov_1/solution.hpp
@@ -12,7 +12,7 @@ constexpr int NumberOfSims = 10;
 
 
 constexpr std::array<int, 8> dx{1,1,1,0,-1,-1,-1,0};
-constexpr std::array<int, 8> dy{-1,1,1,1,1,0,-1,-1};
+constexpr std::array<int, 8> dy{-1,0,1,1,1,0,-1,-1};
 
 constexpr std::array<std::array<int, 9>, 2> transition{{
     {0, 0, 0, 1, 0, 0, 0, 0, 0},
