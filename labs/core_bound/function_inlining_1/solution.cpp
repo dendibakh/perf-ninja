5bcce49d1a111ccefbe84603b95eaef153d1312c
Ivan Esipov <esipoff.vania@yandex.ru>
2026-08-13 17:09:50 +0000

    inline 1st

--- labs/core_bound/function_inlining_1/solution.cpp
+++ labs/core_bound/function_inlining_1/solution.cpp
@@ -23,5 +23,5 @@ static int compare(const void *lhs, const void *rhs) {
 }
 
 void solution(std::array<S, N> &arr) {
-  qsort(arr.data(), arr.size(), sizeof(S), compare);
+  std::sort(arr.begin(), arr.end());
 }
--- labs/core_bound/function_inlining_1/solution.h
+++ labs/core_bound/function_inlining_1/solution.h
@@ -9,6 +9,10 @@ constexpr size_t N = 10000;
 struct S {
   uint32_t key1;
   uint32_t key2;
+
+  __attribute__((always_inline)) bool operator<(const S& b) const {
+    return key1 < b.key1 || (key1 == b.key1 && key2 < b.key2);
+  }
 };
 
 void init(std::array<S, N> &arr);
