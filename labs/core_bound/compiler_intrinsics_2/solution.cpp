423916c60149049d3886fcd90dd162771cf23884
Ivan Esipov <esipoff.vania@yandex.ru>
2026-08-16 22:26:29 +0000

    intr2

--- labs/core_bound/compiler_intrinsics_2/solution.cpp
+++ labs/core_bound/compiler_intrinsics_2/solution.cpp
@@ -1,4 +1,5 @@
 #include "solution.hpp"
+#include <immintrin.h>
 #include <iostream>
 
 // Find the longest line in a file.
@@ -16,8 +17,30 @@ unsigned solution(const std::string &inputContents) {
   unsigned longestLine = 0;
   unsigned curLineLength = 0;
 
-  for (auto s : inputContents) {
-    curLineLength = (s == '\n') ? 0 : curLineLength + 1;
+  int sz = inputContents.size();
+  const char* data = inputContents.data();
+
+  __m256i backslash = _mm256_set1_epi8('\n');
+
+  int i = 0;
+  for (i = 0; i + 32 < sz;) {
+    __m256i vec = _mm256_loadu_si256((__m256i*)(char*)&data[i]);
+    int mask = _mm256_cmpeq_epi8_mask(vec, backslash);
+    if (mask) {
+      int x = __builtin_ctz(mask);
+      longestLine = std::max(curLineLength + x, longestLine);
+      curLineLength = 0;
+      i += x + 1;
+    } else {
+      i += 32;
+      curLineLength += 32;
+      longestLine = std::max(curLineLength, longestLine);
+    }
+  }
+
+
+  for (; i < sz; ++i) {
+    curLineLength = (data[i] == '\n') ? 0 : curLineLength + 1;
     longestLine = std::max(curLineLength, longestLine);
   }
 
