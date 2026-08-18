339d8d59a591b014f5f3a46bf3a716a0f7c1423f
Ivan Esipov <esipoff.vania@yandex.ru>
2026-08-18 14:23:11 +0000

    sw prefetch

--- labs/memory_bound/swmem_prefetch_1/solution.cpp
+++ labs/memory_bound/swmem_prefetch_1/solution.cpp
@@ -11,8 +11,16 @@ static int getSumOfDigits(int n) {
 
 int solution(const hash_map_t *hash_map, const std::vector<int> &lookups) {
   int result = 0;
-
-  for (int val : lookups) {
+  static constexpr int WINDOW = 16;
+  const int sz = lookups.size();
+  for (int i = 0; i < sz - WINDOW; ++i) {
+    auto val = lookups[i];
+    if (hash_map->find(val))
+      result += getSumOfDigits(val);
+    hash_map->pref(lookups[i + WINDOW]);
+  }
+  for (int i = sz - WINDOW; i < sz; ++i) {
+    auto val = lookups[i];
     if (hash_map->find(val))
       result += getSumOfDigits(val);
   }
