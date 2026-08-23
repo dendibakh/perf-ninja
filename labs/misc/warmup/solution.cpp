cfdb091a146f775041b877a6847e926a93d06dd5
a-clap <clapinskiadam@gmail.com>
2026-08-23 20:18:10 +0200

    feat(warmup): init

--- labs/misc/warmup/solution.cpp
+++ labs/misc/warmup/solution.cpp
@@ -1,10 +1,4 @@
 
 #include "solution.h"
 
-int solution(int *arr, int N) {
-  int res = 0;
-  for (int i = 0; i < N; i++) {
-    res += arr[i];
-  }
-  return res;
-}
+int solution(int *arr, int N) { return (N * (N + 1)) / 2; }
