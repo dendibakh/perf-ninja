17d5a87ec72973d42f38fb58a42a6982832ed60e
a-clap <clapinskiadam@gmail.com>
2026-08-23 20:58:21 +0200

    feat(memory_bound): data_packing

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -7,11 +7,12 @@ constexpr int maxRandom = 100;
 
 // FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
-  bool b;
+  float d;
+
+  unsigned l : 14;
+  unsigned i : 7;
+  unsigned s : 7;
+  bool b : 1;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
