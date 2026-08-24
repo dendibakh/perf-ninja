f2d08a7daccd37050d263f29e42625568ceea37c
Ivan Esipov <esipoff.vania@yandex.ru>
2026-08-24 15:55:52 +0000

    cond store

--- labs/bad_speculation/conditional_store_1/solution.cpp
+++ labs/bad_speculation/conditional_store_1/solution.cpp
@@ -5,10 +5,10 @@
 std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
                    const std::uint32_t lower, const std::uint32_t upper) {
   std::size_t count = 0;
+  const std::uint32_t d = upper - lower;
   for (const auto item : input) {
-    if ((lower <= item.first) && (item.first <= upper)) {
-      output[count++] = item;
-    }
+    output[count++] = item;
+    count -= ((lower > item.first) || (item.first > upper));
   }
   return count;
 }
