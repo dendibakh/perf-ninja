fb7cfbc9e488ad92f02db0dc821dbf7dc00a03bb
Hamza Nasir <hamzanasir@hotmail.co.uk>
2026-09-02 00:29:57 +0100

    Solve core_bound/compiler_intrinsics_1

--- labs/core_bound/compiler_intrinsics_1/solution.cpp
+++ labs/core_bound/compiler_intrinsics_1/solution.cpp
@@ -1,6 +1,19 @@
 
 #include "solution.h"
+
+#include <cstring>
+#include <immintrin.h>
+#include <iostream>
 #include <memory>
+#include <ostream>
+
+// Generates a mask where lanes < limit are 0xFFFFFFFF, and lanes >= limit are
+// 0x0
+__m256i generate_limit_mask(int limit) {
+  __m256i indices = _mm256_setr_epi32(0, 1, 2, 3, 4, 5, 6, 7);
+  __m256i limit_vec = _mm256_set1_epi32(limit);
+  return _mm256_cmpgt_epi32(limit_vec, indices);
+}
 
 void imageSmoothing(const InputVector &input, uint8_t radius,
                     OutputVector &output) {
@@ -22,6 +35,21 @@ void imageSmoothing(const InputVector &input, uint8_t radius,
 
   // 2. main loop.
   limit = size - radius;
+  for (; pos + 8 <= limit; pos += 8) {
+    __m128i sub = _mm_cvtepu8_epi16(_mm_loadl_epi64(reinterpret_cast<const __m128i*>(&input[pos - radius - 1])));
+    __m128i add = _mm_cvtepu8_epi16(_mm_loadl_epi64(reinterpret_cast<const __m128i*>(&input[pos + radius])));
+
+    __m128i sumVec = _mm_sub_epi16(add, sub);
+    sumVec = _mm_add_epi16(sumVec, _mm_setr_epi16(currentSum, 0, 0, 0, 0, 0, 0, 0));
+
+    static constexpr int bytesPerLane = 2;
+    sumVec = _mm_add_epi16(sumVec, _mm_bslli_si128(sumVec, bytesPerLane));
+    sumVec = _mm_add_epi16(sumVec, _mm_bslli_si128(sumVec, bytesPerLane * 2));
+    sumVec = _mm_add_epi16(sumVec, _mm_bslli_si128(sumVec, bytesPerLane * 4));
+
+    _mm_storeu_si128(reinterpret_cast<__m128i*>(&output[pos]), sumVec);
+    currentSum = _mm_extract_epi16(sumVec, 7);
+  }
   for (; pos < limit; ++pos) {
     currentSum -= input[pos - radius - 1];
     currentSum += input[pos + radius];
