fb21d41114463cd10159a84bbd2348f2e86c785d
Hamza Nasir <hamzanasir@hotmail.co.uk>
2026-09-05 23:14:48 +0100

    Solve core_bound/compiler_intrinsics_2

--- labs/core_bound/compiler_intrinsics_2/solution.cpp
+++ labs/core_bound/compiler_intrinsics_2/solution.cpp
@@ -1,5 +1,7 @@
 #include "solution.hpp"
-#include <iostream>
+
+#include <cstdint>
+#include <immintrin.h>
 
 // Find the longest line in a file.
 // Implementation uses ternary operator with a hope that compiler will
@@ -13,13 +15,40 @@ if (s == '\n') {
   curLineLength++;
 }*/
 unsigned solution(const std::string &inputContents) {
-  unsigned longestLine = 0;
-  unsigned curLineLength = 0;
 
-  for (auto s : inputContents) {
-    curLineLength = (s == '\n') ? 0 : curLineLength + 1;
-    longestLine = std::max(curLineLength, longestLine);
+  uint32_t trailingLength = 0;
+  uint32_t maxLineLength = 0;
+
+  auto it = inputContents.begin();
+  static constexpr int chunkSize = sizeof(__m256i);
+  static const __m256i mask = _mm256_set1_epi8('\n');
+
+  for (; it + chunkSize <= inputContents.end(); it += chunkSize) {
+
+    __m256i chunk = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(it.base()));
+    uint32_t newlines = _mm256_movemask_epi8(_mm256_cmpeq_epi8(chunk, mask));
+
+    uint32_t nextTrailingLength = _lzcnt_u32(newlines);
+    uint32_t lineBeginIdx = 0;
+
+    while (newlines != 0) {
+
+      uint32_t lineEndIdx = _tzcnt_u32(newlines);
+      uint32_t length = lineEndIdx - lineBeginIdx;
+      lineBeginIdx = lineEndIdx + 1;
+
+      maxLineLength = std::max(trailingLength + length, maxLineLength);
+      trailingLength = 0;
+      newlines = _blsr_u32(newlines);
+    }
+
+    trailingLength += nextTrailingLength;
   }
 
-  return longestLine;
+  for (; it < inputContents.end(); ++it) {
+    trailingLength = *it == '\n' ? 0 : trailingLength + 1;
+    maxLineLength = std::max(trailingLength, maxLineLength);
+  }
+
+  return maxLineLength;
 }
