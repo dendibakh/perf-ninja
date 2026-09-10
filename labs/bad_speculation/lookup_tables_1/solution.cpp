6674b81ede7fa4a82747aad143a814644d3459cf
Hamza Nasir <hamzanasir@hotmail.co.uk>
2026-09-10 21:48:49 +0100

    Solve bad_speculation/lookup_tables_1

--- labs/bad_speculation/lookup_tables_1/solution.cpp
+++ labs/bad_speculation/lookup_tables_1/solution.cpp
@@ -1,19 +1,28 @@
 #include "solution.hpp"
 
+static constexpr std::size_t MAP_SIZE = 101;
+
+static constexpr std::array<std::size_t, MAP_SIZE> createBucketMap() {
+  std::array<std::size_t, MAP_SIZE> bucketMap{};
+  int i = 0;
+  for (; i < 13; ++i) bucketMap[i] = 0;
+  for (; i < 29; ++i) bucketMap[i] = 1;
+  for (; i < 41; ++i) bucketMap[i] = 2;
+  for (; i < 53; ++i) bucketMap[i] = 3;
+  for (; i < 71; ++i) bucketMap[i] = 4;
+  for (; i < 83; ++i) bucketMap[i] = 5;
+  for (; i < MAP_SIZE - 1; ++i) bucketMap[i] = 6;
+  bucketMap[MAP_SIZE - 1] = DEFAULT_BUCKET;
+  return bucketMap;
+}
+
 static std::size_t mapToBucket(std::size_t v) {
-                              //   size of a bucket
-  if      (v < 13)  return 0; //   13
-  else if (v < 29)  return 1; //   16
-  else if (v < 41)  return 2; //   12
-  else if (v < 53)  return 3; //   12
-  else if (v < 71)  return 4; //   18
-  else if (v < 83)  return 5; //   12
-  else if (v < 100) return 6; //   17
-  return DEFAULT_BUCKET;
+  static constexpr std::array<std::size_t, MAP_SIZE> BUCKET_MAP = createBucketMap();
+  return BUCKET_MAP[std::min(v, MAP_SIZE - 1)];
 }
 
 std::array<std::size_t, NUM_BUCKETS> histogram(const std::vector<int> &values) {
-  std::array<std::size_t, NUM_BUCKETS> retBuckets{0};
+  std::array<std::size_t, NUM_BUCKETS> retBuckets{};
   for (auto v : values) {
     retBuckets[mapToBucket(v)]++;
   }
